c5ead9a315f3a8bf227024cc77f404bff68f4b35
Ragav Venkatesan <email@ragav.net>
2018-02-15 12:26:51 -0800

    merge conflicts

--- src/sagemaker/__init__.py
+++ src/sagemaker/__init__.py
@@ -35,4 +35,5 @@ __all__ = [estimator, KMeans, KMeansModel, KMeansPredictor, PCA, PCAModel, PCAPr
            LinearLearnerModel, LinearLearnerPredictor,
            LDA, LDAModel, LDAPredictor,
            FactorizationMachines, FactorizationMachinesModel, FactorizationMachinesPredictor,
+           ImageClassification, ImageClassificationModel, ImageClassificationPredictor
            container_def, s3_input, production_variant, get_execution_role]
