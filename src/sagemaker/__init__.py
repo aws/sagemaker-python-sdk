c61c7ef5fbf69d87c013710b49919497908d7744
Ragav Venkatesan <email@ragav.net>
2018-02-15 12:01:45 -0800

    updating for sync

--- src/sagemaker/__init__.py
+++ src/sagemaker/__init__.py
@@ -35,6 +35,5 @@ __all__ = [estimator, KMeans, KMeansModel, KMeansPredictor, PCA, PCAModel, PCAPr
            LinearLearnerModel, LinearLearnerPredictor,
            LDA, LDAModel, LDAPredictor,
            FactorizationMachines, FactorizationMachinesModel, FactorizationMachinesPredictor,
-           Model, RealTimePredictor, Session,
            ImageClassification, ImageClassificationModel, ImageClassificationPredictor,
            container_def, s3_input, production_variant, get_execution_role]
