57b82895f14010cbc0806a2855de76bdf736b2e2
Ignacio Quintero <rignacio@amazon.com>
2018-04-13 10:49:56 -0700

    Add a unit test for _download_folder() in image.py
    Also added the changelog entry

--- tests/unit/test_image.py
+++ tests/unit/test_image.py
@@ -16,7 +16,7 @@ import os
 
 import pytest
 import yaml
-from mock import call, patch, Mock, ANY
+from mock import call, patch, Mock
 
 import sagemaker
 from sagemaker.local.image import _SageMakerContainer
@@ -184,21 +184,17 @@ def test_check_output():
 @patch('sagemaker.local.image._SageMakerContainer._download_folder')
 def test_train(_download_folder, _cleanup, _execute_and_stream_output, LocalSession, tmpdir, sagemaker_session):
 
+    directories = [str(tmpdir.mkdir('container-root')), str(tmpdir.mkdir('data'))]
     with patch('sagemaker.local.image._SageMakerContainer._create_tmp_folder',
-               side_effect=[str(tmpdir.mkdir('container-root')), str(tmpdir.mkdir('data'))]):
+               side_effect=directories):
 
         instance_count = 2
         image = 'my-image'
         sagemaker_container = _SageMakerContainer('local', instance_count, image, sagemaker_session=sagemaker_session)
         sagemaker_container.train(INPUT_DATA_CONFIG, HYPERPARAMETERS)
 
-        download_folder_calls = []
-        for channel in INPUT_DATA_CONFIG:
-            s3_uri = channel['DataSource']['S3DataSource']['S3Uri']
-            if 's3://' in s3_uri:
-                bucket, prefix = s3_uri.replace('s3://', '').split('/')
-                download_folder_calls.append(call(bucket, prefix, ANY))
-        _download_folder.assert_called()
+        channel_dir = os.path.join(directories[1], 'b')
+        download_folder_calls = [call('my-own-bucket', 'prefix', channel_dir)]
         _download_folder.assert_has_calls(download_folder_calls)
 
         docker_compose_file = os.path.join(sagemaker_container.container_root, 'docker-compose.yaml')
@@ -239,6 +235,36 @@ def test_serve(up, copy, copytree, tmpdir, sagemaker_session):
             for h in sagemaker_container.hosts:
                 assert config['services'][h]['image'] == image
                 assert config['services'][h]['command'] == 'serve'
+
+
+@patch('os.makedirs')
+def test_download_folder(makedirs):
+    boto_mock = Mock(name='boto_session')
+    boto_mock.client('sts').get_caller_identity.return_value = {'Account': '123'}
+
+    session = sagemaker.Session(boto_session=boto_mock, sagemaker_client=Mock())
+
+    train_data = Mock()
+    validation_data = Mock()
+
+    train_data.bucket_name.return_value = BUCKET_NAME
+    train_data.key = '/prefix/train/train_data.csv'
+    validation_data.bucket_name.return_value = BUCKET_NAME
+    validation_data.key = '/prefix/train/validation_data.csv'
+
+    s3_files = [train_data, validation_data]
+    boto_mock.resource('s3').Bucket(BUCKET_NAME).objects.filter.return_value = s3_files
+
+    obj_mock = Mock()
+    boto_mock.resource('s3').Object.return_value = obj_mock
+
+    sagemaker_container = _SageMakerContainer('local', 2, 'my-image', sagemaker_session=session)
+    sagemaker_container._download_folder(BUCKET_NAME, '/prefix', '/tmp')
+
+    obj_mock.download_file.assert_called()
+    calls = [call(os.path.join('/tmp', 'train/train_data.csv')),
+             call(os.path.join('/tmp', 'train/validation_data.csv'))]
+    obj_mock.download_file.assert_has_calls(calls)
 
 
 def test_ecr_login_non_ecr():
