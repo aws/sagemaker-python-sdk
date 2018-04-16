45b67cf8f6dbe4e01536b3f02ae3fad80adcf4eb
Winston Dong <win@amazon.com>
2018-04-16 14:14:08 -0700

    Add scipy.sparse to mocked modules for docs

--- doc/conf.py
+++ doc/conf.py
@@ -16,7 +16,7 @@ class Mock(MagicMock):
 
 MOCK_MODULES = ['tensorflow', 'tensorflow.core', 'tensorflow.core.framework', 'tensorflow.python',
                 'tensorflow.python.framework', 'tensorflow_serving', 'tensorflow_serving.apis',
-                'numpy', 'scipy']
+                'numpy', 'scipy', 'scipy.sparse']
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 version = '1.2.2'
