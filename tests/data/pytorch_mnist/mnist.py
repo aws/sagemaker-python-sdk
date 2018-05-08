cb676ebfe1c6205b976591923ce5c2e92fa017a3
Nadia Yakimakha <32335935+nadiaya@users.noreply.github.com>
2018-05-08 00:03:57 -0700

    Add correct file format to model file.

--- tests/data/pytorch_mnist/mnist.py
+++ tests/data/pytorch_mnist/mnist.py
@@ -165,6 +165,6 @@ def test(model, test_loader, device):
 
 def model_fn(model_dir):
     model = torch.nn.DataParallel(Net())
-    with open(os.path.join(model_dir, 'model'), 'rb') as f:
+    with open(os.path.join(model_dir, 'model.pth'), 'rb') as f:
         model.load_state_dict(torch.load(f))
     return model
