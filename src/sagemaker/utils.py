cbd924e01965ac2564a5564ee588d108c0d0ad6e
icywang86rui <icywang.rui@gmail.com>
2018-07-31 09:14:38 -0700

    Check if SecondaryStatusTransitions for previous DescribeTrainingJob call (#325)

--- src/sagemaker/utils.py
+++ src/sagemaker/utils.py
@@ -146,8 +146,12 @@ def secondary_training_status_changed(current_job_description, prev_job_descript
     if current_secondary_status_transitions is None or len(current_secondary_status_transitions) == 0:
         return False
 
-    last_message = prev_job_description['SecondaryStatusTransitions'][-1]['StatusMessage']\
-        if prev_job_description is not None else ''
+    prev_job_secondary_status_transitions = prev_job_description.get('SecondaryStatusTransitions') \
+        if prev_job_description is not None else None
+
+    last_message = prev_job_secondary_status_transitions[-1]['StatusMessage'] \
+        if prev_job_secondary_status_transitions is not None and len(prev_job_secondary_status_transitions) > 0 else ''
+
     message = current_job_description['SecondaryStatusTransitions'][-1]['StatusMessage']
 
     return message != last_message
