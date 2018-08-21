ba9cac3785c15dd17c814f33afd5cd6967f268e4
Rui Wang <icywang.rui@gmail.com>
2018-08-21 13:25:13 -0700

    Add absolute_import

--- tests/integ/vpc_utils.py
+++ tests/integ/vpc_utils.py
@@ -10,6 +10,8 @@
 # distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF
 # ANY KIND, either express or implied. See the License for the specific
 # language governing permissions and limitations under the License.
+from __future__ import absolute_import
+
 
 def _get_subnet_id_by_name(ec2_client, name):
     desc = ec2_client.describe_subnets(Filters=[
