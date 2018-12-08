c042a6c880ef41e245346f2847ad14f9d6ce849a
yangaws <31293788+yangaws@users.noreply.github.com>
2018-12-08 10:40:52 -0800

    change version range for requests dependency (#533)
    * Define version range of requests module to work with docker-compose and docker
    
    * Update change log
    
    * Fix change log

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ def read(fname):
 # Declare minimal set for installation
 required_packages = ['boto3>=1.9.55', 'numpy>=1.9.0', 'protobuf>=3.1', 'scipy>=0.19.0',
                      'urllib3>=1.21', 'PyYAML>=3.2, <4', 'protobuf3-to-dict>=0.1.5',
-                     'docker-compose>=1.23.0']
+                     'docker-compose>=1.23.0', 'requests>=2.14.2, !=2.18.0, <2.21']
 
 # enum is introduced in Python 3.4. Installing enum back port
 if sys.version_info < (3, 4):
