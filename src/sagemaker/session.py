946adeb3205efb44d7ad99fa7d6bfb71616e69d1
knaresh <knares@gmail.com>
2019-07-19 14:20:04 -0700

    change: improve documentation of some functions (#864)
    [pr-827][followups] Improve documentation of some functions.
    Also some unit test fixes. See comments from marcio in
    https://github.com/aws/sagemaker-python-sdk/pull/827

--- src/sagemaker/session.py
+++ src/sagemaker/session.py
@@ -581,9 +581,9 @@ class Session(object):  # pylint: disable=too-many-public-methods
             input_config (dict): A dictionary describing the input data (and its location) for the job.
             output_config (dict): A dictionary describing the output location for the job.
             resource_config (dict): A dictionary describing the resources to complete the job.
-            tags (list[dict]): List of tags for labeling a transform job.
+            tags (list[dict]): List of tags for labeling a transform job. For more information,
+                see https://docs.aws.amazon.com/sagemaker/latest/dg/API_Tag.html.
             data_processing(dict): A dictionary describing config for combining the input data and transformed data.
-                                   For more, see https://docs.aws.amazon.com/sagemaker/latest/dg/API_Tag.html.
         """
         transform_request = {
             "TransformJobName": job_name,
