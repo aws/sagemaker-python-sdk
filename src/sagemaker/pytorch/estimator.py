783e76b6e9cd0c8b03ec0a49f5538625a46e8c7d
Chuyang Deng <chuyangd@amazon.com>
2019-07-19 09:18:13 -0700

    change: update PyTorch version

--- src/sagemaker/pytorch/estimator.py
+++ src/sagemaker/pytorch/estimator.py
@@ -33,7 +33,7 @@ class PyTorch(Framework):
 
     __framework_name__ = "pytorch"
 
-    LATEST_VERSION = "1.0"
+    LATEST_VERSION = "1.1"
     """The latest version of PyTorch included in the SageMaker pre-built Docker images."""
 
     def __init__(
