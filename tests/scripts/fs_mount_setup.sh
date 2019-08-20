c59cca31737e53b111315e67e6d4e21be5dc8729
Lauren Yu <6631887+laurenyu@users.noreply.github.com>
2019-08-20 12:51:09 -0700

    fix license headers

--- tests/scripts/fs_mount_setup.sh
+++ tests/scripts/fs_mount_setup.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 #
+# Copyright 2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"). You
+# may not use this file except in compliance with the License. A copy of
+# the License is located at
+#
+#     http://aws.amazon.com/apache2.0/
+#
+# or in the "license" file accompanying this file. This file is
+# distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF
+# ANY KIND, either express or implied. See the License for the specific
+# language governing permissions and limitations under the License.
+#
 # Mounting EFS and FSx for Lustre file systems for integration Tests
 FILE_SYSTEM_EFS_ID=$1
 FILE_SYSTEM_FSX_ID=$2
