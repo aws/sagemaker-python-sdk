5ff233675b5a8a6dc55508c52ba90a9cc2569590
Lauren Yu <6631887+laurenyu@users.noreply.github.com>
2019-10-25 15:31:37 -0700

    testing

--- tests/scripts/run-notebook-test.sh
+++ tests/scripts/run-notebook-test.sh
@@ -8,7 +8,8 @@ set -euo pipefail
 python setup.py sdist
 aws s3 --region us-west-2 cp ./dist/sagemaker-*.tar.gz s3://sagemaker-python-sdk-pr/sagemaker.tar.gz
 aws s3 cp s3://sagemaker-mead-cli/mead-nb-test.tar.gz mead-nb-test.tar.gz
-tar -xzf mead-nb-test.tar.gz
+tar -xzfv mead-nb-test.tar.gz
+ls
 git clone --depth 1 https://github.com/awslabs/amazon-sagemaker-examples.git
 export JAVA_HOME=$(get-java-home)
 echo "set JAVA_HOME=$JAVA_HOME"
