f2ef5902f02c2578fd81c4d03bac3e6cb3c853e8
Karim Nakad <knakad@amazon.com>
2019-12-03 16:32:00 -0800

    fix: correct Debugger tests (#305)

--- tests/integ/test_debugger.py
+++ tests/integ/test_debugger.py
@@ -22,8 +22,36 @@ from sagemaker.debugger import TensorBoardOutputConfig
 from sagemaker.debugger import rule_configs
 from sagemaker.mxnet.estimator import MXNet
 from tests.integ import DATA_DIR, PYTHON_VERSION, TRAINING_DEFAULT_TIMEOUT_MINUTES
+from tests.integ.retry import retries
 from tests.integ.timeout import timeout
 
+
+_NON_ERROR_TERMINAL_RULE_JOB_STATUSES = ["NoIssuesFound", "IssuesFound", "Stopped"]
+
+CUSTOM_RULE_REPO_WITH_PLACEHOLDERS = (
+    "{}.dkr.ecr.{}.amazonaws.com/sagemaker-debugger-rule-evaluator:latest"
+)
+
+CUSTOM_RULE_CONTAINERS_ACCOUNTS_MAP = {
+    "ap-east-1": "645844755771",
+    "ap-northeast-1": "670969264625",
+    "ap-northeast-2": "326368420253",
+    "ap-south-1": "552407032007",
+    "ap-southeast-1": "631532610101",
+    "ap-southeast-2": "445670767460",
+    "ca-central-1": "105842248657",
+    "eu-central-1": "691764027602",
+    "eu-north-1": "091235270104",
+    "eu-west-1": "606966180310",
+    "eu-west-2": "074613877050",
+    "eu-west-3": "224335253976",
+    "me-south-1": "050406412588",
+    "sa-east-1": "466516958431",
+    "us-east-1": "864354269164",
+    "us-east-2": "840043622174",
+    "us-west-1": "952348334681",
+    "us-west-2": "759209512951",
+}
 
 # TODO-reinvent-2019: test get_debugger_artifacts_path and get_tensorboard_artifacts_path
 
@@ -32,11 +60,13 @@ def test_mxnet_with_rules(sagemaker_session, mxnet_full_version, cpu_instance_ty
     with timeout(minutes=TRAINING_DEFAULT_TIMEOUT_MINUTES):
         rules = [
             Rule.sagemaker(rule_configs.vanishing_gradient()),
-            Rule.sagemaker(rule_configs.all_zero()),
+            Rule.sagemaker(
+                base_config=rule_configs.all_zero(), rule_parameters={"tensor_regex": ".*"}
+            ),
             Rule.sagemaker(rule_configs.loss_not_decreasing()),
         ]
 
-        script_path = os.path.join(DATA_DIR, "mxnet_mnist", "mnist.py")
+        script_path = os.path.join(DATA_DIR, "mxnet_mnist", "mnist_gluon.py")
         data_path = os.path.join(DATA_DIR, "mxnet_mnist")
 
         mx = MXNet(
@@ -82,21 +112,14 @@ def test_mxnet_with_rules(sagemaker_session, mxnet_full_version, cpu_instance_ty
             == mx.latest_training_job.rule_job_summary()
         )
 
+        _wait_and_assert_that_no_rule_jobs_errored(training_job=mx.latest_training_job)
+
 
 def test_mxnet_with_custom_rule(sagemaker_session, mxnet_full_version, cpu_instance_type):
     with timeout(minutes=TRAINING_DEFAULT_TIMEOUT_MINUTES):
-        rules = [
-            Rule.custom(
-                name="test-custom-rule",
-                instance_type="ml.m5.xlarge",
-                volume_size_in_gb=30,
-                image_uri="453379255795.dkr.ecr.{}.amazonaws.com/script-rule-executor:latest".format(
-                    sagemaker_session.boto_region_name
-                ),
-            )
-        ]
+        rules = [_get_custom_rule(sagemaker_session)]
 
-        script_path = os.path.join(DATA_DIR, "mxnet_mnist", "mnist.py")
+        script_path = os.path.join(DATA_DIR, "mxnet_mnist", "mnist_gluon.py")
         data_path = os.path.join(DATA_DIR, "mxnet_mnist")
 
         mx = MXNet(
@@ -135,6 +158,8 @@ def test_mxnet_with_custom_rule(sagemaker_session, mxnet_full_version, cpu_insta
             job_description["DebugRuleEvaluationStatuses"]
             == mx.latest_training_job.rule_job_summary()
         )
+
+        _wait_and_assert_that_no_rule_jobs_errored(training_job=mx.latest_training_job)
 
 
 def test_mxnet_with_debugger_hook_config(sagemaker_session, mxnet_full_version, cpu_instance_type):
@@ -145,7 +170,7 @@ def test_mxnet_with_debugger_hook_config(sagemaker_session, mxnet_full_version, 
             )
         )
 
-        script_path = os.path.join(DATA_DIR, "mxnet_mnist", "mnist.py")
+        script_path = os.path.join(DATA_DIR, "mxnet_mnist", "mnist_gluon.py")
         data_path = os.path.join(DATA_DIR, "mxnet_mnist")
 
         mx = MXNet(
@@ -171,6 +196,8 @@ def test_mxnet_with_debugger_hook_config(sagemaker_session, mxnet_full_version, 
         job_description = mx.latest_training_job.describe()
         assert job_description["DebugHookConfig"] == debugger_hook_config._to_request_dict()
 
+        _wait_and_assert_that_no_rule_jobs_errored(training_job=mx.latest_training_job)
+
 
 def test_mxnet_with_rules_and_debugger_hook_config(
     sagemaker_session, mxnet_full_version, cpu_instance_type
@@ -178,7 +205,9 @@ def test_mxnet_with_rules_and_debugger_hook_config(
     with timeout(minutes=TRAINING_DEFAULT_TIMEOUT_MINUTES):
         rules = [
             Rule.sagemaker(rule_configs.vanishing_gradient()),
-            Rule.sagemaker(rule_configs.all_zero()),
+            Rule.sagemaker(
+                base_config=rule_configs.all_zero(), rule_parameters={"tensor_regex": ".*"}
+            ),
             Rule.sagemaker(rule_configs.loss_not_decreasing()),
         ]
         debugger_hook_config = DebuggerHookConfig(
@@ -187,7 +216,7 @@ def test_mxnet_with_rules_and_debugger_hook_config(
             )
         )
 
-        script_path = os.path.join(DATA_DIR, "mxnet_mnist", "mnist.py")
+        script_path = os.path.join(DATA_DIR, "mxnet_mnist", "mnist_gluon.py")
         data_path = os.path.join(DATA_DIR, "mxnet_mnist")
 
         mx = MXNet(
@@ -235,28 +264,21 @@ def test_mxnet_with_rules_and_debugger_hook_config(
             == mx.latest_training_job.rule_job_summary()
         )
 
+        _wait_and_assert_that_no_rule_jobs_errored(training_job=mx.latest_training_job)
+
 
 def test_mxnet_with_custom_rule_and_debugger_hook_config(
     sagemaker_session, mxnet_full_version, cpu_instance_type
 ):
     with timeout(minutes=TRAINING_DEFAULT_TIMEOUT_MINUTES):
-        rules = [
-            Rule.custom(
-                name="test-custom-rule",
-                instance_type="ml.m5.xlarge",
-                volume_size_in_gb=30,
-                image_uri="453379255795.dkr.ecr.{}.amazonaws.com/script-rule-executor:latest".format(
-                    sagemaker_session.boto_region_name
-                ),
-            )
-        ]
+        rules = [_get_custom_rule(sagemaker_session)]
         debugger_hook_config = DebuggerHookConfig(
             s3_output_path=os.path.join(
                 "s3://", sagemaker_session.default_bucket(), str(uuid.uuid4()), "tensors"
             )
         )
 
-        script_path = os.path.join(DATA_DIR, "mxnet_mnist", "mnist.py")
+        script_path = os.path.join(DATA_DIR, "mxnet_mnist", "mnist_gluon.py")
         data_path = os.path.join(DATA_DIR, "mxnet_mnist")
 
         mx = MXNet(
@@ -297,6 +319,8 @@ def test_mxnet_with_custom_rule_and_debugger_hook_config(
             job_description["DebugRuleEvaluationStatuses"]
             == mx.latest_training_job.rule_job_summary()
         )
+
+        _wait_and_assert_that_no_rule_jobs_errored(training_job=mx.latest_training_job)
 
 
 def test_mxnet_with_tensorboard_output_config(
@@ -309,7 +333,7 @@ def test_mxnet_with_tensorboard_output_config(
             )
         )
 
-        script_path = os.path.join(DATA_DIR, "mxnet_mnist", "mnist.py")
+        script_path = os.path.join(DATA_DIR, "mxnet_mnist", "mnist_gluon.py")
         data_path = os.path.join(DATA_DIR, "mxnet_mnist")
 
         mx = MXNet(
@@ -338,28 +362,18 @@ def test_mxnet_with_tensorboard_output_config(
             == tensorboard_output_config._to_request_dict()
         )
 
+        _wait_and_assert_that_no_rule_jobs_errored(training_job=mx.latest_training_job)
+
 
 def test_mxnet_with_all_rules_and_configs(sagemaker_session, mxnet_full_version, cpu_instance_type):
     with timeout(minutes=TRAINING_DEFAULT_TIMEOUT_MINUTES):
         rules = [
             Rule.sagemaker(rule_configs.vanishing_gradient()),
-            Rule.sagemaker(rule_configs.all_zero()),
-            Rule.sagemaker(rule_configs.similar_across_runs()),
-            Rule.sagemaker(rule_configs.weight_update_ratio()),
-            Rule.sagemaker(rule_configs.exploding_tensor()),
-            Rule.sagemaker(rule_configs.unchanged_tensor()),
-            Rule.sagemaker(rule_configs.loss_not_decreasing()),
-            Rule.sagemaker(rule_configs.check_input_images()),
-            Rule.sagemaker(rule_configs.dead_relu()),
-            Rule.sagemaker(rule_configs.confusion()),
-            Rule.custom(
-                name="test-custom-rule",
-                instance_type="ml.m5.xlarge",
-                volume_size_in_gb=30,
-                image_uri="453379255795.dkr.ecr.{}.amazonaws.com/script-rule-executor:latest".format(
-                    sagemaker_session.boto_region_name
-                ),
+            Rule.sagemaker(
+                base_config=rule_configs.all_zero(), rule_parameters={"tensor_regex": ".*"}
             ),
+            Rule.sagemaker(rule_configs.loss_not_decreasing()),
+            _get_custom_rule(sagemaker_session),
         ]
         debugger_hook_config = DebuggerHookConfig(
             s3_output_path=os.path.join(
@@ -372,7 +386,7 @@ def test_mxnet_with_all_rules_and_configs(sagemaker_session, mxnet_full_version,
             )
         )
 
-        script_path = os.path.join(DATA_DIR, "mxnet_mnist", "mnist.py")
+        script_path = os.path.join(DATA_DIR, "mxnet_mnist", "mnist_gluon.py")
         data_path = os.path.join(DATA_DIR, "mxnet_mnist")
 
         mx = MXNet(
@@ -417,3 +431,45 @@ def test_mxnet_with_all_rules_and_configs(sagemaker_session, mxnet_full_version,
             job_description["DebugRuleEvaluationStatuses"]
             == mx.latest_training_job.rule_job_summary()
         )
+
+        _wait_and_assert_that_no_rule_jobs_errored(training_job=mx.latest_training_job)
+
+
+def _get_custom_rule(session):
+    script_path = os.path.join(DATA_DIR, "mxnet_mnist", "my_custom_rule.py")
+
+    return Rule.custom(
+        name="test-custom-rule",
+        source=script_path,
+        rule_to_invoke="CustomGradientRule",
+        instance_type="ml.m5.xlarge",
+        volume_size_in_gb=30,
+        image_uri=CUSTOM_RULE_REPO_WITH_PLACEHOLDERS.format(
+            CUSTOM_RULE_CONTAINERS_ACCOUNTS_MAP[session.boto_region_name], session.boto_region_name
+        ),
+    )
+
+
+def _wait_and_assert_that_no_rule_jobs_errored(training_job):
+    # Wait for all rule jobs to complete.
+    # Training job completion takes takes ~5min after training job ends
+    # 120 retries * 10s sleeps = 20min timeout
+    for _ in retries(
+        max_retry_count=120,
+        exception_message_prefix="Waiting for all jobs to be in success status or any to be in error",
+        seconds_to_sleep=10,
+    ):
+        job_description = training_job.describe()
+        debug_rule_evaluation_statuses = job_description.get("DebugRuleEvaluationStatuses")
+        if not debug_rule_evaluation_statuses:
+            break
+        incomplete_rule_job_found = False
+        for debug_rule_evaluation_status in debug_rule_evaluation_statuses:
+            assert debug_rule_evaluation_status["RuleEvaluationStatus"] != "Error"
+            if (
+                debug_rule_evaluation_status["RuleEvaluationStatus"]
+                not in _NON_ERROR_TERMINAL_RULE_JOB_STATUSES
+            ):
+                incomplete_rule_job_found = True
+        if not incomplete_rule_job_found:
+            break
