6decf34c7e4fbbd176ef3567bbf0cf6659c9f6d3
Karim Nakad <knakad@amazon.com>
2019-12-25 10:19:37 -0800

    change: fixing 2 other occurrences

--- tests/integ/test_data_capture_config.py
+++ tests/integ/test_data_capture_config.py
@@ -159,11 +159,11 @@ def test_disabling_data_capture_on_endpoint_shows_correct_data_capture_status(
         predictor.disable_data_capture()
 
         # Wait for endpoint to finish updating
-        # Endpoint update takes ~7min. 50 retries * 30s sleeps = 25min timeout
+        # Endpoint update takes ~7min. 25 retries * 60s sleeps = 25min timeout
         for _ in retries(
-            max_retry_count=50,
+            max_retry_count=25,
             exception_message_prefix="Waiting for 'InService' endpoint status",
-            seconds_to_sleep=30,
+            seconds_to_sleep=60,
         ):
             new_endpoint = sagemaker_session.sagemaker_client.describe_endpoint(
                 EndpointName=predictor.endpoint
@@ -228,11 +228,11 @@ def test_updating_data_capture_on_endpoint_shows_correct_data_capture_status(
         )
 
         # Wait for endpoint to finish updating
-        # Endpoint update takes ~7min. 50 retries * 30s sleeps = 25min timeout
+        # Endpoint update takes ~7min. 25 retries * 60s sleeps = 25min timeout
         for _ in retries(
-            max_retry_count=50,
+            max_retry_count=25,
             exception_message_prefix="Waiting for 'InService' endpoint status",
-            seconds_to_sleep=30,
+            seconds_to_sleep=60,
         ):
             new_endpoint = sagemaker_session.sagemaker_client.describe_endpoint(
                 EndpointName=predictor.endpoint
