e126936bc6db7ebfb5d442a5212b52bb73094948
Karim Nakad <knakad@amazon.com>
2020-01-22 16:19:27 -0800

    infra: add all notebook tests we own
    Some will fail. This blind push saves me the deep dive on each one.

--- tests/scripts/run-notebook-test.sh
+++ tests/scripts/run-notebook-test.sh
@@ -19,8 +19,31 @@ echo "set SAGEMAKER_ROLE_ARN=$SAGEMAKER_ROLE_ARN"
 --region us-west-2 \
 --lifecycle-config-name install-python-sdk \
 --notebook-instance-role-arn $SAGEMAKER_ROLE_ARN \
+./amazon-sagemaker-examples/advanced_functionality/pytorch_extending_our_containers/pytorch_extending_our_containers.ipynb \
+./amazon-sagemaker-examples/advanced_functionality/tensorflow_bring_your_own/tensorflow_bring_your_own.ipynb \
+./amazon-sagemaker-examples/sagemaker-python-sdk/1P_kmeans_highlevel/kmeans_mnist.ipynb \
+./amazon-sagemaker-examples/sagemaker-python-sdk/1P_kmeans_lowlevel/kmeans_mnist_lowlevel.ipynb \
+./amazon-sagemaker-examples/sagemaker-python-sdk/mxnet_gluon_cifar10/mxnet_cifar10_local_mode.ipynb \
+./amazon-sagemaker-examples/sagemaker-python-sdk/mxnet_gluon_cifar10/mxnet_cifar10_with_gluon.ipynb \
+./amazon-sagemaker-examples/sagemaker-python-sdk/mxnet_gluon_mnist/mxnet_mnist_with_gluon.ipynb \
+./amazon-sagemaker-examples/sagemaker-python-sdk/mxnet_gluon_mnist/mxnet_mnist_with_gluon_local_mode.ipynb \
+./amazon-sagemaker-examples/sagemaker-python-sdk/mxnet_gluon_sentiment/mxnet_sentiment_analysis_with_gluon.ipynb \
+./amazon-sagemaker-examples/sagemaker-python-sdk/mxnet_mnist/mxnet_mnist.ipynb \
+./amazon-sagemaker-examples/sagemaker-python-sdk/mxnet_mnist/mxnet_mnist_elastic_inference.ipynb \
+./amazon-sagemaker-examples/sagemaker-python-sdk/mxnet_mnist/mxnet_mnist_with_batch_transform.ipynb \
+./amazon-sagemaker-examples/sagemaker-python-sdk/mxnet_onnx_superresolution/mxnet_onnx.ipynb \
+./amazon-sagemaker-examples/sagemaker-python-sdk/pytorch_cnn_cifar10/pytorch_local_mode_cifar10.ipynb \
+./amazon-sagemaker-examples/sagemaker-python-sdk/pytorch_lstm_word_language_model/pytorch_rnn.ipynb \
+./amazon-sagemaker-examples/sagemaker-python-sdk/pytorch_mnist/pytorch_mnist.ipynb \
+./amazon-sagemaker-examples/sagemaker-python-sdk/tensorflow_abalone_age_predictor_using_keras/tensorflow_abalone_age_predictor_using_keras.ipynb \
+./amazon-sagemaker-examples/sagemaker-python-sdk/tensorflow_abalone_age_predictor_using_layers/tensorflow_abalone_age_predictor_using_layers.ipynb \
 ./amazon-sagemaker-examples/sagemaker-python-sdk/tensorflow_distributed_mnist/tensorflow_batch_transform_mnist.ipynb \
-./amazon-sagemaker-examples/sagemaker-python-sdk/tensorflow_distributed_mnist/tensorflow_batch_transform_mnist.ipynb \
-./amazon-sagemaker-examples/sagemaker-python-sdk/tensorflow_distributed_mnist/tensorflow_batch_transform_mnist.ipynb \
-./amazon-sagemaker-examples/sagemaker-python-sdk/tensorflow_distributed_mnist/tensorflow_batch_transform_mnist.ipynb \
-./amazon-sagemaker-examples/sagemaker-python-sdk/tensorflow_distributed_mnist/tensorflow_batch_transform_mnist.ipynb
+./amazon-sagemaker-examples/sagemaker-python-sdk/tensorflow_distributed_mnist/tensorflow_distributed_mnist.ipynb \
+./amazon-sagemaker-examples/sagemaker-python-sdk/tensorflow_distributed_mnist/tensorflow_local_mode_mnist.ipynb \
+./amazon-sagemaker-examples/sagemaker-python-sdk/tensorflow_iris_dnn_classifier_using_estimators/tensorflow_iris_dnn_classifier_using_estimators.ipynb \
+./amazon-sagemaker-examples/sagemaker-python-sdk/tensorflow_iris_dnn_classifier_using_estimators/tensorflow_iris_dnn_classifier_using_estimators_elastic_inference.ipynb \
+./amazon-sagemaker-examples/sagemaker-python-sdk/tensorflow_iris_dnn_classifier_using_estimators/tensorflow_iris_dnn_classifier_using_estimators_elastic_inference_local.ipynb \
+./amazon-sagemaker-examples/sagemaker-python-sdk/tensorflow_keras_cifar10/tensorflow_keras_CIFAR10.ipynb \
+./amazon-sagemaker-examples/sagemaker-python-sdk/tensorflow_pipemode_example/tensorflow_pipemode_example.ipynb \
+./amazon-sagemaker-examples/sagemaker-python-sdk/tensorflow_resnet_cifar10_with_tensorboard/tensorflow_resnet_cifar10_with_tensorboard.ipynb \
+./amazon-sagemaker-examples/sagemaker-python-sdk/pytorch_mnist/pytorch_mnist.ipynb
