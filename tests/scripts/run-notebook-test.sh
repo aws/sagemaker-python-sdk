6e815b1a13b685749bd6297f6fa2546d613482fd
Karim Nakad <knakad@amazon.com>
2020-02-12 17:03:37 -0800

    infra: remove notebooks with known issues

--- tests/scripts/run-notebook-test.sh
+++ tests/scripts/run-notebook-test.sh
@@ -20,40 +20,21 @@ echo "set SAGEMAKER_ROLE_ARN=$SAGEMAKER_ROLE_ARN"
 --lifecycle-config-name install-python-sdk \
 --notebook-instance-role-arn $SAGEMAKER_ROLE_ARN \
 ./amazon-sagemaker-examples/advanced_functionality/kmeans_bring_your_own_model/kmeans_bring_your_own_model.ipynb \
-./amazon-sagemaker-examples/advanced_functionality/pytorch_extending_our_containers/pytorch_extending_our_containers.ipynb \
-./amazon-sagemaker-examples/advanced_functionality/r_bring_your_own/r_bring_your_own.ipynb \
-./amazon-sagemaker-examples/advanced_functionality/scikit_bring_your_own/scikit_bring_your_own.ipynb \
-./amazon-sagemaker-examples/advanced_functionality/tensorflow_bring_your_own/tensorflow_bring_your_own.ipynb \
 ./amazon-sagemaker-examples/advanced_functionality/tensorflow_iris_byom/tensorflow_BYOM_iris.ipynb \
 ./amazon-sagemaker-examples/sagemaker-python-sdk/1P_kmeans_highlevel/kmeans_mnist.ipynb \
 ./amazon-sagemaker-examples/sagemaker-python-sdk/1P_kmeans_lowlevel/kmeans_mnist_lowlevel.ipynb \
-./amazon-sagemaker-examples/sagemaker-python-sdk/chainer_cifar10/chainer_single_machine_cifar10.ipynb \
-./amazon-sagemaker-examples/sagemaker-python-sdk/chainer_cifar10/chainermn_distributed_cifar10.ipynb \
-./amazon-sagemaker-examples/sagemaker-python-sdk/chainer_mnist/chainer_mnist_local_mode.ipynb \
 ./amazon-sagemaker-examples/sagemaker-python-sdk/chainer_sentiment_analysis/chainer_sentiment_analysis.ipynb \
 ./amazon-sagemaker-examples/sagemaker-python-sdk/keras_script_mode_pipe_mode_horovod/tensorflow_keras_CIFAR10.ipynb \
 ./amazon-sagemaker-examples/sagemaker-python-sdk/managed_spot_training_mxnet/managed_spot_training_mxnet.ipynb \
 ./amazon-sagemaker-examples/sagemaker-python-sdk/managed_spot_training_tensorflow_estimator/managed_spot_training_tensorflow_estimator.ipynb \
-./amazon-sagemaker-examples/sagemaker-python-sdk/mxnet_gluon_cifar10/mxnet_cifar10_local_mode.ipynb \
-./amazon-sagemaker-examples/sagemaker-python-sdk/mxnet_gluon_cifar10/mxnet_cifar10_with_gluon.ipynb \
-./amazon-sagemaker-examples/sagemaker-python-sdk/mxnet_gluon_mnist/mxnet_mnist_with_gluon.ipynb \
-./amazon-sagemaker-examples/sagemaker-python-sdk/mxnet_gluon_mnist/mxnet_mnist_with_gluon_local_mode.ipynb \
 ./amazon-sagemaker-examples/sagemaker-python-sdk/mxnet_gluon_sentiment/mxnet_sentiment_analysis_with_gluon.ipynb \
-./amazon-sagemaker-examples/sagemaker-python-sdk/mxnet_mnist/mxnet_mnist.ipynb \
-./amazon-sagemaker-examples/sagemaker-python-sdk/mxnet_mnist/mxnet_mnist_elastic_inference.ipynb \
-./amazon-sagemaker-examples/sagemaker-python-sdk/mxnet_mnist/mxnet_mnist_elastic_inference_local.ipynb \
 ./amazon-sagemaker-examples/sagemaker-python-sdk/mxnet_mnist/mxnet_mnist_with_batch_transform.ipynb \
 ./amazon-sagemaker-examples/sagemaker-python-sdk/mxnet_onnx_eia/mxnet_onnx_eia.ipynb \
 ./amazon-sagemaker-examples/sagemaker-python-sdk/mxnet_onnx_export/mxnet_onnx_export.ipynb \
 ./amazon-sagemaker-examples/sagemaker-python-sdk/mxnet_onnx_superresolution/mxnet_onnx.ipynb \
-./amazon-sagemaker-examples/sagemaker-python-sdk/paddlepaddle_sentiment_analysis_byo_mms/Bring\ Your\ Own\ DL\ Framework\ to\ Amazon\ Sagemaker\ with\ Model\ Server\ for\ Apache\ MXNet\'s\ \(MMS\)\ BYO\ container.ipynb \
 ./amazon-sagemaker-examples/sagemaker-python-sdk/pytorch_cnn_cifar10/pytorch_local_mode_cifar10.ipynb \
 ./amazon-sagemaker-examples/sagemaker-python-sdk/pytorch_lstm_word_language_model/pytorch_rnn.ipynb \
-./amazon-sagemaker-examples/sagemaker-python-sdk/pytorch_mnist/pytorch_mnist.ipynb \
-./amazon-sagemaker-examples/sagemaker-python-sdk/scikit_learn_inference_pipeline/Inference\ Pipeline\ with\ Scikit-learn\ and\ Linear\ Learner.ipynb \
-./amazon-sagemaker-examples/sagemaker-python-sdk/scikit_learn_iris/Scikit-learn\ Estimator\ Example\ With\ Batch\ Transform.ipynb \
 ./amazon-sagemaker-examples/sagemaker-python-sdk/scikit_learn_randomforest/Sklearn_on_SageMaker_end2end.ipynb \
-./amazon-sagemaker-examples/sagemaker-python-sdk/sparkml_serving_emr_mleap_abalone/sparkml_serving_emr_mleap_abalone.ipynb \
 ./amazon-sagemaker-examples/sagemaker-python-sdk/tensorflow-eager-script-mode/tf-eager-sm-scriptmode.ipynb \
 ./amazon-sagemaker-examples/sagemaker-python-sdk/tensorflow_abalone_age_predictor_using_keras/tensorflow_abalone_age_predictor_using_keras.ipynb \
 ./amazon-sagemaker-examples/sagemaker-python-sdk/tensorflow_abalone_age_predictor_using_layers/tensorflow_abalone_age_predictor_using_layers.ipynb \
@@ -62,7 +43,6 @@ echo "set SAGEMAKER_ROLE_ARN=$SAGEMAKER_ROLE_ARN"
 ./amazon-sagemaker-examples/sagemaker-python-sdk/tensorflow_distributed_mnist/tensorflow_local_mode_mnist.ipynb \
 ./amazon-sagemaker-examples/sagemaker-python-sdk/tensorflow_iris_dnn_classifier_using_estimators/tensorflow_iris_dnn_classifier_using_estimators.ipynb \
 ./amazon-sagemaker-examples/sagemaker-python-sdk/tensorflow_iris_dnn_classifier_using_estimators/tensorflow_iris_dnn_classifier_using_estimators_elastic_inference.ipynb \
-./amazon-sagemaker-examples/sagemaker-python-sdk/tensorflow_iris_dnn_classifier_using_estimators/tensorflow_iris_dnn_classifier_using_estimators_elastic_inference_local.ipynb \
 ./amazon-sagemaker-examples/sagemaker-python-sdk/tensorflow_keras_cifar10/tensorflow_keras_CIFAR10.ipynb \
 ./amazon-sagemaker-examples/sagemaker-python-sdk/tensorflow_moving_from_framework_mode_to_script_mode/tensorflow_moving_from_framework_mode_to_script_mode.ipynb \
 ./amazon-sagemaker-examples/sagemaker-python-sdk/tensorflow_pipemode_example/tensorflow_pipemode_example.ipynb \
@@ -75,7 +55,6 @@ echo "set SAGEMAKER_ROLE_ARN=$SAGEMAKER_ROLE_ARN"
 ./amazon-sagemaker-examples/sagemaker-python-sdk/tensorflow_serving_container/tensorflow_serving_container.ipynb \
 ./amazon-sagemaker-examples/sagemaker-python-sdk/tensorflow_serving_using_elastic_inference_with_your_own_model/tensorflow_serving_pretrained_model_elastic_inference.ipynb \
 ./amazon-sagemaker-examples/sagemaker-spark/pyspark_mnist/pyspark_mnist_custom_estimator.ipynb \
-./amazon-sagemaker-examples/sagemaker-spark/pyspark_mnist/pyspark_mnist_kmeans.ipynb \
 ./amazon-sagemaker-examples/sagemaker-spark/pyspark_mnist/pyspark_mnist_pca_kmeans.ipynb \
 ./amazon-sagemaker-examples/sagemaker-spark/pyspark_mnist/pyspark_mnist_pca_mllib_kmeans.ipynb \
 ./amazon-sagemaker-examples/sagemaker-spark/pyspark_mnist/pyspark_mnist_xgboost.ipynb
