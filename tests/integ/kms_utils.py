47053284183b92e71b050510949e3aba2ab447a0
Karim Nakad <knakad@amazon.com>
2020-03-02 09:44:08 -0800

    properly escape KMS_BUCKET_POLICY constant for .format() to function

--- tests/integ/kms_utils.py
+++ tests/integ/kms_utils.py
@@ -129,36 +129,36 @@ def get_or_create_kms_key(
     return kms_key_arn
 
 
-KMS_BUCKET_POLICY = """{
+KMS_BUCKET_POLICY = """{{
   "Version": "2012-10-17",
   "Id": "PutObjPolicy",
   "Statement": [
-    {
+    {{
       "Sid": "DenyIncorrectEncryptionHeader",
       "Effect": "Deny",
       "Principal": "*",
       "Action": "s3:PutObject",
       "Resource": "arn:{partition}:s3:::{bucket_name}/*",
-      "Condition": {
-        "StringNotEquals": {
+      "Condition": {{
+        "StringNotEquals": {{
           "s3:x-amz-server-side-encryption": "{partition}:kms"
-        }
-      }
-    },
-    {
+        }}
+      }}
+    }},
+    {{
       "Sid": "DenyUnEncryptedObjectUploads",
       "Effect": "Deny",
       "Principal": "*",
       "Action": "s3:PutObject",
       "Resource": "arn:{partition}:s3:::{bucket_name}/*",
-      "Condition": {
-        "Null": {
+      "Condition": {{
+        "Null": {{
           "s3:x-amz-server-side-encryption": "true"
-        }
-      }
-    }
+        }}
+      }}
+    }}
   ]
-}"""
+}}"""
 
 
 @contextlib.contextmanager
