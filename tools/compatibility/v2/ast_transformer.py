92e6d570d7efb5cd1f6d1c61c3dc62489933c5c1
Lauren Yu <6631887+laurenyu@users.noreply.github.com>
2020-05-08 10:25:19 -0700

    change: create ASTTransformer class to handle migrating Python SDK code for v2
    As a start, this class ensures that the framework_version parameter
    is specified when framework classes are instantiated.

---
+++ tools/compatibility/v2/ast_transformer.py
@@ -0,0 +1,41 @@
+# Copyright 2020 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"). You
+# may not use this file except in compliance with the License. A copy of
+# the License is located at
+#
+#     http://aws.amazon.com/apache2.0/
+#
+# or in the "license" file accompanying this file. This file is
+# distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF
+# ANY KIND, either express or implied. See the License for the specific
+# language governing permissions and limitations under the License.
+"""An ast.NodeTransformer subclass for updating SageMaker Python SDK code."""
+from __future__ import absolute_import
+
+import ast
+
+from modifiers import framework_version
+
+FUNCTION_CALL_MODIFIERS = [framework_version.FrameworkVersionEnforcer()]
+
+
+class ASTTransformer(ast.NodeTransformer):
+    """An ``ast.NodeTransformer`` subclass that walks the abstract syntax tree and
+    modifies nodes to upgrade the given SageMaker Python SDK code.
+    """
+
+    def visit_Call(self, node):
+        """Visits an ``ast.Call`` node and returns a modified node, if needed.
+        See https://docs.python.org/3/library/ast.html#ast.NodeTransformer.
+
+        Args:
+            node (ast.Call): a node that represents a function call.
+
+        Returns:
+            ast.Call: a node that represents a function call, which has
+                potentially been modified from the original input.
+        """
+        for function_checker in FUNCTION_CALL_MODIFIERS:
+            function_checker.check_and_modify_node(node)
+        return node
