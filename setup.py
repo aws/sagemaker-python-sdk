c58bd0fac012d6e5093f5263a2547423fd427a77
Lauren Yu <6631887+laurenyu@users.noreply.github.com>
2020-05-09 17:14:38 -0700

    fix: upgrade boto3 to 1.13.6 (#1483)

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ def read_version():
 
 # Declare minimal set for installation
 required_packages = [
-    "boto3>=1.10.44",
+    "boto3>=1.13.6",
     "numpy>=1.9.0",
     "protobuf>=3.1",
     "scipy>=0.19.0",
