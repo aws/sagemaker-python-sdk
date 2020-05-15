d197b7456ed9aee68c6c026f13c0ce731db03804
Lauren Yu <6631887+laurenyu@users.noreply.github.com>
2020-05-15 14:48:55 -0700

    infra: add tools/ dir to pylint check (#1499)

--- tools/__init__.py
+++ tools/__init__.py
@@ -10,4 +10,5 @@
 # distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF
 # ANY KIND, either express or implied. See the License for the specific
 # language governing permissions and limitations under the License.
+"""Tools to assist with using the SageMake Python SDK."""
 from __future__ import absolute_import
