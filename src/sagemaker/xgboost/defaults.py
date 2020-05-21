16768ff9cc2dbfdd9aa42c63520753ca19c0b9f1
Kim <kimedw@amazon.com>
2020-05-20 23:40:07 -0700

    change: blacklist unknown xgboost image versions

--- src/sagemaker/xgboost/defaults.py
+++ src/sagemaker/xgboost/defaults.py
@@ -14,10 +14,12 @@
 from __future__ import absolute_import
 
 XGBOOST_NAME = "xgboost"
+XGBOOST_1P_VERSIONS = ["1", "latest"]
 XGBOOST_VERSION_0_90 = "0.90"
 XGBOOST_VERSION_0_90_1 = "0.90-1"
 XGBOOST_VERSION_0_90_2 = "0.90-2"
 XGBOOST_LATEST_VERSION = "1.0-1"
+# XGBOOST_SUPPORTED_VERSIONS has XGBoost Framework versions sorted from oldest to latest
 XGBOOST_SUPPORTED_VERSIONS = [
     XGBOOST_VERSION_0_90_1,
     XGBOOST_VERSION_0_90_2,
