64c66f22b338a62117d967817fdba471365e7916
owahab <ola.wahab@zalando.de>
2020-06-08 13:31:25 +0200

    feature: Add tests for ast_transformer

--- tests/unit/v2/utils.py
+++
@@ -1,9 +0,0 @@
-from os.path import join
-
-SAMPLES_DIRECTORY = "/Users/owahab/Desktop/personal/sagemaker-python-sdk/tests/unit/v2/samples/"
-
-
-def get_sample_file(filename):
-    file_path = join(SAMPLES_DIRECTORY, filename)
-    with open(file_path) as file_content:
-        return file_content.read()
