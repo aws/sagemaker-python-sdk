7a6dd355575c5244a381b29cfe0b7df36a461e9f
Chuyang Deng <chuyangd@amazon.com>
2020-06-10 15:57:35 -0700

    fix: workflow passing spot training param to training job

--- src/sagemaker/workflow/airflow.py
+++ src/sagemaker/workflow/airflow.py
@@ -181,6 +181,9 @@ def training_base_config(estimator, inputs=None, job_name=None, mini_batch_size=
     if job_config["vpc_config"] is not None:
         train_config["VpcConfig"] = job_config["vpc_config"]
 
+    if estimator.train_use_spot_instances:
+        train_config["EnableManagedSpotTraining"] = True
+
     if estimator.hyperparameters() is not None:
         hyperparameters = {str(k): str(v) for (k, v) in estimator.hyperparameters().items()}
 
