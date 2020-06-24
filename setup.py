60bffc27508f3085df7c64320b3c0b93577a29ef
Eric Johnson <65414824+metrizable@users.noreply.github.com>
2020-06-24 16:53:02 -0700

    feature: add 3.8 as supported python version (#1626)

--- setup.py
+++ setup.py
@@ -102,6 +102,7 @@ setup(
         "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
     ],
     install_requires=required_packages,
     extras_require=extras,
