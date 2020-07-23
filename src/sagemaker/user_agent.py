675dc124623adf03635d056505dddaed2a218596
Nadia Yakimakha <32335935+nadiaya@users.noreply.github.com>
2020-07-23 16:16:10 -0700

    fix pylint

--- src/sagemaker/user_agent.py
+++ src/sagemaker/user_agent.py
@@ -16,8 +16,6 @@ from __future__ import absolute_import
 import platform
 import sys
 
-import boto3
-import botocore
 import importlib_metadata
 
 SDK_VERSION = importlib_metadata.version("sagemaker")
