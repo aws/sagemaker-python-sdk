39d1699dd5a7995192c5921faad0b02b770f7cf3
Balaji Veeramani <bveeramani@berkeley.edu>
2020-07-24 15:02:37 -0500

    Update src/sagemaker/cli/compatibility/v2/modifiers/serde.py
    Co-authored-by: Lauren Yu <6631887+laurenyu@users.noreply.github.com>

--- src/sagemaker/cli/compatibility/v2/modifiers/serde.py
+++ src/sagemaker/cli/compatibility/v2/modifiers/serde.py
@@ -159,7 +159,7 @@ class SerdeConstructorRenamer(Modifier):
             return node
 
         namespace_name = NEW_CLASS_NAME_TO_NAMESPACES[new_class_name][0]
-        subpackage_name = namespace_name[namespace_name.find(".") + 1 :]
+        subpackage_name = namespace_name.split(".")[1]
         assert subpackage_name in {"serializers", "deserializers"}
         return pasta.parse("%s.%s()" % (subpackage_name, new_class_name)).body[0].value
 
