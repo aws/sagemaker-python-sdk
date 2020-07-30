b34d6802a4a2c304379769b48879db2a77d809aa
Chuyang Deng <chuyangd@amazon.com>
2020-07-30 10:20:01 -0700

    add migration tool for image_uris.retrieve

---
+++ src/sagemaker/cli/compatibility/v2/modifiers/image_uris.py
@@ -0,0 +1,96 @@
+# Copyright 2020 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"). You
+# may not use this file except in compliance with the License. A copy of
+# the License is located at
+#
+#     http://aws.amazon.com/apache2.0/
+#
+# or in the "license" file accompanying this file. This file is
+# distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF
+# ANY KIND, either express or implied. See the License for the specific
+# language governing permissions and limitations under the License.
+"""Classes to modify image_uris.retrieve() code to be compatible
+with version 2.0 and later of the SageMaker Python SDK.
+"""
+from __future__ import absolute_import
+
+import ast
+
+from sagemaker.cli.compatibility.v2.modifiers import matching
+from sagemaker.cli.compatibility.v2.modifiers.modifier import Modifier
+
+GET_IMAGE_URI_NAME = "get_image_uri"
+GET_IMAGE_URI_NAMESPACES = ("sagemaker", "sagemaker.amazon_estimator")
+
+
+class ImageURIRetrieveRefactor(Modifier):
+    """A class to refactor *get_image_uri() method."""
+
+    def node_should_be_modified(self, node):
+        """Checks if the ``ast.Call`` node calls a function of interest.
+
+        This looks for the following calls:
+
+        - ``sagemaker.get_image_uri``
+        - ``sagemaker.amazon_estimator.get_image_uri``
+        - ``get_image_uri``
+
+        Args:
+            node (ast.Call): a node that represents a function call. For more,
+                see https://docs.python.org/3/library/ast.html#abstract-grammar.
+
+        Returns:
+            bool: If the ``ast.Call`` instantiates a class of interest.
+        """
+        return matching.matches_name_or_namespaces(node, GET_IMAGE_URI_NAME, GET_IMAGE_URI_NAMESPACES)
+
+    def modify_node(self, node):
+        """Modifies the ``ast.Call`` node to call ``image_uris.retrieve`` instead.
+        And switch the first two parameters from (region, repo) to (framework, region)
+
+        Args:
+            node (ast.Call): a node that represents a *image_uris.retrieve call.
+        """
+        if matching.matches_name(node, GET_IMAGE_URI_NAME):
+            node.func.id = "image_uris.retrieve"
+            node.func.params.argOne, node.func.params.argTwo = node.func.params.argTwo, node.func.params.argOne
+        elif matching.matches_attr(node, GET_IMAGE_URI_NAME):
+            node.func.attr = "image_uris.retrieve"
+            node.func.params.argOne, node.func.params.argTwo = node.func.params.argTwo, node.func.params.argOne
+        return node
+
+
+class ImageURIRetrieveImportFromRenamer(Modifier):
+    """A class to update import statements of ``get_image_uri``."""
+
+    def node_should_be_modified(self, node):
+        """Checks if the import statement imports ``get_image_uri`` from the correct module.
+
+        Args:
+            node (ast.ImportFrom): a node that represents a ``from ... import ... `` statement.
+                For more, see https://docs.python.org/3/library/ast.html#abstract-grammar.
+
+        Returns:
+            bool: If the import statement imports ``get_image_uri`` from the correct module.
+        """
+        return node.module in GET_IMAGE_URI_NAMESPACES and any(
+            name.name == GET_IMAGE_URI_NAMESPACES for name in node.names
+        )
+
+    def modify_node(self, node):
+        """Changes the ``ast.ImportFrom`` node's name from ``get_image_uri`` to ``image_uris``.
+
+        Args:
+            node (ast.ImportFrom): a node that represents a ``from ... import ... `` statement.
+                For more, see https://docs.python.org/3/library/ast.html#abstract-grammar.
+
+        Returns:
+            ast.AST: the original node, which has been potentially modified.
+        """
+        for name in node.names:
+            if name.name == GET_IMAGE_URI_NAME:
+                name.name = "image_uris"
+            if node.module == "sagemaker.amazon_estimator":
+                node.module = "sagemaker"
+        return node
