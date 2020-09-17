e7bfaa119722b2e2b40066b99a58722d672d506a
Edward J. Kim <edward@edwardkim.org>
2020-09-17 00:14:33 +0000

    Add absolute_import to xgboost utils file

--- src/sagemaker/xgboost/utils.py
+++ src/sagemaker/xgboost/utils.py
@@ -11,6 +11,8 @@
 # ANY KIND, either express or implied. See the License for the specific
 # language governing permissions and limitations under the License.
 """Placeholder docstring"""
+from __future__ import absolute_import
+
 from sagemaker.xgboost import defaults
 
 
