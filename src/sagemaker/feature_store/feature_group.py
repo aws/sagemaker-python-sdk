26f043eb3f7e29297f678f1b7d1b38f38db029ad
Rui Wang Napieralski <wru@amazon.com>
2020-12-01 11:13:05 -0800

    fix: use iterrows to iterate pandas dataframe
    itertuples doesn't return named tuples with large number of column(>254)
    with python versions < 3.7. See:
    https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.DataFrame.itertuples.html
    row._asdict() would fail with regular tuples. Switching to iterrows here
    to avoid this limitation.

--- src/sagemaker/feature_store/feature_group.py
+++ src/sagemaker/feature_store/feature_group.py
@@ -179,10 +179,10 @@ class IngestionManagerPandas:
             end_index (int): ending position to ingest in this batch.
         """
         logger.info("Started ingesting index %d to %d", start_index, end_index)
-        for row in data_frame[start_index:end_index].itertuples(index=False):
+        for _, row in data_frame[start_index:end_index].iterrows():
             record = [
                 FeatureValue(feature_name=name, value_as_string=str(value))
-                for name, value in row._asdict().items()
+                for name, value in row.items()
             ]
             sagemaker_session.put_record(
                 feature_group_name=feature_group_name, record=[value.to_dict() for value in record]
