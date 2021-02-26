0578a3d8f282d267cd30c653788d2c677c3b53a7
Yuchen Nie <31755997+ycnie@users.noreply.github.com>
2021-02-25 18:34:00 -0800

    change: add mxnet 1.7.0 eia configuration (#2173)

--- tests/conftest.py
+++ tests/conftest.py
@@ -157,8 +157,16 @@ def mxnet_training_py_version(mxnet_training_version, request):
 
 
 @pytest.fixture(scope="module", params=["py2", "py3"])
-def mxnet_eia_py_version(request):
-    return request.param
+def mxnet_eia_py_version(mxnet_eia_version, request):
+    if Version(mxnet_eia_version) < Version("1.7.0"):
+        return request.param
+    else:
+        return "py3"
+
+
+@pytest.fixture(scope="module")
+def mxnet_eia_latest_py_version():
+    return "py3"
 
 
 @pytest.fixture(scope="module", params=["py2", "py3"])
