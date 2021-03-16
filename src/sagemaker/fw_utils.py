20910a9bee701d325d00c8db91b89cd199f2b852
Indu Thangakrishnan <thangakr@amazon.com>
2021-03-16 14:30:37 -0700

    Add SMDP support for TF 2.3.2

--- src/sagemaker/fw_utils.py
+++ src/sagemaker/fw_utils.py
@@ -59,7 +59,7 @@ SM_DATAPARALLEL_SUPPORTED_INSTANCE_TYPES = (
     "local_gpu",
 )
 SM_DATAPARALLEL_SUPPORTED_FRAMEWORK_VERSIONS = {
-    "tensorflow": ["2.3.0", "2.3.1", "2.4.1"],
+    "tensorflow": ["2.3.0", "2.3.1", "2.3.2", "2.4.1"],
     "pytorch": ["1.6.0", "1.7.1"],
 }
 SMDISTRIBUTED_SUPPORTED_STRATEGIES = ["dataparallel", "modelparallel"]
