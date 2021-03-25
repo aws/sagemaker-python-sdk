d6210d7a26b562348971898475bf5ea41e088f1a
wanyixia <wanyixia@amazon.com>
2021-03-25 16:23:27 -0700

    change:add test for compilation

--- tests/conftest.py
+++ tests/conftest.py
@@ -224,6 +224,31 @@ def neo_pytorch_cpu_instance_type():
 
 
 @pytest.fixture(scope="module")
+def tfs_eia_latest_py_version():
+    return "py3"
+
+
+@pytest.fixture(scope="module")
+def tfs_eia_latest_version():
+    return "2.3"
+
+
+@pytest.fixture(scope="module")
+def tfs_eia_target_device():
+    return "ml_eia2"
+
+
+@pytest.fixture(scope="module")
+def tfs_eia_cpu_instance_type():
+    return "ml.c5.xlarge"
+
+
+@pytest.fixture(scope="module")
+def tfs_eia_compilation_job_name():
+    return utils.name_from_base("tfs-eia-compilation")
+
+    
+@pytest.fixture(scope="module")
 def xgboost_framework_version(xgboost_version):
     if xgboost_version in ("1", "latest"):
         pytest.skip("Skipping XGBoost algorithm version.")
