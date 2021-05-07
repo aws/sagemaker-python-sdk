b2260d227b47cde566204ba4d2f56fa0af15a532
Wang Napieralski <wru@88665a0d5290.ant.amazon.com>
2021-05-07 15:41:55 -0700

    fix: fix get_execution_role on studio

--- src/sagemaker/session.py
+++ src/sagemaker/session.py
@@ -3490,12 +3490,21 @@ class Session(object):  # pylint: disable=too-many-public-methods
         """
         if os.path.exists(NOTEBOOK_METADATA_FILE):
             with open(NOTEBOOK_METADATA_FILE, "rb") as f:
-                instance_name = json.loads(f.read())["ResourceName"]
+                metadata = json.loads(f.read())
+                instance_name = metadata["ResourceName"]
+                domain_id = metadata.get("DomainId")
+                user_profile_name = metadata.get("UserProfileName")
             try:
-                instance_desc = self.sagemaker_client.describe_notebook_instance(
-                    NotebookInstanceName=instance_name
-                )
-                return instance_desc["RoleArn"]
+                if domain_id is None:
+                    instance_desc = self.sagemaker_client.describe_notebook_instance(
+                        NotebookInstanceName=instance_name
+                    )
+                    return instance_desc["RoleArn"]
+                else:
+                    user_profile_desc = self.sagemaker_client.describe_user_profile(
+                        DomainId=domain_id, UserProfileName=user_profile_name
+                    )
+                    return user_profile_desc["UserSettings"]["ExecutionRole"]
             except ClientError:
                 LOGGER.debug(
                     "Couldn't call 'describe_notebook_instance' to get the Role "
