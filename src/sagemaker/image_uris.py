f408bafda19382f8df2917a4d05b3c1b3596bcaa
Ahsan Khan <ahsan.al.zaki@gmail.com>
2021-07-02 12:05:44 -0700

    add image tags

--- src/sagemaker/image_uris.py
+++ src/sagemaker/image_uris.py
@@ -109,6 +109,8 @@ def retrieve(
 
     if _should_auto_select_container_version(instance_type, distribution):
         container_versions = {
+            "tensorflow-2.4-gpu-py37": "cu110-ubuntu18.04-v1",
+            "tensorflow-2.4.1-gpu-py37": "cu110-ubuntu18.04-v1.1",
             "tensorflow-2.3-gpu-py37": "cu110-ubuntu18.04-v3",
             "tensorflow-2.3.1-gpu-py37": "cu110-ubuntu18.04",
             "tensorflow-2.3.2-gpu-py37": "cu110-ubuntu18.04",
@@ -121,6 +123,8 @@ def retrieve(
             "pytorch-1.6.0-gpu-py36": "cu110-ubuntu18.04",
             "pytorch-1.6-gpu-py3": "cu110-ubuntu18.04-v3",
             "pytorch-1.6.0-gpu-py3": "cu110-ubuntu18.04",
+            "pytorch-1.7-gpu-py36": "cu110-ubuntu18.04-v1",
+            "pytorch-1.7.1-gpu-py36": "cu110-ubuntu18.04-v1.0",
         }
         key = "-".join([framework, tag])
         if key in container_versions:
