8e93bde5055a7748c67de966e85958037be4393b
Balaji Veeramani <bveeramani@berkeley.edu>
2021-08-03 13:28:51 -0500

    Appease lint

--- tests/integ/test_serverless.py
+++ tests/integ/test_serverless.py
@@ -12,8 +12,6 @@
 # language governing permissions and limitations under the License.
 from __future__ import absolute_import
 
-import os
-
 import pytest
 
 from sagemaker.serverless import LambdaModel
