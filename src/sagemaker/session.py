d8822ac61cd12114e554ab8cb4b4b498e6490d4b
Tulio Casagrande <tuliocasagrande@gmail.com>
2021-08-10 19:55:43 -0300

    documentation: clarify that default_bucket creates a bucket (#2429)
    Co-authored-by: Ahsan Khan <ahsan.al.zaki@gmail.com>
    Co-authored-by: Shreya Pandit <pandishr@amazon.com>

--- src/sagemaker/session.py
+++ src/sagemaker/session.py
@@ -357,6 +357,8 @@ class Session(object):  # pylint: disable=too-many-public-methods
     def default_bucket(self):
         """Return the name of the default bucket to use in relevant Amazon SageMaker interactions.
 
+        This function will create the s3 bucket if it does not exist.
+
         Returns:
             str: The name of the default bucket, which is of the form:
                 ``sagemaker-{region}-{AWS account ID}``.
