1737f9ac5733ab8c82fb41a2155bdd38fe881b50
Shreya Pandit <pandishr@amazon.com>
2021-08-27 10:34:33 -0700

    update attribute param to be correct

--- src/sagemaker/automl/automl.py
+++ src/sagemaker/automl/automl.py
@@ -420,7 +420,7 @@ class AutoML(object):
             serializer=serializer,
             deserializer=deserializer,
             endpoint_name=endpoint_name,
-            model_kms_key=model_kms_key,
+            kms_key=model_kms_key,
             tags=tags,
             wait=wait,
         )
