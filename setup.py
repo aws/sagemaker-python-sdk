2185415611caa7e4bce539a96a253d8c55b92e16
jmahlik <j38999128+jmahlik@users.noreply.github.com>
2021-09-01 07:40:08 -0500

    fix: local mode do not allow docker-py 5.0.1 due to buffering issue

--- setup.py
+++ setup.py
@@ -51,6 +51,7 @@ extras = {
     "local": [
         "urllib3>=1.21.1,!=1.25,!=1.25.1",
         "docker-compose>=1.25.2",
+        "docker!=5.0.1",
         "PyYAML>=5.3, <6",  # PyYAML version has to match docker-compose requirements
         "psutil",
     ],
