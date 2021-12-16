67bb944c5f9bc4a2ac54abf61851a1d3bc73423e
Qingzi-Lan <qingzila@amazon.com>
2021-12-16 15:05:54 -0800

    format using tox

--- src/sagemaker/fw_utils.py
+++ src/sagemaker/fw_utils.py
@@ -84,7 +84,7 @@ SM_DATAPARALLEL_SUPPORTED_FRAMEWORK_VERSIONS = {
         "1.9",
         "1.9.0",
         "1.9.1",
-        "1.10.0"
+        "1.10.0",
     ],
 }
 SMDISTRIBUTED_SUPPORTED_STRATEGIES = ["dataparallel", "modelparallel"]
