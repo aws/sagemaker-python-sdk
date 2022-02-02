5fa84890a780cf83ba1f34f44508b10cad51be55
Can Karakus <cakarak@amazon.com>
2022-02-02 13:47:43 -0800

    fix: fix mp config yaml file name

--- src/sagemaker/mp_config.py
+++ src/sagemaker/mp_config.py
@@ -174,7 +174,7 @@ class ModelParallelConfig:
 
         # load the schema from yaml file
         dir_path = os.path.dirname(os.path.realpath(__file__))
-        config_path = os.path.join(dir_path, "config.yaml")
+        config_path = os.path.join(dir_path, "mp_config.yaml")
         with open(config_path, "r") as f:
             schema = yaml.safe_load(f)
 
