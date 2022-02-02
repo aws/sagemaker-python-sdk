f94245c364144cbfbcaf18053cab98a0d5aa77b8
Benjamin Crabtree <bencrab@amazon.com>
2022-02-02 15:22:26 +0000

    fix: use request url so that we dont have to pass aws creds

--- doc/conf.py
+++ doc/conf.py
@@ -17,8 +17,7 @@ import pkg_resources
 from datetime import datetime
 import sys
 import os
-from jumpstart.utils import create_jumpstart_model_table
-
+from doc_utils.jumpstart_doc_utils import create_jumpstart_model_table
 sys.path.append(os.path.join(os.path.dirname(__file__), "/doc/"))
 
 project = "sagemaker"
