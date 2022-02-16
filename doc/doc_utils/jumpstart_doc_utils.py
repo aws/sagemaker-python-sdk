86d53c5252ae048684929738c9f89d0e4c8e09e4
Jeniya Tabassum <jeniya.tabassum@gmail.com>
2022-02-16 12:35:28 -0700

    doc: Support for generation of Jumpstart model table on build (#2924)
    Co-authored-by: Mufaddal Rohawala <muffi179@gmail.com>
    Co-authored-by: Ahsan Khan <ahsan.al.zaki@gmail.com>
    Co-authored-by: Mufaddal Rohawala <89424143+mufaddal-rohawala@users.noreply.github.com>
    Co-authored-by: Basil Beirouti <beirb@amazon.com>
    Co-authored-by: Payton Staub <pstaub@amazon.com>
    Co-authored-by: Shreya Pandit <shreya.pandit25@gmail.com>
    Co-authored-by: Basil Beirouti <BasilBeirouti@gmail.com>
    Co-authored-by: Payton Staub <staubhpa@gmail.com>
    Co-authored-by: Mohamed Ali Jamaoui <m.ali.jamaoui@gmail.com>
    Co-authored-by: ci <ci>
    Co-authored-by: Jeniya Tabassum <jeniya.tabassum@gmail.com>
    Co-authored-by: sreedes <70613743+sreedes@users.noreply.github.com>
    Co-authored-by: Navin Soni <navinns@amazon.com>
    Co-authored-by: Miyoung <myoung8739@gmail.com>
    Co-authored-by: Ameen Khan <ameenmk@amazon.com>
    Co-authored-by: Zhankui Lu <zhankuil@amazon.com>
    Co-authored-by: Xiaoguang Chen <68292680+xgchena@users.noreply.github.com>
    Co-authored-by: Jonathan Guinegagne <12092593+JGuinegagne@users.noreply.github.com>
    Co-authored-by: Zhankui Lu <zhankuilv@gmail.com>
    Co-authored-by: Yifei Zhu <66866419+yzhu0@users.noreply.github.com>
    Co-authored-by: Qingzi-Lan <83724147+Qingzi-Lan@users.noreply.github.com>
    Co-authored-by: Navin Soni <navinsoni89@gmail.com>
    Co-authored-by: marckarp <karpmar@8c859028ba7e.ant.amazon.com>
    Co-authored-by: chenxy <chenxy@amazon.com>
    Co-authored-by: Xinghan Chen <47259301+xchen909@users.noreply.github.com>
    Co-authored-by: Tulio Casagrande <tuliocasagrande@gmail.com>
    Co-authored-by: jerrypeng7773 <50377760+jerrypeng7773@users.noreply.github.com>
    Co-authored-by: marckarp <mkarpmar@gmail.com>
    Co-authored-by: jayatalr <69013381+jayatalr@users.noreply.github.com>
    Co-authored-by: bhaoz <96764005+bhaoz@users.noreply.github.com>
    Co-authored-by: Ethan Cheng <shouhc@amazon.com>
    Co-authored-by: Xiaoguang Chen <xgchen@amazon.com>
    Co-authored-by: keerthanvasist <kvasist@amazon.com>
    Co-authored-by: Shreya Pandit <pandishr@amazon.com>
    Co-authored-by: Ivy Bazan <nbbazan@amazon.com>
    Co-authored-by: IvyBazan <45951687+IvyBazan@users.noreply.github.com>
    Co-authored-by: Benjamin Crabtree <bencrab@amazon.com>
    Co-authored-by: iasoon <ilion.beyst@gmail.com>
    Co-authored-by: evakravi <69981223+evakravi@users.noreply.github.com>

---
+++ doc/doc_utils/jumpstart_doc_utils.py
@@ -0,0 +1,89 @@
+# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"). You
+# may not use this file except in compliance with the License. A copy of
+# the License is located at
+#
+#     http://aws.amazon.com/apache2.0/
+#
+# or in the "license" file accompanying this file. This file is
+# distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF
+# ANY KIND, either express or implied. See the License for the specific
+# language governing permissions and limitations under the License.
+from __future__ import absolute_import
+from urllib import request
+import json
+from packaging.version import Version
+
+JUMPSTART_REGION = "eu-west-2"
+SDK_MANIFEST_FILE = "models_manifest.json"
+JUMPSTART_BUCKET_BASE_URL = "https://jumpstart-cache-prod-{}.s3.{}.amazonaws.com".format(
+    JUMPSTART_REGION, JUMPSTART_REGION
+)
+
+
+def get_jumpstart_sdk_manifest():
+    url = "{}/{}".format(JUMPSTART_BUCKET_BASE_URL, SDK_MANIFEST_FILE)
+    with request.urlopen(url) as f:
+        models_manifest = f.read().decode("utf-8")
+    return json.loads(models_manifest)
+
+
+def get_jumpstart_sdk_spec(key):
+    url = "{}/{}".format(JUMPSTART_BUCKET_BASE_URL, key)
+    with request.urlopen(url) as f:
+        model_spec = f.read().decode("utf-8")
+    return json.loads(model_spec)
+
+
+def create_jumpstart_model_table():
+    sdk_manifest = get_jumpstart_sdk_manifest()
+    sdk_manifest_top_versions_for_models = {}
+
+    for model in sdk_manifest:
+        if model["model_id"] not in sdk_manifest_top_versions_for_models:
+            sdk_manifest_top_versions_for_models[model["model_id"]] = model
+        else:
+            if Version(
+                sdk_manifest_top_versions_for_models[model["model_id"]]["version"]
+            ) < Version(model["version"]):
+                sdk_manifest_top_versions_for_models[model["model_id"]] = model
+
+    file_content = []
+
+    file_content.append("==================================\n")
+    file_content.append("JumpStart Available Model Table\n")
+    file_content.append("==================================\n")
+    file_content.append(
+        """
+    JumpStart for the SageMaker Python SDK uses model ids and model versions to access the necessary
+    utilities. This table serves to provide the core material plus some extra information that can be useful
+    in selecting the correct model id and corresponding parameters.\n
+    """
+    )
+    file_content.append(
+        """
+    If you want to automatically use the latest version of the model, use "*" for the `model_version` attribute.
+    We highly suggest pinning an exact model version however.\n
+    """
+    )
+    file_content.append("\n")
+    file_content.append(".. list-table:: Available Models\n")
+    file_content.append("   :widths: 50 20 20 20\n")
+    file_content.append("   :header-rows: 1\n")
+    file_content.append("   :class: datatable\n")
+    file_content.append("\n")
+    file_content.append("   * - Model ID\n")
+    file_content.append("     - Fine Tunable?\n")
+    file_content.append("     - Latest Version\n")
+    file_content.append("     - Min SDK Version\n")
+
+    for model in sorted(sdk_manifest, key=lambda elt: elt["model_id"]):
+        model_spec = get_jumpstart_sdk_spec(model["spec_key"])
+        file_content.append("   * - {}\n".format(model["model_id"]))
+        file_content.append("     - {}\n".format(model_spec["training_supported"]))
+        file_content.append("     - {}\n".format(model["version"]))
+        file_content.append("     - {}\n".format(model["min_version"]))
+
+    f = open("doc_utils/jumpstart.rst", "w")
+    f.writelines(file_content)
