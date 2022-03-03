2dfde229ff6f0789c46cf628e220e57077aec4e7
Payton Staub <pstaub@amazon.com>
2022-03-02 16:04:30 -0800

    Add property check

--- src/sagemaker/workflow/steps.py
+++ src/sagemaker/workflow/steps.py
@@ -301,7 +301,8 @@ class TrainingStep(ConfigurableRetryStep):
         )
         request_dict = self.estimator.sagemaker_session._get_train_request(**train_args)
         request_dict.pop("TrainingJobName")
-        request_dict["HyperParameters"].pop("sagemaker_job_name", None)
+        if "HyperParameters" in request_dict:
+            request_dict["HyperParameters"].pop("sagemaker_job_name", None)
 
         return request_dict
 
