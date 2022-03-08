51115512676755bc7635311181491460f81a3f76
Ahsan Khan <ahsan.al.zaki@gmail.com>
2022-03-07 17:28:35 -0800

    feat: Python 3.9 for readthedocs (#2973)
    Co-authored-by: Ben Crabtree <bencrab@amazon.com>
    Co-authored-by: Navin Soni <navinsoni89@gmail.com>
    Co-authored-by: Jeniya Tabassum <jeniya.tabassum@gmail.com>
    Co-authored-by: Dewen Qi <qidewen@amazon.com>
    Co-authored-by: Payton Staub <pstaub@amazon.com>
    Co-authored-by: qidewenwhen <32910701+qidewenwhen@users.noreply.github.com>
    Co-authored-by: Qingzi-Lan <83724147+Qingzi-Lan@users.noreply.github.com>
    Co-authored-by: Payton Staub <staubhpa@gmail.com>

--- doc/conf.py
+++ doc/conf.py
@@ -10,7 +10,7 @@
 # distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF
 # ANY KIND, either express or implied. See the License for the specific
 # language governing permissions and limitations under the License.
-"""Placeholder docstring"""
+"""Configuration for generating readthedocs docstrings."""
 from __future__ import absolute_import
 
 import pkg_resources
