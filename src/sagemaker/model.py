223d4deabcd10aed5a27bbd74949c270c8ab22c0
Qingzi-Lan <qingzila@amazon.com>
2022-03-16 10:53:50 -0700

    update

--- src/sagemaker/model.py
+++ src/sagemaker/model.py
@@ -622,7 +622,7 @@ class Model(ModelBase):
         if (
             framework.lower() == "pytorch"
             or framework.lower() == "tensorflow"
-            and re.match("(?=^ml_)(?!ml_inf)", target_instance_type) is not None
+            and re.match("(?=^ml_)(?!ml_inf)", str(target_instance_type)) is not None
             and framework_version is not None
         ):
             input_model_config["FrameworkVersion"] = utils.get_short_version(framework_version)
