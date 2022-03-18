ea86e37f77eeb800a1b8db94b1f5ed7fef5c3698
Qingzi-Lan <83724147+Qingzi-Lan@users.noreply.github.com>
2022-03-17 18:01:34 -0700

    feature: TF242 ioc support (#2982)
    Co-authored-by: Mufaddal Rohawala <89424143+mufaddal-rohawala@users.noreply.github.com>

--- src/sagemaker/model.py
+++ src/sagemaker/model.py
@@ -630,6 +630,8 @@ class Model(ModelBase):
 
         if (
             framework.lower() == "pytorch"
+            or framework.lower() == "tensorflow"
+            and target_instance_type is not None
             and re.match("(?=^ml_)(?!ml_inf)", target_instance_type) is not None
             and framework_version is not None
         ):
