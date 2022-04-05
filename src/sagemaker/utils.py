85dc3de436310dae8b0ca37fdd177ba3bfc5b26e
Sergey Ermolin <ermolins@amazon.com>
2022-04-05 16:50:58 -0700

    fixed comment line by running  -m tox -e black-format

--- src/sagemaker/utils.py
+++ src/sagemaker/utils.py
@@ -83,7 +83,7 @@ def name_from_base(base, max_length=63, short=False):
 
 def unique_name_from_base(base, max_length=63):
     """Placeholder Docstring"""
-    random.seed(int(uuid.uuid4())) #using uuid to randomize, otherwise system timestamp is used. 
+    random.seed(int(uuid.uuid4()))  # using uuid to randomize, otherwise system timestamp is used.
     unique = "%04x" % random.randrange(16**4)  # 4-digit hex
     ts = str(int(time.time()))
     available_length = max_length - 2 - len(ts) - len(unique)
