daf42d2be5630f2eec3cf4f35ee76d7965a80fce
Basil Beirouti <beirb@amazon.com>
2022-04-25 12:44:52 -0700

    formatting fixes

--- src/sagemaker/session.py
+++ src/sagemaker/session.py
@@ -2801,7 +2801,7 @@ class Session(object):  # pylint: disable=too-many-public-methods
         description=None,
         drift_check_baselines=None,
         customer_metadata_properties=None,
-        validation_specification=None
+        validation_specification=None,
     ):
         """Get request dictionary for CreateModelPackage API.
 
@@ -2847,7 +2847,7 @@ class Session(object):  # pylint: disable=too-many-public-methods
             description,
             drift_check_baselines=drift_check_baselines,
             customer_metadata_properties=customer_metadata_properties,
-            validation_specification=validation_specification
+            validation_specification=validation_specification,
         )
         if model_package_group_name is not None:
             try:
@@ -4208,7 +4208,7 @@ def get_model_package_args(
     container_def_list=None,
     drift_check_baselines=None,
     customer_metadata_properties=None,
-    validation_specification=None
+    validation_specification=None,
 ):
     """Get arguments for create_model_package method.
 
@@ -4299,7 +4299,7 @@ def get_create_model_package_request(
     tags=None,
     drift_check_baselines=None,
     customer_metadata_properties=None,
-    validation_specification=None
+    validation_specification=None,
 ):
     """Get request dictionary for CreateModelPackage API.
 
