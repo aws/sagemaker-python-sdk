6245be8af7e02b73ad96efb25adedc439d611d91
Mufaddal Rohawala <89424143+mufaddal-rohawala@users.noreply.github.com>
2022-05-06 10:14:36 -0700

    fix: repack model locally when local_code local mode (#3094)

--- src/sagemaker/model.py
+++ src/sagemaker/model.py
@@ -456,9 +456,14 @@ class Model(ModelBase):
             if is_pipeline_variable(self.model_data):
                 # model is not yet there, defer repacking to later during pipeline execution
                 return
-
-            bucket = self.bucket or self.sagemaker_session.default_bucket()
-            repacked_model_data = "s3://" + "/".join([bucket, key_prefix, "model.tar.gz"])
+            if local_code and self.model_data.startswith("file://"):
+                repacked_model_data = self.model_data
+            else:
+                bucket = self.bucket or self.sagemaker_session.default_bucket()
+                repacked_model_data = "s3://" + "/".join([bucket, key_prefix, "model.tar.gz"])
+                self.uploaded_code = fw_utils.UploadedCode(
+                    s3_prefix=repacked_model_data, script_name=os.path.basename(self.entry_point)
+                )
 
             utils.repack_model(
                 inference_script=self.entry_point,
@@ -471,9 +476,6 @@ class Model(ModelBase):
             )
 
             self.repacked_model_data = repacked_model_data
-            self.uploaded_code = fw_utils.UploadedCode(
-                s3_prefix=self.repacked_model_data, script_name=os.path.basename(self.entry_point)
-            )
 
     def _script_mode_env_vars(self):
         """Returns a mapping of environment variables for script mode execution"""
