6bf4402096a84a20cec6d9814434a46db5c7e690
teticio <teticio@gmail.com>
2022-05-25 20:41:59 +0100

    bug: local_gpu gives error Unknown runtime specified nvidia

--- src/sagemaker/local/image.py
+++ src/sagemaker/local/image.py
@@ -746,7 +746,6 @@ class _SageMakerContainer(object):
             "volumes": [v.map for v in optml_volumes],
             "environment": environment,
             "networks": {"sagemaker-local": {"aliases": [host]}},
-            "deploy": {"resources": {"reservations": {"devices": [{"capabilities": ["gpu"]}]}}},
         }
 
         if command != "process":
@@ -760,7 +759,7 @@ class _SageMakerContainer(object):
         # for GPU support pass in nvidia as the runtime, this is equivalent
         # to setting --runtime=nvidia in the docker commandline.
         if self.instance_type == "local_gpu":
-            host_config["runtime"] = "nvidia"
+            host_config["deploy"] = {"resources": {"reservations": {"devices": [{"capabilities": ["gpu"]}]}}}
 
         if command == "serve":
             serving_port = (
