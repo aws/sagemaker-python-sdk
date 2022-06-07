239d6392a87e25b84993794af9bccec8919eb238
Loki <lokravi@amazon.com>
2022-06-06 23:18:57 -0700

    fix: syntax error in TF trcomp

--- src/sagemaker/tensorflow/training_compiler/config.py
+++ src/sagemaker/tensorflow/training_compiler/config.py
@@ -25,7 +25,7 @@ class TrainingCompilerConfig(BaseConfig):
     """The SageMaker Training Compiler configuration class."""
 
     SUPPORTED_INSTANCE_CLASS_PREFIXES = ["p3", "g4dn", "p4", "g5"]
-    MIN_SUPPORTED_VERSION = version.parse("2.9")
+    MIN_SUPPORTED_VERSION = "2.9.1"
 
     def __init__(
         self,
