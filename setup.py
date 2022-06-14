797db75c5a9efb0109d16747b5de723effd185f8
Mufaddal Rohawala <muffi179@gmail.com>
2022-06-13 19:41:04 -0700

    feature: support python 3.10

--- setup.py
+++ setup.py
@@ -95,6 +95,7 @@ setup(
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
     ],
     install_requires=required_packages,
     extras_require=extras,
