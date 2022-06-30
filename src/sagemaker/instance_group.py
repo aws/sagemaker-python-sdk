980a4b9b41d719948c6cd5c77052730be2e3dc4a
Miyoung Choi <cmiyoung@amazon.com>
2022-06-30 15:03:27 -0700

    documentation: documentation for heterogeneous cluster

--- src/sagemaker/instance_group.py
+++ src/sagemaker/instance_group.py
@@ -10,15 +10,12 @@
 # distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF
 # ANY KIND, either express or implied. See the License for the specific
 # language governing permissions and limitations under the License.
-"""This file defines instance group for heterogeneous cluster."""
+"""Defines the InstanceGroup class that configures a heterogeneous cluster."""
 from __future__ import absolute_import
 
 
 class InstanceGroup(object):
-    """Accepts instance group parameters for conversion to request dict.
-
-    The `_to_request_dict` provides a method to turn the parameters into a dict.
-    """
+    """The class to create instance groups for a heterogeneous cluster."""
 
     def __init__(
         self,
@@ -26,16 +23,30 @@ class InstanceGroup(object):
         instance_type=None,
         instance_count=None,
     ):
-        """Initialize a ``InstanceGroup`` instance.
+        """It initializes an ``InstanceGroup`` instance.
 
-        InstanceGroup accepts instance group parameters and provides a method to turn
-        these parameters into a dictionary.
+        You can create instance group object of the ``InstanceGroup`` class
+        by specifying the instance group configuration arguments.
+
+        For instructions on how to use InstanceGroup objects
+        to configure a heterogeneous cluster
+        through the SageMaker generic and framework estimator classes, see
+        `Train Using a Heterogeneous Cluster
+        <https://docs.aws.amazon.com/sagemaker/latest/dg/train-heterogeneous-cluster.html>`_
+        in the *Amazon SageMaker developer guide*.
 
         Args:
-            instance_group_name (str): Name of the instance group.
-            instance_type (str): Type of EC2 instance to use in the instance group,
-                for example, 'ml.c4.xlarge'.
-            instance_count (int): Number of EC2 instances to use in the instance group.
+            instance_group_name (str): The name of the instance group.
+            instance_type (str): The instance type to use in the instance group.
+            instance_count (int): The number of instances to use in the instance group.
+
+                .. tip::
+
+                    For more information about available values for the arguments,
+                    see `InstanceGroup
+                    <https://docs.aws.amazon.com/sagemaker/latest/APIReference/API_InstanceGroup.html>`_
+                    API in the `Amazon SageMaker API reference`.
+
         """
         self.instance_group_name = instance_group_name
         self.instance_type = instance_type
