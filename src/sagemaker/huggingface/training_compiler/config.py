1db9ed088471261447a103febb147c4fabf8f3ef
Loki <lokeshwaran94@gmail.com>
2022-08-22 16:52:09 -0700

    Update src/sagemaker/huggingface/training_compiler/config.py
    Co-authored-by: Miyoung <myoung8739@gmail.com>

--- src/sagemaker/huggingface/training_compiler/config.py
+++ src/sagemaker/huggingface/training_compiler/config.py
@@ -123,7 +123,7 @@ class TrainingCompilerConfig(BaseConfig):
                     if Version(estimator.pytorch_version) in SpecifierSet("< 1.11"):
                         error_helper_string = (
                             "Distribution mechanism 'pytorchxla' is currently only supported for "
-                            "PyTorch >= 1.11 when Training Compiler is enabled. Received "
+                            "PyTorch >= 1.11 when SageMaker Training Compiler is enabled. Received "
                             "pytorch_version={} which is unsupported."
                         )
                         raise ValueError(error_helper_string.format(estimator.pytorch_version))
