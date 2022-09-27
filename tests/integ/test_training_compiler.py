cf5cfa00d466a22d6ada763cc35a387dd862eabb
Bruce Zhang <eitug@amazon.com>
2022-09-27 16:48:13 -0700

    change tf test version to fit tf version

--- tests/integ/test_training_compiler.py
+++ tests/integ/test_training_compiler.py
@@ -243,7 +243,7 @@ def test_tensorflow(
             py_version="py39",
             git_config={
                 "repo": "https://github.com/tensorflow/models.git",
-                "branch": "v2.9.2",
+                "branch": tensorflow_training_latest_version,
             },
             source_dir=".",
             entry_point="official/vision/train.py",
