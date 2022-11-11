8ac08946549ec879199e0274e9c6e3d8fe34c103
tejaschumbalkar <tejaschumbalkar@gmail.com>
2022-11-11 10:22:38 -0800

    address comments

--- src/sagemaker/fw_utils.py
+++ src/sagemaker/fw_utils.py
@@ -122,6 +122,7 @@ SM_DATAPARALLEL_SUPPORTED_FRAMEWORK_VERSIONS = {
         "1.11",
         "1.11.0",
         "1.12",
+        "1.12.0",
         "1.12.1",
     ],
 }
@@ -133,6 +134,7 @@ PYTORCHDDP_SUPPORTED_FRAMEWORK_VERSIONS = [
     "1.11",
     "1.11.0",
     "1.12",
+    "1.12.0",
     "1.12.1",
 ]
 
