ac5471f3553c30cb2fc01e944f70a08fb7d96769
Mike Schneider <xncqr@amazon.com>
2023-01-24 22:14:30 +0000

    remote 1.13 and only use 1.13.1 in test_fw_utils

--- tests/unit/test_fw_utils.py
+++ tests/unit/test_fw_utils.py
@@ -912,7 +912,6 @@ def test_validate_smdataparallel_args_not_raises():
         ("ml.p3.16xlarge", "pytorch", "1.12.0", "py38", smdataparallel_enabled),
         ("ml.p3.16xlarge", "pytorch", "1.12.1", "py38", smdataparallel_enabled),
         ("ml.p3.16xlarge", "pytorch", "1.12", "py38", smdataparallel_enabled),
-        ("ml.p3.16xlarge", "pytorch", "1.13", "py39", smdataparallel_enabled),
         ("ml.p3.16xlarge", "pytorch", "1.13.1", "py39", smdataparallel_enabled),
         ("ml.p3.16xlarge", "tensorflow", "2.4.1", "py3", smdataparallel_enabled_custom_mpi),
         ("ml.p3.16xlarge", "tensorflow", "2.4.1", "py37", smdataparallel_enabled_custom_mpi),
