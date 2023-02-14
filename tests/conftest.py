9a527b7f795e68c12e68895386d254b878b8391d
Loki <lokravi@amazon.com>
2023-02-14 03:41:33 +0000

    Removing unused fixtures

--- tests/conftest.py
+++ tests/conftest.py
@@ -602,22 +602,6 @@ def inf_instance_family(inf_instance_type):
     return "_".join(inf_instance_type.split(".")[0:2])
 
 
-@pytest.fixture(scope="session")
-def imagenet_train_set(request, sagemaker_session, tmpdir_factory):
-    """
-    Copies the Imagenet dataset from the bucket it's hosted in to the local bucket in the test region.
-    Due to licensing issues, access to this dataset is controlled through an allowlist
-    """
-    local_path = tmpdir_factory.mktemp("imagenet_tfrecords_train_set")
-    sagemaker_session.download_data(
-        path=local_path, bucket="collection-of-ml-datasets", key_prefix="Imagenet/TFRecords/train"
-    )
-    train_input = sagemaker_session.upload_data(
-        path=local_path, key_prefix="integ-test-data/imagenet/TFRecords/train"
-    )
-    return train_input
-
-
 def pytest_generate_tests(metafunc):
     if "instance_type" in metafunc.fixturenames:
         boto_config = metafunc.config.getoption("--boto-config")
