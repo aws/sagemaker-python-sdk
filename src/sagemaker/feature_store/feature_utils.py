9eb872e23f67030c459167ce2833a0963238ea3e
JoseJuan98 <josejuan-98@hotmail.com>
2023-03-10 23:07:41 +0100

    fix: docstyle

--- src/sagemaker/feature_store/feature_utils.py
+++ src/sagemaker/feature_store/feature_utils.py
@@ -91,8 +91,7 @@ def get_feature_group_as_dataframe(
     verbose: bool = True,
     **kwargs,
 ) -> DataFrame:
-    """Get data from a :class:`sagemaker.feature_store.feature_group.FeatureGroup` as
-    a :class:`pandas.DataFrame`
+    """:class:`sagemaker.feature_store.feature_group.FeatureGroup` as :class:`pandas.DataFrame`
 
     Examples:
         >>> from sagemaker.feature_store.feature_utils import get_feature_group_as_dataframe
