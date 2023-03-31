d81a30506af9fcfa87529f8002f3ce7be79f2da5
evakravi <69981223+evakravi@users.noreply.github.com>
2023-03-31 04:21:01 -0400

    feat: jumpstart instance types (#3686)

--- src/sagemaker/hyperparameters.py
+++ src/sagemaker/hyperparameters.py
@@ -30,6 +30,8 @@ def retrieve_default(
     model_id=None,
     model_version=None,
     include_container_hyperparameters=False,
+    tolerate_vulnerable_model: bool = False,
+    tolerate_deprecated_model: bool = False,
 ) -> Dict[str, str]:
     """Retrieves the default training hyperparameters for the model matching the given arguments.
 
@@ -47,6 +49,13 @@ def retrieve_default(
             that indicates the entrypoint script to use. These hyperparameters may be required
             when creating a training job with boto3, however the ``Estimator`` classes
             add required container hyperparameters to the job. (Default: False).
+        tolerate_vulnerable_model (bool): True if vulnerable versions of model
+            specifications should be tolerated (exception not raised). If False, raises an
+            exception if the script used by this version of the model has dependencies with known
+            security vulnerabilities. (Default: False).
+        tolerate_deprecated_model (bool): True if deprecated models should be tolerated
+            (exception not raised). False if these models should raise an exception.
+            (Default: False).
     Returns:
         dict: The hyperparameters to use for the model.
 
@@ -59,7 +68,12 @@ def retrieve_default(
         )
 
     return artifacts._retrieve_default_hyperparameters(
-        model_id, model_version, region, include_container_hyperparameters
+        model_id,
+        model_version,
+        region,
+        include_container_hyperparameters,
+        tolerate_vulnerable_model,
+        tolerate_deprecated_model,
     )
 
 
@@ -68,7 +82,7 @@ def validate(
     model_id: Optional[str] = None,
     model_version: Optional[str] = None,
     hyperparameters: Optional[dict] = None,
-    validation_mode: Optional[HyperparameterValidationMode] = None,
+    validation_mode: HyperparameterValidationMode = HyperparameterValidationMode.VALIDATE_PROVIDED,
 ) -> None:
     """Validates hyperparameters for models.
 
@@ -98,6 +112,9 @@ def validate(
         raise ValueError(
             "Must specify `model_id` and `model_version` when validating hyperparameters."
         )
+
+    if model_id is None or model_version is None:
+        raise RuntimeError("Model id and version must both be non-None")
 
     if hyperparameters is None:
         raise ValueError("Must specify hyperparameters.")
