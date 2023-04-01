68edb384c5da523f2ff1321561f96ca32cbaa4df
Philipp Schmid <32632186+philschmid@users.noreply.github.com>
2023-04-01 10:59:29 +0200

    Update test_huggingface_torch_distributed.py

--- tests/integ/test_huggingface_torch_distributed.py
+++ tests/integ/test_huggingface_torch_distributed.py
@@ -15,7 +15,7 @@ from __future__ import absolute_import
 import os
 import pytest
 import sagemaker.utils
-from sagemaker.pytorch import HuggingFace
+from sagemaker.huggingface import HuggingFace
 from tests.integ import timeout
 from tests.integ import DATA_DIR, TRAINING_DEFAULT_TIMEOUT_MINUTES
 from tests.integ.timeout import timeout, timeout_and_delete_endpoint_by_name
