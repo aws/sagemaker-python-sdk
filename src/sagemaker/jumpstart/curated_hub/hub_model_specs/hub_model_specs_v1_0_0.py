fe60c435dc6be4811d3f20165a7617d3c151534f
whittech1 <97195892+whittech1@users.noreply.github.com>
2023-07-05 22:37:49 -0400

    flesh out translation logic

--- src/sagemaker/jumpstart/curated_hub/hub_model_specs/hub_model_specs_v1_0_0.py
+++ src/sagemaker/jumpstart/curated_hub/hub_model_specs/hub_model_specs_v1_0_0.py
@@ -1,45 +1,53 @@
-
+from dataclasses import dataclass
 from typing import Any, Dict, Union, Optional, List, Mapping
+
 
 @dataclass
 class Origin:
-      OriginTrainingJobArn: Optional[str]
-      OriginEndpointArn: Optional[str]
+    OriginTrainingJobArn: Optional[str]
+    OriginEndpointArn: Optional[str]
+
 
 @dataclass
 class Dependencies:
     DependencyOriginPath: str
     DependencyCopyPath: str
-    DependencyType: str #enum?
+    DependencyType: str  # enum?
+
 
 @dataclass
 class DatasetConfig:
-    TrainingDatasetLocation: Optional[str] # S3 validation?
-    ValidationDatasetLocation: Optional[str] # S3 validation?
-    DataFormatLocation: Optional[str] # S3 validation?
+    TrainingDatasetLocation: Optional[str]  # S3 validation?
+    ValidationDatasetLocation: Optional[str]  # S3 validation?
+    DataFormatLocation: Optional[str]  # S3 validation?
     PredictColumn: Optional[str]
+
 
 @dataclass
 class Metric:
     Name: str
     Regex: str
 
+
 @dataclass
 class SdkEstimatorArgs:
-        EntryPoint: Optional[str]
-        EnableNetworkIsolation: Optional[bool]
-        Environment: Mapping[str, str]
-        Metrics: List[Metric]
-        OutputPath: Optional[str]
+    EntryPoint: Optional[str]
+    EnableNetworkIsolation: Optional[bool]
+    Environment: Mapping[str, str]
+    Metrics: List[Metric]
+    OutputPath: Optional[str]
+
 
 @dataclass
 class SdkArgs:
-      MinSdkVersion: Optional[str]
-      SdkEstimatorArgs: Optional[SdkEstimatorArgs]
+    MinSdkVersion: Optional[str]
+    SdkEstimatorArgs: Optional[SdkEstimatorArgs]
+
 
 @dataclass
 class CustomImageConfig:
-    ImageLocation: str # ecr address
+    ImageLocation: str  # ecr address
+
 
 @dataclass
 class FrameworkImageConfig:
@@ -49,50 +57,58 @@ class FrameworkImageConfig:
     TransformersVersion: Optional[str]
     BaseFramework: Optional[str]
 
+
 @dataclass
 class ModelArtifactConfig:
-    ArtifactLocation: str # s3 address
+    ArtifactLocation: str  # s3 address
+
 
 @dataclass
-class ScriptConfig: 
-    ScriptLocation: str #s3 address
+class ScriptConfig:
+    ScriptLocation: str  # s3 address
+
 
 @dataclass
-class InstanceConfig: 
-            DefaultInstanceType: str
-            InstanceTypeOptions: List[str]
+class InstanceConfig:
+    DefaultInstanceType: str
+    InstanceTypeOptions: List[str]
+
 
 @dataclass
 class Hyperparameters:
-            Name: str
-            DefaultValue: Optional[str]
-            Type: str #enum
-            Options: List[Optional[str]]
-            Label: Optional[str]
-            Description: Optional[str]
-            Regex: Optional[str]
-            Min: Optional[str]
-            Max: Optional[str]
+    Name: str
+    DefaultValue: Optional[str]
+    Type: str  # enum
+    Options: List[Optional[str]]
+    Label: Optional[str]
+    Description: Optional[str]
+    Regex: Optional[str]
+    Min: Optional[str]
+    Max: Optional[str]
+
 
 @dataclass
 class ExtraChannels:
     ChannelName: str
-    ChannelDataLocation: str # s3 uri (prefix or key)
+    ChannelDataLocation: str  # s3 uri (prefix or key)
+
 
 @dataclass
 class DefaultTrainingConfig:
-        SdkArgs: Optional[SdkArgs]
-        CustomImageConfig: Optional[CustomImageConfig]
-        FrameworkImageConfig: Optional[FrameworkImageConfig]
-        ModelArtifactConfig: Optional[ModelArtifactConfig]
-        ScriptConfig: Optional[ScriptConfig]
-        InstanceConfig: Optional[InstanceConfig]
-        Hyperparameters: List[Hyperparameters]
-        ExtraChannels: List[ExtraChannels]
+    SdkArgs: Optional[SdkArgs]
+    CustomImageConfig: Optional[CustomImageConfig]
+    FrameworkImageConfig: Optional[FrameworkImageConfig]
+    ModelArtifactConfig: Optional[ModelArtifactConfig]
+    ScriptConfig: Optional[ScriptConfig]
+    InstanceConfig: Optional[InstanceConfig]
+    Hyperparameters: List[Hyperparameters]
+    ExtraChannels: List[ExtraChannels]
+
 
 @dataclass
 class InferenceNotebookConfig:
-      NotebookLocation: str # s3 uri,
+    NotebookLocation: str  # s3 uri,
+
 
 @dataclass
 class DefaultDeploymentConfig:
@@ -104,9 +120,10 @@ class DefaultDeploymentConfig:
     InstanceConfig: Optional[InstanceConfig]
     InferenceNotebookConfig: Optional[InferenceNotebookConfig]
 
+
 @dataclass
 class HubModelSpec_v1_0_0:
-    capabilities: List[str] #enum?
+    capabilities: List[str]  # enum?
     DataType: str
     MlTask: str
     Framework: Optional[str]
@@ -115,4 +132,3 @@ class HubModelSpec_v1_0_0:
     DatasetConfig: Optional[DatasetConfig]
     DefaultTrainingConfig: Optional[DefaultTrainingConfig]
     DefaultDeploymentConfig: Optional[DefaultDeploymentConfig]
-    }
--- src/sagemaker/jumpstart/curated_hub/jumpstart_curated_public_hub.py
+++ src/sagemaker/jumpstart/curated_hub/jumpstart_curated_public_hub.py
@@ -1,24 +1,29 @@
+import json
+from dataclasses import dataclass
+from typing import List
 
-from typing import Any, Dict, Union, Optional, List
-from sagemaker.session import Session
 import boto3
-from sagemaker.jumpstart.utils import (
-    verify_model_region_and_return_specs,
-)
+
+from sagemaker.jumpstart.curated_hub.hub_model_specs.hub_model_specs_v1_0_0 import HubModelSpec_v1_0_0, \
+    DefaultDeploymentConfig, SdkArgs, FrameworkImageConfig, ModelArtifactConfig, ScriptConfig, \
+    InstanceConfig, InferenceNotebookConfig
 from sagemaker.jumpstart.enums import (
     JumpStartScriptScope,
 )
 from sagemaker.jumpstart.types import (
-    JumpStartModelHeader,
     JumpStartModelSpecs,
-    JumpStartVersionedModelId,
 )
-from dataclasses import dataclass
+from sagemaker.jumpstart.utils import (
+    verify_model_region_and_return_specs,
+)
+from sagemaker.session import Session
+
 
 @dataclass
-class JumpStartModel:
+class PublicModelId:
     id: str
     version: str
+
 
 class JumpStartCuratedPublicHub:
     """JumpStartCuratedPublicHub class.
@@ -27,27 +32,41 @@ class JumpStartCuratedPublicHub:
     """
 
     def __init__(self, curated_hub_s3_prefix: str):
-      self.curated_hub_s3_prefix = curated_hub_s3_prefix
+        self.curated_hub_s3_prefix = curated_hub_s3_prefix
+        self.curated_hub_name = f"{curated_hub_s3_prefix}-sagemaker-testing"  # TODO verify name
 
-      self._region = "us-west-2"
-      self._s3_client = boto3.client("s3", region_name=self._region)
-      self._sagemaker_session = Session()
+        self._region = "us-west-2"
+        self._s3_client = boto3.client("s3", region_name=self._region)
+        self._sm_client = boto3.client("sagemaker", region_name=self._region)
+        self._sagemaker_session = Session()
 
     def create(self):
-       """
+        """
        Workflow:
        1. Verifications
         a) S3 location exists
         b) if not creates one
        2. CreateHub
        """
-       
+        # TODO chrstfu@ if you could take this piece please
+        self._s3_client.create_bucket(bucket=self.curated_hub_name)  # TODO create bucket if not exists, verify args
 
+        hub_bucket_s3_uri = f"s3://{self.curated_hub_name}"
+        response = self._sm_client.create_hub(
+            HubName=self.curated_hub_name,
+            HubDescription="This is a curated hub.",  # TODO verify description
+            HubDisplayName=self.curated_hub_name,  # TODO verify display name
+            HubSearchKeywords=[],
+            S3StorageConfig={
+                'S3OutputPath': hub_bucket_s3_uri,
+            },
+            Tags=[]
+        )
 
-    def import_models(self, model_ids: List[JumpStartModel]):
-       for model_id in model_ids:
-        self.import_model(model_id)
-       """
+    def import_models(self, model_ids: List[PublicModelId]):
+        for model_id in model_ids:
+            self.import_model(model_id)
+        """
        Workflow:
        1. Fo reach model_id in list
        2. Pull model metadata
@@ -62,14 +81,96 @@ class JumpStartCuratedPublicHub:
         a) Checks if model ID already exists in Hub
        6. 
        """
-      
-       
 
-    def import_model(self, jumpstart_model: JumpStartModel) -> JumpStartModelSpecs:
-       model_spec = verify_model_region_and_return_specs(
-          model_id=jumpstart_model.id,
-          version=jumpstart_model.version,
-          scope=JumpStartScriptScope.INFERENCE,
-          region=self._region
-      )
-       
+    def import_model(self, public_js_model: PublicModelId) -> None:
+        model_specs = verify_model_region_and_return_specs(
+            model_id=public_js_model.id,
+            version=public_js_model.version,
+            scope=JumpStartScriptScope.INFERENCE,
+            region=self._region
+        )
+
+        # TODO verify ideal naming (not urgent)
+        hub_content_name = f"{model_specs.model_id}-copy"
+
+        # TODO Several fields are not present in SDK specs as they are only in Studio specs right now (not urgent)
+        hub_content_display_name = model_specs.model_id
+        hub_content_description = f"This is the very informative {model_specs.model_id} description"
+        hub_content_markdown = f"This is the {model_specs.model_id} markdown"  # TODO markdown file needs loading
+
+        self.copy_hub_content_dependencies_to_hub_bucket(model_specs=model_specs)
+
+        hub_content_document = self.make_hub_content_document(model_specs=model_specs)
+        response = self._sm_client.import_hub_content(
+            HubName=self.curated_hub_name,
+            HubContentName=hub_content_name,
+            HubContentType="Model",
+            DocumentSchemaVersion="1.0.0",
+            HubContentDisplayName=hub_content_display_name,
+            HubContentDescription=hub_content_description,
+            HubContentMarkdown=hub_content_markdown,
+            HubContentDocument=hub_content_document,
+            HubContentSearchKeywords=[],
+            Tags=[]
+        )
+
+    def copy_hub_content_dependencies_to_hub_bucket(self, model_specs: JumpStartModelSpecs) -> None:
+        """Copies artifact and script tarballs into the hub bucket.
+
+        Unfortunately, this logic is duplicated/inconsistent with what is in Studio."""
+        pass  # TODO
+
+    def make_hub_content_document(self, model_specs: JumpStartModelSpecs) -> str:
+        """Converts the provided JumpStartModelSpecs into a Hub Content Document."""
+        # TODO copy artifact and script tarballs
+        copied_inference_artifact_location = "s3://foo"
+        copied_inference_script_location = "s3://foo"
+
+        hub_model_spec = HubModelSpec_v1_0_0(
+            capabilities=[],  # TODO add inference if needed?
+            DataType="",  # TODO not in SDK metadata
+            MlTask="",  # TODO not in SDK metadata
+            Framework=model_specs.hosting_ecr_specs.framework,
+            Origin=None,
+            Dependencies=[],  # TODO add references to copied artifacts
+            DatasetConfig=None,  # Out of scope in p0
+            DefaultTrainingConfig=None,  # Out of scope in p0
+            DefaultDeploymentConfig=self.make_hub_content_deployment_config(
+                model_specs=model_specs,
+                copied_artifact_location=copied_inference_artifact_location,
+                copied_script_location=copied_inference_script_location,
+            )
+        )
+        return json.dumps(hub_model_spec)  # TODO verify/fix string representation
+
+    def make_hub_content_deployment_config(
+        self, model_specs: JumpStartModelSpecs, copied_artifact_location: str, copied_script_location: str
+    ) -> DefaultDeploymentConfig:
+        """Creates a DefaultDeploymentConfig from the provided JumpStartModelSpecs."""
+        return DefaultDeploymentConfig(
+            SdkArgs=SdkArgs(
+                MinSdkVersion=model_specs.min_sdk_version,
+                SdkEstimatorArgs=None,  # Out of scope in p0
+            ),
+            FrameworkImageConfig=FrameworkImageConfig(
+                Framework=model_specs.hosting_ecr_specs.framework,
+                FrameworkVersion=model_specs.hosting_ecr_specs.framework_version,
+                PythonVersion=model_specs.hosting_ecr_specs.py_version,
+                TransformersVersion=getattr(model_specs.hosting_ecr_specs, "huggingface_transformers_version", None),
+                BaseFramework=None,  # TODO verify necessity
+            ),
+            ModelArtifactConfig=ModelArtifactConfig(
+                ArtifactLocation=copied_artifact_location,
+            ),
+            ScriptConfig=ScriptConfig(
+                ScriptLocation=copied_script_location,
+            ),
+            InstanceConfig=InstanceConfig(
+                DefaultInstanceType=model_specs.default_inference_instance_type,
+                InstanceTypeOptions=model_specs.supported_inference_instance_types or []
+            ),
+            InferenceNotebookConfig=InferenceNotebookConfig(
+                NotebookLocation="s3://foo"  # TODO not present in SDK metadata
+            ),
+            CustomImageConfig=None,
+        )
