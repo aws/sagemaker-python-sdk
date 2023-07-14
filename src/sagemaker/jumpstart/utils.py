2d5f5ecb31efc1ed3da4099af7aab7459888d8f5
Evan Kravitz <evakravi@amazon.com>
2023-07-14 19:08:35 +0000

    fix: s3 public url for china regions

--- src/sagemaker/jumpstart/utils.py
+++ src/sagemaker/jumpstart/utils.py
@@ -405,9 +405,10 @@ def verify_model_region_and_return_specs(
     if model_specs.hosting_eula_key and scope == constants.JumpStartScriptScope.INFERENCE.value:
         LOGGER.info(
             "Using model with end-user license agreement (EULA). "
-            "See https://%s.s3.%s.amazonaws.com/%s for terms of use.",
+            "See https://%s.s3.%s.amazonaws.com%s/%s for terms of use.",
             get_jumpstart_content_bucket(region=region),
             region,
+            ".cn" if region.startswith("cn-") else "",
             model_specs.hosting_eula_key,
         )
 
