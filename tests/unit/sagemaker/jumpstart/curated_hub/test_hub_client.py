26278418c2d5461ed7a985327fbc730954bcfd75
chrstfu <chrstfu@amazon.com>
2023-08-11 01:52:02 +0000

    fix: Adding more unit testing

---
+++ tests/unit/sagemaker/jumpstart/curated_hub/test_hub_client.py
@@ -0,0 +1,83 @@
+# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"). You
+# may not use this file except in compliance with the License. A copy of
+# the License is located at
+#
+#     http://aws.amazon.com/apache2.0/
+#
+# or in the "license" file accompanying this file. This file is
+# distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF
+# ANY KIND, either express or implied. See the License for the specific
+# language governing permissions and limitations under the License.
+"""This module contains utilities related to SageMaker JumpStart."""
+from __future__ import absolute_import
+import unittest
+
+from mock.mock import patch, Mock
+
+from sagemaker.jumpstart.curated_hub.hub_client import CuratedHubClient
+
+
+class HubClientTest(unittest.TestCase):
+    def setUp(self):
+        self.mock_curated_hub_client = CuratedHubClient("mock_hub_name", "us-west-2")
+
+    def test_create_hub(self):
+        hub_name = "hub_name"
+        hub_s3_bucket_name = "hub_bucket_name"
+        self.mock_curated_hub_client._sm_client = Mock()
+
+        self.mock_curated_hub_client.create_hub(hub_name, hub_s3_bucket_name)
+
+        self.mock_curated_hub_client._sm_client.create_hub.assert_called_with(
+            HubName=hub_name,
+            HubDescription="This is a curated hub.",  # TODO verify description
+            HubDisplayName=hub_name,
+            HubSearchKeywords=[],
+            S3StorageConfig={
+                "S3OutputPath": f"s3://{hub_s3_bucket_name}",
+            },
+            Tags=[],
+        )
+
+    def test_desribe_model(self):
+        model_specs = Mock()
+        model_specs.model_id = "model_id"
+        model_specs.version = "version"
+        self.mock_curated_hub_client._sm_client = Mock()
+
+        self.mock_curated_hub_client.desribe_model(model_specs)
+
+        self.mock_curated_hub_client._sm_client.describe_hub_content.assert_called_with(
+            HubName=self.mock_curated_hub_client.curated_hub_name,
+            HubContentName=model_specs.model_id,
+            HubContentType="Model",
+            HubContentVersion=model_specs.version,
+        )
+
+    def test_delete_version_of_model(self):
+        model_id = "model_id"
+        version = "version"
+        self.mock_curated_hub_client._sm_client = Mock()
+
+        self.mock_curated_hub_client.delete_version_of_model(model_id, version)
+
+        self.mock_curated_hub_client._sm_client.delete_hub_content.assert_called_with(
+            HubName=self.mock_curated_hub_client.curated_hub_name,
+            HubContentName=model_id,
+            HubContentType="Model",
+            HubContentVersion=version,
+        )
+
+    def test_list_hub_content_versions_no_content_noop(self):
+        hub_content_name = "hub_content_name"
+        self.mock_curated_hub_client._sm_client = Mock()
+
+        self.mock_curated_hub_client._list_hub_content_versions_no_content_noop(hub_content_name)
+
+        self.mock_curated_hub_client._sm_client.list_hub_content_versions.assert_called_with(
+            HubName=self.mock_curated_hub_client.curated_hub_name,
+            HubContentName=hub_content_name,
+            HubContentType="Model",
+        )
---
+++ tests/unit/sagemaker/jumpstart/curated_hub/test_utils.py
@@ -0,0 +1,67 @@
+# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"). You
+# may not use this file except in compliance with the License. A copy of
+# the License is located at
+#
+#     http://aws.amazon.com/apache2.0/
+#
+# or in the "license" file accompanying this file. This file is
+# distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF
+# ANY KIND, either express or implied. See the License for the specific
+# language governing permissions and limitations under the License.
+"""This module contains utilities related to SageMaker JumpStart."""
+from __future__ import absolute_import
+import unittest
+
+from mock.mock import patch, Mock
+
+from sagemaker.jumpstart.curated_hub.utils import *
+
+
+class UtilsTest(unittest.TestCase):
+    def test_list_objects_by_prefix_invalid_bucket_name_fails(self):
+        with self.assertRaises(ValueError) as context:
+            list_objects_by_prefix(None, "prefix", None)
+
+    def test_list_objects_by_prefix_invalid_prefix_fails(self):
+        with self.assertRaises(ValueError) as context:
+            list_objects_by_prefix("bucket", None, None)
+
+    def test_list_objects_by_prefix_no_content_empty(self):
+        mock_s3_client = Mock()
+        contents = {"Contents": ["hello"]}
+        mock_s3_client.list_objects_v2.return_value = contents
+
+        res = list_objects_by_prefix("bucket", "prefix", mock_s3_client)
+
+        self.assertEqual(res, contents["Contents"])
+
+    def test_to_s3_folder_prefix_removes_strings(self):
+        input = "////test"
+        output = "test/"
+
+        self.assertEqual(to_s3_folder_prefix(input), output)
+
+    def test_convert_s3_key_to_new_prefix(self):
+        src = "src"
+        dest = "dst"
+        input = f"{src}/test"
+        output = f"{dest}/test"
+
+        self.assertEqual(convert_s3_key_to_new_prefix(input, src, dest), output)
+
+    def test_base_framework(self):
+        model_specs = Mock()
+        model_specs.hosting_ecr_specs.framework = "huggingface"
+        model_specs.hosting_ecr_specs.framework_version = "version"
+
+        self.assertEqual(
+            base_framework(model_specs), f"pytorch{model_specs.hosting_ecr_specs.framework_version}"
+        )
+
+    def test_get_model_framework(self):
+        model_specs = Mock()
+        model_specs.model_id = "test-model-id"
+
+        self.assertEqual(get_model_framework(model_specs), "test")
