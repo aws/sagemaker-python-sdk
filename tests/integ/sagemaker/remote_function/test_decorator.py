9c34d2ffa54b3a9dacde5c5f479bcc41a418a4ea
Erick Benitez-Ramos <benieric@amazon.com>
2023-08-25 09:20:51 -0700

    chore: black-format

--- tests/integ/sagemaker/remote_function/test_decorator.py
+++ tests/integ/sagemaker/remote_function/test_decorator.py
@@ -275,7 +275,7 @@ def test_with_incompatible_dependencies(
     """
     This test is limited by the python version it is run with.
     It is currently working with python 3.8+. However, running it with older versions
-    or versions in the future may require changes to 'old_deps_requirements.txt' 
+    or versions in the future may require changes to 'old_deps_requirements.txt'
     to fulfill testing scenario.
 
     """
