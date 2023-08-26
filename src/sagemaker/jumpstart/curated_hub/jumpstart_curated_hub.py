3b54e7787bf0138c6bb8342fc8ef3700e274c0ef
chrstfu <chrstfu@amazon.com>
2023-08-26 01:02:00 +0000

    fix

--- src/sagemaker/jumpstart/curated_hub/jumpstart_curated_hub.py
+++ src/sagemaker/jumpstart/curated_hub/jumpstart_curated_hub.py
@@ -165,6 +165,8 @@ class JumpStartCuratedHub:
                 raise get_hub_s3_bucket_permissions_error(hub_s3_bucket_name)
             
             print(f"Received error: {ex.response}")
+            print(f"Error code: {NO_SUCH_BUCKET_ERROR_CODE}")
+            print(f"Equality check: {ex.response['Error']['Code'] == NO_SUCH_BUCKET_ERROR_CODE}")
             raise
 
     def _init_dependencies(self):
