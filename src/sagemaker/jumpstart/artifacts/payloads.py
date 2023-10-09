fc900e7df8cde4700598c33e0624fd3836655305
Evan Kravitz <evakravi@amazon.com>
2023-10-09 13:43:19 +0000

    chore: improve docstring

--- src/sagemaker/jumpstart/artifacts/payloads.py
+++ src/sagemaker/jumpstart/artifacts/payloads.py
@@ -10,7 +10,7 @@
 # distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF
 # ANY KIND, either express or implied. See the License for the specific
 # language governing permissions and limitations under the License.
-"""This module contains functions for obtaining example payloads for JumpStart models."""
+"""This module contains functions to obtain JumpStart model payloads."""
 from __future__ import absolute_import
 from copy import deepcopy
 from typing import Dict, Optional
