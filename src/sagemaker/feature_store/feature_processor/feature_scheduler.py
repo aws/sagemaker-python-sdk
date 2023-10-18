b78f12de978caa47332d7621c14bd27d2ff184c1
Matthew Fong <mwfong@amazon.com>
2023-10-18 16:50:15 -0700

    git rid of indentation

--- src/sagemaker/feature_store/feature_processor/feature_scheduler.py
+++ src/sagemaker/feature_store/feature_processor/feature_scheduler.py
@@ -284,7 +284,7 @@ def schedule(
             at expression, rate expression and cron expression. See the
             `CreateSchedule API
             <https://docs.aws.amazon.com/scheduler/latest/APIReference/API_CreateSchedule.html#scheduler-CreateSchedulerequest-ScheduleExpression>`_
-               for more details.
+            for more details.
         state (str): Specifies whether the schedule is enabled or disabled. Valid values are
             ENABLED and DISABLED. See the `State request parameter
             <https://docs.aws.amazon.com/scheduler/latest/APIReference/API_CreateSchedule.html#scheduler-CreateSchedule-request-State>`_
