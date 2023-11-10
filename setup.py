19767de51b28f3b02e99aee80e5ea17197caef48
martinRenou <martin.renou@gmail.com>
2023-11-10 09:05:19 +0100

    Change: Python 3.11 testing

--- setup.py
+++ setup.py
@@ -102,6 +102,7 @@ setup(
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
     ],
     install_requires=required_packages,
     extras_require=extras,
