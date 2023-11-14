315d054b180c92a2b83c9c2ead80629aac4286cf
whittech1 <97195892+whittech1@users.noreply.github.com>
2023-11-13 22:37:36 -0500

    fix: pin model version in JS model package test (#1396)

--- tests/integ/sagemaker/jumpstart/model/test_jumpstart_model.py
+++ tests/integ/sagemaker/jumpstart/model/test_jumpstart_model.py
@@ -140,6 +140,7 @@ def test_jumpstart_model_register(setup):
 
     model = JumpStartModel(
         model_id=model_id,
+        model_version="1.1.0",
         role=get_sm_session().get_caller_identity_arn(),
         sagemaker_session=get_sm_session(),
     )
