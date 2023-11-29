c48bba7e2cfdade514e6a3f4f740ccee5a787c7a
evakravi <69981223+evakravi@users.noreply.github.com>
2023-11-28 21:52:38 -0500

    chore: add jumpstart gated model integration tests (#1415)

--- tests/integ/sagemaker/jumpstart/model/test_jumpstart_model.py
+++ tests/integ/sagemaker/jumpstart/model/test_jumpstart_model.py
@@ -16,6 +16,7 @@ import time
 from unittest import mock
 
 import pytest
+
 import tests.integ
 
 from sagemaker.jumpstart.model import JumpStartModel
@@ -30,11 +31,15 @@ from tests.integ.sagemaker.jumpstart.utils import (
     get_tabular_data,
 )
 
-from sagemaker.enums import EndpointType
+INF2_SUPPORTED_REGIONS = {
+    "us-west-2",
+    "us-east-1",
+    "us-east-2",
+}
 
 MAX_INIT_TIME_SECONDS = 5
 
-GATED_INFERENCE_MODEL_SUPPORTED_REGIONS = {
+GATED_INFERENCE_MODEL_PACKAGE_SUPPORTED_REGIONS = {
     "us-west-2",
     "us-east-1",
     "eu-west-1",
@@ -67,44 +72,6 @@ def test_non_prepacked_jumpstart_model(setup):
     assert response is not None
 
 
-@pytest.mark.skipif(
-    tests.integ.test_region() not in tests.integ.INFERENCE_COMPONENT_SUPPORTED_REGIONS,
-    reason="inference component based endpoint is not supported in certain regions",
-)
-def test_non_prepacked_jumpstart_model_deployed_on_inference_component_based_endpoint(setup):
-
-    model_id = "huggingface-llm-falcon-7b-instruct-bf16"  # default g5.2xlarge
-
-    model = JumpStartModel(
-        model_id=model_id,
-        role=get_sm_session().get_caller_identity_arn(),
-        sagemaker_session=get_sm_session(),
-    )
-
-    predictor = model.deploy(endpoint_type=EndpointType.INFERENCE_COMPONENT_BASED)
-
-    inference_input = {
-        "inputs": "Girafatron is obsessed with giraffes, the most glorious animal on the "
-        + "face of this Earth. Giraftron believes all other animals are irrelevant when compared "
-        + "to the glorious majesty of the giraffe.\nDaniel: Hello, Girafatron!\nGirafatron:",
-        "parameters": {
-            "max_new_tokens": 50,
-            "top_k": 10,
-            "return_full_text": False,
-            "do_sample": True,
-        },
-    }
-
-    response = predictor.predict(inference_input)
-    assert response is not None
-
-    # Delete predictor
-    predictor.delete_predictor(wait=True)
-
-    # Delete endpoint
-    predictor.delete_endpoint()
-
-
 def test_prepacked_jumpstart_model(setup):
 
     model_id = "huggingface-txt2img-conflictx-complex-lineart"
@@ -126,8 +93,8 @@ def test_prepacked_jumpstart_model(setup):
 
 
 @pytest.mark.skipif(
-    tests.integ.test_region() not in GATED_INFERENCE_MODEL_SUPPORTED_REGIONS,
-    reason=f"JumpStart gated inference models unavailable in {tests.integ.test_region()}.",
+    tests.integ.test_region() not in GATED_INFERENCE_MODEL_PACKAGE_SUPPORTED_REGIONS,
+    reason=f"JumpStart model package inference models unavailable in {tests.integ.test_region()}.",
 )
 def test_model_package_arn_jumpstart_model(setup):
 
@@ -135,6 +102,7 @@ def test_model_package_arn_jumpstart_model(setup):
 
     model = JumpStartModel(
         model_id=model_id,
+        model_version="2.*",  # version <3.0.0 uses model packages
         role=get_sm_session().get_caller_identity_arn(),
         sagemaker_session=get_sm_session(),
     )
@@ -150,6 +118,62 @@ def test_model_package_arn_jumpstart_model(setup):
     }
 
     response = predictor.predict(payload, custom_attributes="accept_eula=true")
+
+    assert response is not None
+
+
+@pytest.mark.skipif(
+    tests.integ.test_region() not in INF2_SUPPORTED_REGIONS,
+    reason=f"INF2 instances unavailable in {tests.integ.test_region()}.",
+)
+def test_jumpstart_gated_model_neuron(setup):
+
+    model_id = "meta-textgenerationneuron-llama-2-7b"
+
+    model = JumpStartModel(
+        model_id=model_id,
+        role=get_sm_session().get_caller_identity_arn(),
+        sagemaker_session=get_sm_session(),
+    )
+
+    # uses ml.inf2.xlarge instance
+    predictor = model.deploy(
+        tags=[{"Key": JUMPSTART_TAG, "Value": os.environ[ENV_VAR_JUMPSTART_SDK_TEST_SUITE_ID]}],
+        accept_eula=True,
+    )
+
+    payload = {
+        "inputs": "some-payload",
+    }
+
+    response = predictor.predict(payload)
+
+    assert response is not None
+
+
+def test_jumpstart_gated_model(setup):
+
+    model_id = "meta-textgeneration-llama-2-7b"
+
+    model = JumpStartModel(
+        model_id=model_id,
+        model_version="3.*",  # version >=3.0.0 stores artifacts in jumpstart-private-cache-* buckets
+        role=get_sm_session().get_caller_identity_arn(),
+        sagemaker_session=get_sm_session(),
+    )
+
+    # uses ml.g5.2xlarge instance
+    predictor = model.deploy(
+        tags=[{"Key": JUMPSTART_TAG, "Value": os.environ[ENV_VAR_JUMPSTART_SDK_TEST_SUITE_ID]}],
+        accept_eula=True,
+    )
+
+    payload = {
+        "inputs": "some-payload",
+        "parameters": {"max_new_tokens": 256, "top_p": 0.9, "temperature": 0.6},
+    }
+
+    response = predictor.predict(payload)
 
     assert response is not None
 
