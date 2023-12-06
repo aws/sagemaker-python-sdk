37acde52a14da5edc6fc6bb4fd40f8f9e6a683e2
Raymond Liu <tzujui@amazon.com>
2023-12-06 22:28:06 +0000

    fix: enable model builder integration tests

--- tests/integ/sagemaker/serve/constants.py
+++ tests/integ/sagemaker/serve/constants.py
@@ -12,24 +12,24 @@
 # language governing permissions and limitations under the License.
 from __future__ import absolute_import
 
-# import os
+import os
 import platform
 
-# from tests.integ import DATA_DIR
+from tests.integ import DATA_DIR
 
-# SERVE_IN_PROCESS_TIMEOUT = 5
-# SERVE_MODEL_PACKAGE_TIMEOUT = 10
-# SERVE_LOCAL_CONTAINER_TIMEOUT = 10
+SERVE_IN_PROCESS_TIMEOUT = 5
+SERVE_MODEL_PACKAGE_TIMEOUT = 10
+SERVE_LOCAL_CONTAINER_TIMEOUT = 10
 SERVE_SAGEMAKER_ENDPOINT_TIMEOUT = 15
-# SERVE_SAVE_TIMEOUT = 2
+SERVE_SAVE_TIMEOUT = 2
 
-# NOT_RUNNING_ON_PY38 = platform.python_version_tuple()[1] != "8"
+NOT_RUNNING_ON_PY38 = platform.python_version_tuple()[1] != "8"
 NOT_RUNNING_ON_PY310 = platform.python_version_tuple()[1] != "10"
-# NOT_RUNNING_ON_INF_EXP_DEV_PIPELINE = os.getenv("TEST_OWNER") != "INF_EXP_DEV"
+NOT_RUNNING_ON_INF_EXP_DEV_PIPELINE = os.getenv("TEST_OWNER") != "INF_EXP_DEV"
 
-# XGB_RESOURCE_DIR = os.path.join(DATA_DIR, "serve_resources", "xgboost")
-# PYTORCH_SQUEEZENET_RESOURCE_DIR = os.path.join(DATA_DIR, "serve_resources", "pytorch")
-# TF_EFFICIENT_RESOURCE_DIR = os.path.join(DATA_DIR, "serve_resources", "tensorflow")
-# HF_DIR = os.path.join(DATA_DIR, "serve_resources", "hf")
+XGB_RESOURCE_DIR = os.path.join(DATA_DIR, "serve_resources", "xgboost")
+PYTORCH_SQUEEZENET_RESOURCE_DIR = os.path.join(DATA_DIR, "serve_resources", "pytorch")
+TF_EFFICIENT_RESOURCE_DIR = os.path.join(DATA_DIR, "serve_resources", "tensorflow")
+HF_DIR = os.path.join(DATA_DIR, "serve_resources", "hf")
 
-# BYOC_IMAGE_URI_TEMPLATE = "661407751302.dkr.ecr.{}.amazonaws.com/byoc-integ-test-images:{}"
+BYOC_IMAGE_URI_TEMPLATE = "661407751302.dkr.ecr.{}.amazonaws.com/byoc-integ-test-images:{}"
