0c0472cae75a74ec557218ecb37701cfb17d842f
qidewenwhen <qidewenwhen@gmail.com>
2024-01-19 16:14:49 -0800

    feat: Support selective pipeline execution between function step and regular step

--- tests/integ/sagemaker/workflow/test_selective_execution.py
+++ tests/integ/sagemaker/workflow/test_selective_execution.py
@@ -16,6 +16,7 @@ import os
 
 import pytest
 
+from sagemaker.processing import ProcessingInput
 from tests.integ import DATA_DIR
 from sagemaker.sklearn import SKLearnProcessor
 from sagemaker.workflow.step_outputs import get_step
@@ -84,7 +85,7 @@ def test_selective_execution_among_pure_function_steps(
             region_name=region_name,
             role=role,
             no_of_steps=2,
-            last_step_name="sum",
+            last_step_name_prefix="sum",
             execution_parameters=dict(),
             step_status="Succeeded",
             step_result_type=int,
@@ -97,7 +98,7 @@ def test_selective_execution_among_pure_function_steps(
             region_name=region_name,
             role=role,
             no_of_steps=2,
-            last_step_name="sum",
+            last_step_name_prefix="sum",
             execution_parameters=dict(),
             step_status="Succeeded",
             step_result_type=int,
@@ -115,7 +116,7 @@ def test_selective_execution_among_pure_function_steps(
             pass
 
 
-def test_selective_execution_of_regular_step_depended_by_function_step(
+def test_selective_execution_of_regular_step_referenced_by_function_step(
     sagemaker_session,
     role,
     pipeline_name,
@@ -168,7 +169,7 @@ def test_selective_execution_of_regular_step_depended_by_function_step(
             region_name=region_name,
             role=role,
             no_of_steps=2,
-            last_step_name="func",
+            last_step_name_prefix="func",
             execution_parameters=dict(),
             step_status="Succeeded",
             step_result_type=str,
@@ -182,7 +183,7 @@ def test_selective_execution_of_regular_step_depended_by_function_step(
             region_name=region_name,
             role=role,
             no_of_steps=2,
-            last_step_name="func",
+            last_step_name_prefix="func",
             execution_parameters=dict(),
             step_status="Succeeded",
             step_result_type=str,
@@ -192,6 +193,105 @@ def test_selective_execution_of_regular_step_depended_by_function_step(
                 source_pipeline_execution_arn=execution.arn,
                 selected_steps=[get_step(final_output).name],
             ),
+        )
+
+    finally:
+        try:
+            pipeline.delete()
+        except Exception:
+            pass
+
+
+def test_selective_execution_of_function_step_referenced_by_regular_step(
+    pipeline_session,
+    role,
+    pipeline_name,
+    region_name,
+    dummy_container_without_error,
+    sklearn_latest_version,
+):
+    # Test Selective Pipeline Execution on function step -> [select: regular step]
+    os.environ["AWS_DEFAULT_REGION"] = region_name
+    processing_job_instance_counts = 2
+
+    @step(
+        name="step1",
+        role=role,
+        image_uri=dummy_container_without_error,
+        instance_type=INSTANCE_TYPE,
+        keep_alive_period_in_seconds=60,
+    )
+    def func(var: int):
+        return 1, var
+
+    step_output = func(processing_job_instance_counts)
+
+    script_path = os.path.join(DATA_DIR, "dummy_script.py")
+    input_file_path = os.path.join(DATA_DIR, "dummy_input.txt")
+    inputs = [
+        ProcessingInput(source=input_file_path, destination="/opt/ml/processing/inputs/"),
+    ]
+
+    sklearn_processor = SKLearnProcessor(
+        framework_version=sklearn_latest_version,
+        role=role,
+        instance_type=INSTANCE_TYPE,
+        instance_count=step_output[1],
+        command=["python3"],
+        sagemaker_session=pipeline_session,
+        base_job_name="test-sklearn",
+    )
+
+    step_args = sklearn_processor.run(
+        inputs=inputs,
+        code=script_path,
+    )
+    process_step = ProcessingStep(
+        name="MyProcessStep",
+        step_args=step_args,
+    )
+
+    pipeline = Pipeline(
+        name=pipeline_name,
+        steps=[process_step],
+        sagemaker_session=pipeline_session,
+    )
+
+    try:
+        execution, _ = create_and_execute_pipeline(
+            pipeline=pipeline,
+            pipeline_name=pipeline_name,
+            region_name=region_name,
+            role=role,
+            no_of_steps=2,
+            last_step_name_prefix=process_step.name,
+            execution_parameters=dict(),
+            step_status="Succeeded",
+            wait_duration=1000,  # seconds
+        )
+
+        _, execution_steps2 = create_and_execute_pipeline(
+            pipeline=pipeline,
+            pipeline_name=pipeline_name,
+            region_name=region_name,
+            role=role,
+            no_of_steps=2,
+            last_step_name_prefix=process_step.name,
+            execution_parameters=dict(),
+            step_status="Succeeded",
+            wait_duration=1000,  # seconds
+            selective_execution_config=SelectiveExecutionConfig(
+                source_pipeline_execution_arn=execution.arn,
+                selected_steps=[process_step.name],
+            ),
+        )
+
+        execution_proc_job = pipeline_session.describe_processing_job(
+            execution_steps2[0]["Metadata"]["ProcessingJob"]["Arn"].split("/")[-1]
+        )
+        assert (
+            execution_proc_job["ProcessingResources"]["ClusterConfig"]["InstanceCount"]
+            == processing_job_instance_counts
         )
 
     finally:
