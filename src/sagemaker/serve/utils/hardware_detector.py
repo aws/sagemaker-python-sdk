cf49ca8bb25c7f963f0b1f451b1716de79b5073b
Jonathan Makunga <makung@amazon.com>
2024-01-24 10:06:41 -0800

    Addressed PR review comments

--- src/sagemaker/serve/utils/hardware_detector.py
+++ src/sagemaker/serve/utils/hardware_detector.py
@@ -15,6 +15,8 @@ from __future__ import absolute_import
 
 import logging
 
+from botocore.exceptions import ClientError
+
 from sagemaker import Session
 
 logger = logging.getLogger(__name__)
@@ -23,7 +25,6 @@ logger = logging.getLogger(__name__)
 fallback_gpu_resource_mapping = {
     "ml.p5.48xlarge": {"Count": 8, "TotalGpuMemoryInMiB": 655360},
     "ml.p4d.24xlarge": {"Count": 8, "TotalGpuMemoryInMiB": 327680},
-    "ml.p4de.24xlarge": {"Count": 8, "TotalGpuMemoryInMiB": 610352},
     "ml.p3.2xlarge": {"Count": 1, "TotalGpuMemoryInMiB": 16384},
     "ml.p3.8xlarge": {"Count": 4, "TotalGpuMemoryInMiB": 65536},
     "ml.p3.16xlarge": {"Count": 8, "TotalGpuMemoryInMiB": 131072},
@@ -37,7 +38,6 @@ fallback_gpu_resource_mapping = {
     "ml.g4dn.8xlarge": {"Count": 1, "TotalGpuMemoryInMiB": 16384},
     "ml.g4dn.16xlarge": {"Count": 1, "TotalGpuMemoryInMiB": 16384},
     "ml.g4dn.12xlarge": {"Count": 4, "TotalGpuMemoryInMiB": 65536},
-    "ml.g5n.xlarge": {"Count": 1, "TotalGpuMemoryInMiB": 22888},
     "ml.g5.2xlarge": {"Count": 1, "TotalGpuMemoryInMiB": 24576},
     "ml.g5.4xlarge": {"Count": 1, "TotalGpuMemoryInMiB": 24576},
     "ml.g5.8xlarge": {"Count": 1, "TotalGpuMemoryInMiB": 24576},
@@ -48,55 +48,56 @@ fallback_gpu_resource_mapping = {
 }
 
 
-def _get_gpu_info(instance_type: str, session: Session) -> int:
+def _get_gpu_info(instance_type: str, session: Session) -> tuple:
     """Get GPU info for the provided instance"""
     ec2_client = session.boto_session.client("ec2")
+    ec2_instance = _format_instance_type(instance_type)
 
-    split_instance = instance_type.split(".")
-    split_instance.pop(0)
+    try:
+        instance_info = ec2_client.describe_instance_types(InstanceTypes=[ec2_instance]).get(
+            "InstanceTypes"
+        )[0]
+    except ClientError as e:
+        logger.error("Failed to describe instance types %s", e)
+        raise ValueError("Provided instance_type is not GPU enabled.")
 
-    ec2_instance = ".".join(split_instance)
+    if instance_info is not None:
+        gpus_info = instance_info.get("GpuInfo")
+        if gpus_info is not None:
+            instance_gpu_info = (
+                gpus_info.get("Gpus")[0].get("Count"),
+                gpus_info.get("TotalGpuMemoryInMiB"),
+            )
 
-    instance_info = ec2_client.describe_instance_types(InstanceTypes=[ec2_instance]).get(
-        "InstanceTypes"
-    )[0]
+            logger.info("GPU Info [%s]: %s", ec2_instance, instance_gpu_info)
+            return instance_gpu_info
 
-    gpus_info = instance_info.get("GpuInfo")
-    if gpus_info:
-        return gpus_info.get("Gpus")[0].get("Count")
+    logger.error("Provided instance_type is not GPU enabled: [%s]", ec2_instance)
     raise ValueError("Provided instance_type is not GPU enabled.")
 
 
-def _aggregate_gpu_memory_size(instance_type: str, session: Session) -> int:
-    """Get Aggregate GPU Memory Size in MiB for the provided instance"""
-    ec2_client = session.boto_session.client("ec2")
-
-    split_instance = instance_type.split(".")
-    split_instance.pop(0)
-
-    ec2_instance = ".".join(split_instance)
-
-    instance_info = ec2_client.describe_instance_types(InstanceTypes=[ec2_instance]).get(
-        "InstanceTypes"
-    )[0]
-
-    gpus_info = instance_info.get("GpuInfo")
-    if gpus_info:
-        return gpus_info.get("TotalGpuMemoryInMiB")
-    raise ValueError("Provided instance_type is not GPU enabled.")
-
-
-def _get_gpu_info_fallback(instance_type: str) -> int:
+def _get_gpu_info_fallback(instance_type: str) -> tuple:
     """Get GPU info for the provided instance fallback"""
-    fallback_instance = fallback_gpu_resource_mapping.get(instance_type)
-    if fallback_instance is None or not fallback_instance.get("Count"):
+    fallback_instance_gpu_info = fallback_gpu_resource_mapping.get(instance_type)
+    ec2_instance = _format_instance_type(instance_type)
+    if fallback_instance_gpu_info is None:
+        logger.error("Provided instance_type is not GPU enabled: [%s]", ec2_instance)
         raise ValueError("Provided instance_type is not GPU enabled.")
-    return fallback_instance.get("Count")
+
+    fallback_instance_gpu_info = (
+        fallback_instance_gpu_info.get("Count"),
+        fallback_instance_gpu_info.get("TotalGpuMemoryInMiB"),
+    )
+    logger.info("GPU Info [%s]: %s", ec2_instance, fallback_instance_gpu_info)
+    return fallback_instance_gpu_info
 
 
-def _get_aggregate_gpu_memory_size_fallback(instance_type: str) -> int:
-    """Get Aggregate GPU Memory Size in MiB for the provided instance fallback"""
-    fallback_instance = fallback_gpu_resource_mapping.get(instance_type)
-    if fallback_instance is None or not fallback_instance.get("TotalGpuMemoryInMiB"):
-        raise ValueError("Provided instance_type is not GPU enabled.")
-    return fallback_instance.get("TotalGpuMemoryInMiB")
+def _format_instance_type(instance_type: str) -> str:
+    """Formats provided instance type name"""
+    split_instance = instance_type.split(".")
+
+    if len(split_instance) > 2:
+        split_instance.pop(0)
+
+    ec2_instance = ".".join(split_instance)
+    return ec2_instance
--- tests/unit/sagemaker/serve/utils/test_hardware_detector.py
+++ tests/unit/sagemaker/serve/utils/test_hardware_detector.py
@@ -12,148 +12,76 @@
 # language governing permissions and limitations under the License.
 from __future__ import absolute_import
 
+from botocore.exceptions import ClientError
 from mock.mock import patch
 import pytest
 
 from sagemaker.serve.utils import hardware_detector
 
 REGION = "us-west-2"
-VALID_INSTANCE_TYPE = "ml.p5.48xlarge"
+VALID_INSTANCE_TYPE = "ml.g5.48xlarge"
 INVALID_INSTANCE_TYPE = "fl.c5.57xxlarge"
-DESCRIBE_INSTANCE_TYPE_RESULT = {
-    "InstanceTypes": [
-        {
-            "InstanceType": "g5.48xlarge",
-            "CurrentGeneration": True,
-            "FreeTierEligible": False,
-            "SupportedUsageClasses": ["on-demand", "spot"],
-            "SupportedRootDeviceTypes": ["ebs"],
-            "SupportedVirtualizationTypes": ["hvm"],
-            "BareMetal": False,
-            "Hypervisor": "nitro",
-            "ProcessorInfo": {
-                "SupportedArchitectures": ["x86_64"],
-                "SustainedClockSpeedInGhz": 3.3,
-                "Manufacturer": "AMD",
-            },
-            "VCpuInfo": {"DefaultVCpus": 192, "DefaultCores": 96, "DefaultThreadsPerCore": 2},
-            "MemoryInfo": {"SizeInMiB": 786432},
-            "InstanceStorageSupported": True,
-            "InstanceStorageInfo": {
-                "TotalSizeInGB": 7600,
-                "Disks": [{"SizeInGB": 3800, "Count": 2, "Type": "ssd"}],
-                "NvmeSupport": "required",
-                "EncryptionSupport": "required",
-            },
-            "EbsInfo": {
-                "EbsOptimizedSupport": "default",
-                "EncryptionSupport": "supported",
-                "EbsOptimizedInfo": {
-                    "BaselineBandwidthInMbps": 19000,
-                    "BaselineThroughputInMBps": 2375.0,
-                    "BaselineIops": 80000,
-                    "MaximumBandwidthInMbps": 19000,
-                    "MaximumThroughputInMBps": 2375.0,
-                    "MaximumIops": 80000,
-                },
-                "NvmeSupport": "required",
-            },
-            "NetworkInfo": {
-                "NetworkPerformance": "100 Gigabit",
-                "MaximumNetworkInterfaces": 7,
-                "MaximumNetworkCards": 1,
-                "DefaultNetworkCardIndex": 0,
-                "NetworkCards": [
-                    {
-                        "NetworkCardIndex": 0,
-                        "NetworkPerformance": "100 Gigabit",
-                        "MaximumNetworkInterfaces": 7,
-                        "BaselineBandwidthInGbps": 100.0,
-                        "PeakBandwidthInGbps": 100.0,
-                    }
-                ],
-                "Ipv4AddressesPerInterface": 50,
-                "Ipv6AddressesPerInterface": 50,
-                "Ipv6Supported": True,
-                "EnaSupport": "required",
-                "EfaSupported": True,
-                "EfaInfo": {"MaximumEfaInterfaces": 1},
-                "EncryptionInTransitSupported": True,
-                "EnaSrdSupported": True,
-            },
-            "GpuInfo": {
-                "Gpus": [
-                    {
-                        "Name": "A10G",
-                        "Manufacturer": "NVIDIA",
-                        "Count": 8,
-                        "MemoryInfo": {"SizeInMiB": 24576},
-                    }
-                ],
-                "TotalGpuMemoryInMiB": 196608,
-            },
-            "PlacementGroupInfo": {"SupportedStrategies": ["cluster", "partition", "spread"]},
-            "HibernationSupported": False,
-            "BurstablePerformanceSupported": False,
-            "DedicatedHostsSupported": True,
-            "AutoRecoverySupported": False,
-            "SupportedBootModes": ["legacy-bios", "uefi"],
-            "NitroEnclavesSupport": "supported",
-            "NitroTpmSupport": "supported",
-            "NitroTpmInfo": {"SupportedVersions": ["2.0"]},
-        }
-    ]
-}
+EXPECTED_INSTANCE_GPU_INFO = (8, 196608)
 
 
 @patch("sagemaker.session.Session")
 def test_get_gpu_info_success(session):
-    session.boto_session.client(
-        "ec2"
-    ).describe_instance_types.return_value = DESCRIBE_INSTANCE_TYPE_RESULT
+    client = session.boto_session.client("ec2")
+    client.describe_instance_types.return_value = {
+        "InstanceTypes": [
+            {
+                "GpuInfo": {
+                    "Gpus": [
+                        {
+                            "Name": "A10G",
+                            "Manufacturer": "NVIDIA",
+                            "Count": 8,
+                            "MemoryInfo": {"SizeInMiB": 24576},
+                        }
+                    ],
+                    "TotalGpuMemoryInMiB": 196608,
+                },
+            }
+        ]
+    }
 
-    gpu_info = hardware_detector._get_gpu_info(VALID_INSTANCE_TYPE, session)
+    instance_gpu_info = hardware_detector._get_gpu_info(VALID_INSTANCE_TYPE, session)
 
-    assert gpu_info == 8
+    client.describe_instance_types.assert_called_once_with(InstanceTypes=["g5.48xlarge"])
+    assert instance_gpu_info == EXPECTED_INSTANCE_GPU_INFO
 
 
 @patch("sagemaker.session.Session")
 def test_get_gpu_info_throws(session):
-    session.boto_session.client("ec2").describe_instance_types.return_value = {
-        "InstanceTypes": [{}]
-    }
+    client = session.boto_session.client("ec2")
+    client.describe_instance_types.return_value = {"InstanceTypes": [{}]}
 
     with pytest.raises(ValueError):
         hardware_detector._get_gpu_info(INVALID_INSTANCE_TYPE, session)
 
 
 @patch("sagemaker.session.Session")
-def test_aggregate_gpu_memory_size_success(session):
-    session.boto_session.client(
-        "ec2"
-    ).describe_instance_types.return_value = DESCRIBE_INSTANCE_TYPE_RESULT
-
-    aggregate_memory_size_in_mib = hardware_detector._aggregate_gpu_memory_size(
-        VALID_INSTANCE_TYPE, session
+def test_get_gpu_info_describe_instance_types_throws(session):
+    client = session.boto_session.client("ec2")
+    client.describe_instance_types.side_effect = ClientError(
+        {
+            "Error": {
+                "Code": "InvalidInstanceType",
+                "Message": f"An error occurred (InvalidInstanceType) when calling the DescribeInstanceTypes "
+                f"operation: The following supplied instance types do not exist: [#{INVALID_INSTANCE_TYPE}]",
+            }
+        },
+        "DescribeInstanceTypes",
     )
 
-    assert aggregate_memory_size_in_mib == 196608
-
-
-@patch("sagemaker.session.Session")
-def test_aggregate_gpu_memory_size_throws(session):
-    session.boto_session.client("ec2").describe_instance_types.return_value = {
-        "InstanceTypes": [{}]
-    }
-
     with pytest.raises(ValueError):
-        hardware_detector._aggregate_gpu_memory_size(INVALID_INSTANCE_TYPE, session)
+        hardware_detector._get_gpu_info(INVALID_INSTANCE_TYPE, session)
 
 
 def test_get_gpu_info_fallback_success():
-    gpu_info = hardware_detector._get_gpu_info_fallback(VALID_INSTANCE_TYPE)
+    fallback_instance_gpu_info = hardware_detector._get_gpu_info_fallback(VALID_INSTANCE_TYPE)
 
-    assert gpu_info == 8
+    assert fallback_instance_gpu_info == EXPECTED_INSTANCE_GPU_INFO
 
 
 def test_get_gpu_info_fallback_throws():
@@ -161,14 +89,13 @@ def test_get_gpu_info_fallback_throws():
         hardware_detector._get_gpu_info_fallback(INVALID_INSTANCE_TYPE)
 
 
-def test_get_gpu_aggregate_memory_size_in_mib_fallback_success():
-    aggregate_memory_size_in_mib = hardware_detector._get_aggregate_gpu_memory_size_fallback(
-        VALID_INSTANCE_TYPE
-    )
+def test_format_instance_type_success():
+    formatted_instance_type = hardware_detector._format_instance_type(VALID_INSTANCE_TYPE)
 
-    assert aggregate_memory_size_in_mib == 655360
+    assert formatted_instance_type == "g5.48xlarge"
 
 
-def test_get_gpu_aggregate_memory_size_in_mib_fallback_throws():
-    with pytest.raises(ValueError):
-        hardware_detector._get_aggregate_gpu_memory_size_fallback(INVALID_INSTANCE_TYPE)
+def test_format_instance_type_without_ml_success():
+    formatted_instance_type = hardware_detector._format_instance_type("g5.48xlarge")
+
+    assert formatted_instance_type == "g5.48xlarge"
