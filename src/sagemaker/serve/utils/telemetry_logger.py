30169b375853e46b5fd5c28950ce9a23bd0b2ce6
Jonathan Makunga <makung@amazon.com>
2024-02-12 14:24:41 -0800

    Fix tests

--- src/sagemaker/serve/utils/telemetry_logger.py
+++ src/sagemaker/serve/utils/telemetry_logger.py
@@ -72,7 +72,7 @@ def _capture_telemetry(func_name: str):
             if self.model_server == ModelServer.DJL_SERVING or self.model_server == ModelServer.TGI:
                 extra += f"&x-modelName={self.model}"
 
-            if self.sagemaker_session.endpoint_arn:
+            if self.sagemaker_session and self.sagemaker_session.endpoint_arn:
                 extra += f"&x-endpointArn={self.sagemaker_session.endpoint_arn}"
 
             start_timer = perf_counter()
