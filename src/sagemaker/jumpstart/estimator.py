d4f7a00549077839d4e870e1d706b7fa31aa858d
Benjamin Crabtree <bencrab@amazon.com>
2024-02-20 22:32:13 +0000

    fix param

--- src/sagemaker/jumpstart/estimator.py
+++ src/sagemaker/jumpstart/estimator.py
@@ -530,7 +530,7 @@ class JumpStartEstimator(Estimator):
                 hub_arn = hub_name
             else:
                 hub_arn = construct_hub_arn_from_name(
-                    hub_name=hub_name, region=region, sagemaker_session=sagemaker_session
+                    hub_name=hub_name, region=region, session=sagemaker_session
                 )
 
         estimator_init_kwargs = get_init_kwargs(
