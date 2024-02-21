ecd1f97d1de4a3bc4d640cb3b0921dbf969687be
Ben Crabtree <bencrab@amazon.com>
2024-02-21 10:58:01 -0500

    feat: add hub and hubcontent support in retrieval function for jumpstart model cache (#4438)

--- src/sagemaker/jumpstart/cache.py
+++ src/sagemaker/jumpstart/cache.py
@@ -490,28 +490,6 @@ class JumpStartModelsCache:
         details, _ = self._content_cache.get(JumpStartCachedContentKey(HubDataType.HUB, hub_arn))
         return details.formatted_content
 
-    def get_hub_model(self, hub_model_arn: str) -> JumpStartModelSpecs:
-        """Return JumpStart-compatible specs for a given Hub model
-
-        Args:
-            hub_model_arn (str): Arn for the Hub model to get specs for
-        """
-
-        details, _ = self._content_cache.get(
-            JumpStartCachedContentKey(HubDataType.MODEL, hub_model_arn)
-        )
-        return details.formatted_content
-
-    def get_hub(self, hub_arn: str) -> Dict[str, Any]:
-        """Return descriptive info for a given Hub
-
-        Args:
-            hub_arn (str): Arn for the Hub to get info for
-        """
-
-        details, _ = self._content_cache.get(JumpStartCachedContentKey(HubDataType.HUB, hub_arn))
-        return details.formatted_content
-
     def clear(self) -> None:
         """Clears the model ID/version and s3 cache."""
         self._content_cache.clear()
