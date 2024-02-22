492251137e78f3aaac7cebdbfbe0c8dc2b59a898
JJ Lim <jj.lim418@gmail.com>
2024-02-22 21:22:29 +0000

    initial barebone for hub utils and curated hub

--- src/sagemaker/jumpstart/curated_hub/curated_hub.py
+++ src/sagemaker/jumpstart/curated_hub/curated_hub.py
@@ -14,36 +14,68 @@
 from __future__ import absolute_import
 
 from typing import Optional, Dict, Any
-
+import boto3
 from sagemaker.session import Session
+from sagemaker.jumpstart.constants import (
+    JUMPSTART_DEFAULT_REGION_NAME,
+)
+
+from sagemaker.jumpstart.types import HubDataType
+import sagemaker.jumpstart.curated_hub.utils as hubutils
 
 
 class CuratedHub:
     """Class for creating and managing a curated JumpStart hub"""
 
-    def __init__(self, hub_name: str, region: str, session: Optional[Session] = None):
-        self.hub_name = hub_name
+    def __init__(
+        self,
+        name: str,
+        region: str = JUMPSTART_DEFAULT_REGION_NAME,
+        session: Optional[Session] = None,
+    ):
+        self.name = name
+        if session.boto_region_name != region:
+            # TODO: Handle error
+            pass
         self.region = region
-        self.session = session
-        self._sm_session = session or Session()
+        self._session = session or Session(boto3.Session(region_name=region))
+
+    def create(
+        self,
+        description: str,
+        display_name: Optional[str] = None,
+        search_keywords: Optional[str] = None,
+        bucket_name: Optional[str] = None,
+        tags: Optional[str] = None,
+    ) -> Dict[str, str]:
+        """Creates a hub with the given description"""
+
+        return hubutils.create_hub(
+            hub_name=self.name,
+            hub_description=description,
+            hub_display_name=display_name,
+            hub_search_keywords=search_keywords,
+            hub_bucket_name=bucket_name,
+            tags=tags,
+            sagemaker_session=self._session,
+        )
 
     def describe_model(self, model_name: str, model_version: str = "*") -> Dict[str, Any]:
         """Returns descriptive information about the Hub Model"""
 
-        hub_content = self._sm_session.describe_hub_content(
-            model_name, "Model", self.hub_name, model_version
+        hub_content = hubutils.describe_hub_content(
+            hub_name=self.name,
+            content_name=model_name,
+            content_type=HubDataType.MODEL,
+            content_version=model_version,
+            sagemaker_session=self._session,
         )
-
-        # TODO: Parse HubContent
-        # TODO: Parse HubContentDocument
 
         return hub_content
 
     def describe(self) -> Dict[str, Any]:
         """Returns descriptive information about the Hub"""
 
-        hub_info = self._sm_session.describe_hub(hub_name=self.hub_name)
-
-        # TODO: Validations?
+        hub_info = hubutils.describe_hub(hub_name=self.name, sagemaker_session=self._session)
 
         return hub_info
