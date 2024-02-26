9073bed53c7906a86eff6a7f003a7c5700dbb9f7
Sirut Buasai <sirutbuasai27@hotmail.com>
2024-02-26 15:06:34 -0800

    update pt2.2 sm training dlc pysdk

--- tests/unit/test_utils.py
+++ tests/unit/test_utils.py
@@ -384,6 +384,8 @@ def test_set_nested_value():
 
 
 def test_get_short_version():
+    assert sagemaker.utils.get_short_version("2.2.0") == "2.2"
+    assert sagemaker.utils.get_short_version("2.2") == "2.2"
     assert sagemaker.utils.get_short_version("2.1.0") == "2.1"
     assert sagemaker.utils.get_short_version("2.1") == "2.1"
     assert sagemaker.utils.get_short_version("2.0.1") == "2.0"
