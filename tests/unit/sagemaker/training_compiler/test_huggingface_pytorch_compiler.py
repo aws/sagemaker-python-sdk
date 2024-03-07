7fecc331d18882b6ae9b85cb20e2ea1e47c1e995
Erick Benitez-Ramos <141277478+benieric@users.noreply.github.com>
2024-03-07 15:18:42 -0800

    fix: make unit tests compatible with pytest-xdist (#4486)
    * fix: make unit tests compatible with pytest-xdist
    
    * fix failing test

--- tests/unit/sagemaker/training_compiler/test_huggingface_pytorch_compiler.py
+++ tests/unit/sagemaker/training_compiler/test_huggingface_pytorch_compiler.py
@@ -202,7 +202,7 @@ def test_unsupported_cpu_instance(
         ).fit()
 
 
-@pytest.mark.parametrize("unsupported_gpu_instance_class", UNSUPPORTED_GPU_INSTANCE_CLASSES)
+@pytest.mark.parametrize("unsupported_gpu_instance_class", sorted(UNSUPPORTED_GPU_INSTANCE_CLASSES))
 def test_unsupported_gpu_instance(
     unsupported_gpu_instance_class,
     huggingface_training_compiler_version,
@@ -366,7 +366,7 @@ def test_unsupported_distribution(
 @patch("sagemaker.utils.create_tar_file", MagicMock())
 @patch("sagemaker.estimator.name_from_base", return_value=JOB_NAME)
 @patch("time.time", return_value=TIME)
-@pytest.mark.parametrize("instance_class", SUPPORTED_GPU_INSTANCE_CLASSES)
+@pytest.mark.parametrize("instance_class", sorted(SUPPORTED_GPU_INSTANCE_CLASSES))
 def test_pytorchxla_distribution(
     time,
     name_from_base,
@@ -430,7 +430,7 @@ def test_pytorchxla_distribution(
 @patch("sagemaker.utils.create_tar_file", MagicMock())
 @patch("sagemaker.estimator.name_from_base", return_value=JOB_NAME)
 @patch("time.time", return_value=TIME)
-@pytest.mark.parametrize("instance_class", SUPPORTED_GPU_INSTANCE_CLASSES)
+@pytest.mark.parametrize("instance_class", sorted(SUPPORTED_GPU_INSTANCE_CLASSES))
 def test_default_compiler_config(
     time,
     name_from_base,
