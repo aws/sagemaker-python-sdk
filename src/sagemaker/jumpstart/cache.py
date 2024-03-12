cd9831d7884501942d4a633ceedecfef67dfdfba
Ben Crabtree <bencrab@amazon.com>
2024-03-12 13:15:58 -0400

    MultiPartCopy with Sync Algorithm (#4475)
    * first pass at sync function with util classes
    
    * adding tests and update clases
    
    * linting
    
    * file generator class inheritance
    
    * lint
    
    * multipart copy and algorithm updates
    
    * modularize sync
    
    * reformatting folders
    
    * testing for sync
    
    * do not tolerate vulnerable
    
    * remove prints
    
    * handle multithreading progress bar
    
    * update tests
    
    * optimize function and add hub bucket prefix
    
    * docstrings and linting

--- src/sagemaker/jumpstart/cache.py
+++ src/sagemaker/jumpstart/cache.py
@@ -486,7 +486,9 @@ class JumpStartModelsCache:
                 formatted_content=hub_description,
             )
 
-        raise ValueError(self._file_type_error_msg(data_type))
+        raise ValueError(
+            self._file_type_error_msg(data_type)
+        )
 
     def get_manifest(
         self,
