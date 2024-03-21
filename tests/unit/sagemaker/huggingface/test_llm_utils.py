193c4a573a6b2837ca41aab5a1342f20953a87cb
JJ Lim <jj.lim418@gmail.com>
2024-03-21 05:32:47 +0000

    revert unrelated changes

--- tests/unit/sagemaker/huggingface/test_llm_utils.py
+++ tests/unit/sagemaker/huggingface/test_llm_utils.py
@@ -12,7 +12,6 @@
 # language governing permissions and limitations under the License.
 from __future__ import absolute_import
 
-import pytest
 from unittest import TestCase
 from urllib.error import HTTPError
 from unittest.mock import Mock, patch
