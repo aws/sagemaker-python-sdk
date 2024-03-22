a6fc9b33ada0fb168a612ad995e2786316c09b77
JJ Lim <jj.lim418@gmail.com>
2024-03-22 22:25:16 +0000

    less indentation the better

--- src/sagemaker/jumpstart/curated_hub/parser_utils.py
+++ src/sagemaker/jumpstart/curated_hub/parser_utils.py
@@ -33,26 +33,24 @@ def walk_and_apply_json(json_obj: Dict[Any, Any], apply) -> Dict[Any, Any]:
     """Recursively walks a json object and applies a given function to the keys."""
 
     def _walk_and_apply_json(json_obj, new):
-        if isinstance(json_obj, dict):
-            if isinstance(new, dict):
-                for key, value in json_obj.items():
-                    new_key = apply(key)
-                    if isinstance(value, dict):
-                        new[new_key] = {}
-                        _walk_and_apply_json(value, new=new[new_key])
-                    elif isinstance(value, list):
-                        new[new_key] = []
-                        for item in value:
-                            _walk_and_apply_json(item, new=new[new_key])
-                    else:
-                        new[new_key] = value
-            elif isinstance(new, list):
-                new.append(_walk_and_apply_json(json_obj, new={}))
-        elif isinstance(json_obj, list):
-            if isinstance(new, dict):
-                new.update(json_obj)
-            elif isinstance(new, list):
-                new.append(json_obj)
+        if isinstance(json_obj, dict) and isinstance(new, dict):
+            for key, value in json_obj.items():
+                new_key = apply(key)
+                if isinstance(value, dict):
+                    new[new_key] = {}
+                    _walk_and_apply_json(value, new=new[new_key])
+                elif isinstance(value, list):
+                    new[new_key] = []
+                    for item in value:
+                        _walk_and_apply_json(item, new=new[new_key])
+                else:
+                    new[new_key] = value
+        elif isinstance(json_obj, dict) and isinstance(new, list):
+            new.append(_walk_and_apply_json(json_obj, new={}))
+        elif isinstance(json_obj, list) and isinstance(new, dict):
+            new.update(json_obj)
+        elif isinstance(json_obj, list) and isinstance(new, list):
+            new.append(json_obj)
         return new
 
     return _walk_and_apply_json(json_obj, new={})
