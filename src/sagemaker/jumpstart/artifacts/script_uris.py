5c721416766cad45d79fe4939526c145d942d971
JJ Lim <jj.lim418@gmail.com>
2024-03-28 07:21:34 +0000

    Revert "Fix CreateModel operation: 1 validation error detected: Value X at 'primaryContainer.modelDataUrl' failed to satisfy constraint"
    This reverts commit f7c93dedbf18b59d55180df991439c40e0636e36.

--- src/sagemaker/jumpstart/artifacts/script_uris.py
+++ src/sagemaker/jumpstart/artifacts/script_uris.py
@@ -95,6 +95,9 @@ def _retrieve_script_uri(
             getattr(model_specs, "training_prepacked_script_key") or model_specs.training_script_key
         )
 
+    # this is the full ARN for HubContent
+    if hub_arn:
+        return model_script_key
 
     bucket = os.environ.get(
         ENV_VARIABLE_JUMPSTART_SCRIPT_ARTIFACT_BUCKET_OVERRIDE
