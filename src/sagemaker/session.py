a5c97abee4ec4f7d701ec351333f57fb2311f85d
Henry Wang <henrywa@amazon.com>
2024-04-01 16:51:20 +0000

    fix: Remove space specific business logic from Python SDK function to fetch execution role

--- src/sagemaker/session.py
+++ src/sagemaker/session.py
@@ -5668,7 +5668,6 @@ class Session(object):  # pylint: disable=too-many-public-methods
                 instance_name = metadata.get("ResourceName")
                 domain_id = metadata.get("DomainId")
                 user_profile_name = metadata.get("UserProfileName")
-                space_name = metadata.get("SpaceName")
                 execution_role_arn = metadata.get("ExecutionRoleArn")
             try:
                 if domain_id is None:
@@ -5680,11 +5679,6 @@ class Session(object):  # pylint: disable=too-many-public-methods
                 # find execution role from the metadata file if present
                 if execution_role_arn is not None:
                     return execution_role_arn
-
-                # In Shared Space app, find execution role from DefaultSpaceSettings on domain level
-                if space_name is not None:
-                    domain_desc = self.sagemaker_client.describe_domain(DomainId=domain_id)
-                    return domain_desc["DefaultSpaceSettings"]["ExecutionRole"]
 
                 user_profile_desc = self.sagemaker_client.describe_user_profile(
                     DomainId=domain_id, UserProfileName=user_profile_name
