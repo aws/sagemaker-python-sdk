3d249c40a1f20a0b184c04276005d8256a2a98f1
Haixin Wang <98612668+haixiw@users.noreply.github.com>
2024-04-16 10:51:05 -0700

    feat: optimum 0.0.21 (#4577)
    * feat: optimum 0.0.21
    
    * minor update

--- tests/unit/sagemaker/image_uris/test_huggingface_llm.py
+++ tests/unit/sagemaker/image_uris/test_huggingface_llm.py
@@ -38,6 +38,7 @@ HF_VERSIONS_MAPPING = {
         "0.0.18": "1.13.1-optimum0.0.18-neuronx-py310-ubuntu22.04",
         "0.0.19": "1.13.1-optimum0.0.19-neuronx-py310-ubuntu22.04",
         "0.0.20": "1.13.1-optimum0.0.20-neuronx-py310-ubuntu22.04",
+        "0.0.21": "1.13.1-optimum0.0.21-neuronx-py310-ubuntu22.04",
     },
 }
 
