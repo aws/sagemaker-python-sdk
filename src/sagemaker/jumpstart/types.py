68aada51c2cbfde30af22d3f868819fd0c12b479
Jonathan Makunga <makung@amazon.com>
2024-04-18 22:57:17 -0700

    Resolved PR Review Comments

--- src/sagemaker/jumpstart/types.py
+++ src/sagemaker/jumpstart/types.py
@@ -15,7 +15,6 @@ from __future__ import absolute_import
 from copy import deepcopy
 from enum import Enum
 from typing import Any, Dict, List, Optional, Set, Union
-
 from sagemaker.utils import get_instance_type_family, format_tags, Tags, deep_override_dict
 from sagemaker.model_metrics import ModelMetrics
 from sagemaker.metadata_properties import MetadataProperties
