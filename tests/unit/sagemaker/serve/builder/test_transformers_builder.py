65cc586969cf402ced7d76e52caa71a0cfeaebb1
Samrudhi Sharma <154457034+samruds@users.noreply.github.com>
2024-05-15 13:47:30 -0700

    fix: Image URI should take precedence for HF models (#4684)
    * Fix: Image URI should take precedence for HF models
    
    * Fix formatting
    
    * Fix formatting
    
    * Fix formatting
    
    * Increase coverage -  UT pass

--- tests/unit/sagemaker/serve/builder/test_transformers_builder.py
+++ tests/unit/sagemaker/serve/builder/test_transformers_builder.py
@@ -58,6 +58,10 @@ mock_sample_output = [
 mock_schema_builder = MagicMock()
 mock_schema_builder.sample_input = mock_sample_input
 mock_schema_builder.sample_output = mock_sample_output
+MOCK_IMAGE_CONFIG = (
+    "763104351884.dkr.ecr.us-west-2.amazonaws.com/"
+    "huggingface-pytorch-inference:2.0.0-transformers4.28.1-gpu-py310-cu118-ubuntu20.04-v1.0"
+)
 
 
 class TestTransformersBuilder(unittest.TestCase):
@@ -88,6 +92,46 @@ class TestTransformersBuilder(unittest.TestCase):
         predictor = model.deploy(model_data_download_timeout=1800)
 
         assert model.vpc_config == MOCK_VPC_CONFIG
+        assert builder.env_vars["MODEL_LOADING_TIMEOUT"] == "1800"
+        assert isinstance(predictor, TransformersLocalModePredictor)
+
+        assert builder.nb_instance_type == "ml.g5.24xlarge"
+
+        builder._original_deploy = MagicMock()
+        builder._prepare_for_mode.return_value = (None, {})
+        predictor = model.deploy(mode=Mode.SAGEMAKER_ENDPOINT, role="mock_role_arn")
+        assert "HF_MODEL_ID" in model.env
+
+        with self.assertRaises(ValueError) as _:
+            model.deploy(mode=Mode.IN_PROCESS)
+
+    @patch(
+        "sagemaker.serve.builder.transformers_builder._get_nb_instance",
+        return_value="ml.g5.24xlarge",
+    )
+    @patch("sagemaker.serve.builder.transformers_builder._capture_telemetry", side_effect=None)
+    def test_image_uri(
+        self,
+        mock_get_nb_instance,
+        mock_telemetry,
+    ):
+        builder = ModelBuilder(
+            model=mock_model_id,
+            schema_builder=mock_schema_builder,
+            mode=Mode.LOCAL_CONTAINER,
+            image_uri=MOCK_IMAGE_CONFIG,
+        )
+
+        builder._prepare_for_mode = MagicMock()
+        builder._prepare_for_mode.side_effect = None
+
+        model = builder.build()
+        builder.serve_settings.telemetry_opt_out = True
+
+        builder.modes[str(Mode.LOCAL_CONTAINER)] = MagicMock()
+        predictor = model.deploy(model_data_download_timeout=1800)
+
+        assert builder.image_uri == MOCK_IMAGE_CONFIG
         assert builder.env_vars["MODEL_LOADING_TIMEOUT"] == "1800"
         assert isinstance(predictor, TransformersLocalModePredictor)
 
