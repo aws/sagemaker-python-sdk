d9d4f1360bcd996bf72357834c9e938b5a7dbf97
Jonathan Makunga <makung@amazon.com>
2024-05-17 14:12:44 -0700

    Add TEI Serving

--- src/sagemaker/serve/utils/telemetry_logger.py
+++ src/sagemaker/serve/utils/telemetry_logger.py
@@ -52,12 +52,8 @@ MODE_TO_CODE = {
 }
 
 MODEL_SERVER_TO_CODE = {
-    str(ModelServer.TORCHSERVE): 1,
-    str(ModelServer.MMS): 2,
-    str(ModelServer.TENSORFLOW_SERVING): 3,
-    str(ModelServer.DJL_SERVING): 4,
-    str(ModelServer.TRITON): 5,
-    str(ModelServer.TGI): 6,
+    str(model_server): model_server.value
+    for model_server in ModelServer
 }
 
 MLFLOW_MODEL_PATH_CODE = {
