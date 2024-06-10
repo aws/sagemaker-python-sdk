bc84f20f9bb5a06e3e782463e823bdb75bc6b0eb
Evan Kravitz <evakravi@amazon.com>
2024-06-10 20:44:12 +0000

    fix: estimator.deploy not respecting instance type

--- src/sagemaker/jumpstart/factory/estimator.py
+++ src/sagemaker/jumpstart/factory/estimator.py
@@ -322,7 +322,11 @@ def get_deploy_kwargs(
         model_id=model_id,
         model_from_estimator=True,
         model_version=model_version,
-        instance_type=model_deploy_kwargs.instance_type if training_instance_type is None else None,
+        instance_type=(
+            model_deploy_kwargs.instance_type
+            if training_instance_type is None or instance_type is not None
+            else None
+        ),
         region=region,
         image_uri=image_uri,
         source_dir=source_dir,
