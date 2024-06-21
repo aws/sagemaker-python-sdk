12d7cf7a16dfb1f65584c5d421224d2b2fd78da9
Malav Shastri <malavhs@amazon.com>
2024-06-21 01:32:51 -0400

    disabling pylint for release

--- src/sagemaker/jumpstart/hub/hub.py
+++ src/sagemaker/jumpstart/hub/hub.py
@@ -10,6 +10,7 @@
 # distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF
 # ANY KIND, either express or implied. See the License for the specific
 # language governing permissions and limitations under the License.
+# pylint: skip-file
 """This module provides the JumpStart Hub class."""
 from __future__ import absolute_import
 from datetime import datetime
