2edec4f66fbd5d46d2ea396206490a48d21f0650
Bryannah Hernandez <brymh@amazon.com>
2024-07-23 13:43:53 -0700

    deps and mb

--- src/sagemaker/serve/builder/transformers_builder.py
+++ src/sagemaker/serve/builder/transformers_builder.py
@@ -18,7 +18,7 @@ from abc import ABC, abstractmethod
 from typing import Type
 from pathlib import Path
 from packaging.version import Version
-
+ 
 from sagemaker.model import Model
 from sagemaker import image_uris
 from sagemaker.serve.utils.local_hardware import (
@@ -44,20 +44,22 @@ from sagemaker.serve.mode.function_pointers import Mode
 from sagemaker.serve.utils.telemetry_logger import _capture_telemetry
 from sagemaker.base_predictor import PredictorBase
 from sagemaker.huggingface.llm_utils import get_huggingface_model_metadata
-
+from sagemaker.serve.model_server.multi_model_server.inference import create_conda_env
+ 
+ 
 logger = logging.getLogger(__name__)
 DEFAULT_TIMEOUT = 1800
 LOCAL_MODES = [Mode.LOCAL_CONTAINER, Mode.IN_PROCESS]
-
-
+ 
+ 
 """Retrieves images for different libraries - Pytorch, TensorFlow from HuggingFace hub
 """
-
-
+ 
+ 
 # pylint: disable=W0108
 class Transformers(ABC):
     """Transformers build logic with ModelBuilder()"""
-
+ 
     def __init__(self):
         self.model = None
         self.serve_settings = None
@@ -86,11 +88,11 @@ class Transformers(ABC):
         self.schema_builder = None
         self.inference_spec = None
         self.shared_libs = None
-
+ 
     @abstractmethod
     def _prepare_for_mode(self):
         """Abstract method"""
-
+ 
     def _create_transformers_model(self) -> Type[Model]:
         """Initializes HF model with or without image_uri"""
         if self.image_uri is None:
@@ -103,35 +105,35 @@ class Transformers(ABC):
                 role=self.role_arn,
                 sagemaker_session=self.sagemaker_session,
             )
-
+ 
         logger.info("Detected %s. Proceeding with the the deployment.", self.image_uri)
-
+ 
         self._original_deploy = pysdk_model.deploy
         pysdk_model.deploy = self._transformers_model_builder_deploy_wrapper
         return pysdk_model
-
+ 
     def _get_hf_metadata_create_model(self) -> Type[Model]:
         """Initializes the model after fetching image
-
+ 
         1. Get the metadata for deciding framework
         2. Get the supported hugging face versions
         3. Create model
         4. Fetch image
-
+ 
         Returns:
             pysdk_model: Corresponding model instance
         """
-
+ 
         hf_model_md = get_huggingface_model_metadata(
             self.env_vars.get("HF_MODEL_ID"), self.env_vars.get("HUGGING_FACE_HUB_TOKEN")
         )
         hf_config = image_uris.config_for_framework("huggingface").get("inference")
         config = hf_config["versions"]
         base_hf_version = sorted(config.keys(), key=lambda v: Version(v), reverse=True)[0]
-
+ 
         if hf_model_md is None:
             raise ValueError("Could not fetch HF metadata")
-
+ 
         if "pytorch" in hf_model_md.get("tags"):
             self.pytorch_version = self._get_supported_version(
                 hf_config, base_hf_version, "pytorch"
@@ -164,7 +166,7 @@ class Transformers(ABC):
                 tensorflow_version=self.tensorflow_version,
                 vpc_config=self.vpc_config,
             )
-
+ 
         if not self.image_uri and self.mode == Mode.LOCAL_CONTAINER:
             self.image_uri = pysdk_model.serving_image_uri(
                 self.sagemaker_session.boto_region_name, "local"
@@ -173,26 +175,26 @@ class Transformers(ABC):
             self.image_uri = pysdk_model.serving_image_uri(
                 self.sagemaker_session.boto_region_name, self.instance_type
             )
-
+ 
         if pysdk_model is None or self.image_uri is None:
             raise ValueError("PySDK model unable to be created, try overriding image_uri")
-
+ 
         if not pysdk_model.image_uri:
             pysdk_model.image_uri = self.image_uri
-
+ 
         return pysdk_model
-
+ 
     @_capture_telemetry("transformers.deploy")
     def _transformers_model_builder_deploy_wrapper(self, *args, **kwargs) -> Type[PredictorBase]:
         """Returns predictor depending on local or sagemaker endpoint mode
-
+ 
         Returns:
             TransformersLocalModePredictor: During local mode deployment
         """
         timeout = kwargs.get("model_data_download_timeout")
         if timeout:
             self.env_vars.update({"MODEL_LOADING_TIMEOUT": str(timeout)})
-
+ 
         if "mode" in kwargs and kwargs.get("mode") != self.mode:
             overwrite_mode = kwargs.get("mode")
             # mode overwritten by customer during model.deploy()
@@ -201,7 +203,7 @@ class Transformers(ABC):
                 overwrite_mode,
                 self.mode,
             )
-
+ 
             if overwrite_mode == Mode.SAGEMAKER_ENDPOINT:
                 self.mode = self.pysdk_model.mode = Mode.SAGEMAKER_ENDPOINT
             elif overwrite_mode == Mode.LOCAL_CONTAINER:
@@ -209,18 +211,18 @@ class Transformers(ABC):
                 self.mode = self.pysdk_model.mode = Mode.LOCAL_CONTAINER
             else:
                 raise ValueError("Mode %s is not supported!" % overwrite_mode)
-
+ 
         self._set_instance()
-
+ 
         serializer = self.schema_builder.input_serializer
         deserializer = self.schema_builder._output_deserializer
         if self.mode == Mode.LOCAL_CONTAINER:
             timeout = kwargs.get("model_data_download_timeout")
-
+ 
             predictor = TransformersLocalModePredictor(
                 self.modes[str(Mode.LOCAL_CONTAINER)], serializer, deserializer
             )
-
+ 
             self.modes[str(Mode.LOCAL_CONTAINER)].create_server(
                 self.image_uri,
                 timeout if timeout else DEFAULT_TIMEOUT,
@@ -230,14 +232,14 @@ class Transformers(ABC):
                 jumpstart=False,
             )
             return predictor
-
+ 
         if self.mode == Mode.IN_PROCESS:
             timeout = kwargs.get("model_data_download_timeout")
-
+ 
             predictor = TransformersInProcessModePredictor(
                 self.modes[str(Mode.IN_PROCESS)], serializer, deserializer
             )
-
+ 
             self.modes[str(Mode.IN_PROCESS)].create_server(
                 self.image_uri,
                 timeout if timeout else DEFAULT_TIMEOUT,
@@ -246,42 +248,42 @@ class Transformers(ABC):
                 self.pysdk_model.env,
             )
             return predictor
-
+ 
         if "mode" in kwargs:
             del kwargs["mode"]
         if "role" in kwargs:
             self.pysdk_model.role = kwargs.get("role")
             del kwargs["role"]
-
+ 
         if not _is_optimized(self.pysdk_model):
             self._prepare_for_mode()
-
+ 
         if "endpoint_logging" not in kwargs:
             kwargs["endpoint_logging"] = True
-
+ 
         if "initial_instance_count" not in kwargs:
             kwargs.update({"initial_instance_count": 1})
-
+ 
         predictor = self._original_deploy(*args, **kwargs)
-
+ 
         predictor.serializer = serializer
         predictor.deserializer = deserializer
         return predictor
-
+ 
     def _build_transformers_env(self):
         """Build model for hugging face deployment using"""
         self.nb_instance_type = _get_nb_instance()
-
+ 
         _create_dir_structure(self.model_path)
         if not hasattr(self, "pysdk_model"):
-
+ 
             if self.inference_spec is not None:
                 self.env_vars.update({"HF_MODEL_ID": self.inference_spec.get_model()})
             else:
                 self.env_vars.update({"HF_MODEL_ID": self.model})
-
+ 
             logger.info(self.env_vars)
-
+ 
             # TODO: Move to a helper function
             if hasattr(self.env_vars, "HF_API_TOKEN"):
                 self.hf_model_config = _get_model_config_properties_from_hf(
@@ -291,14 +293,14 @@ class Transformers(ABC):
                 self.hf_model_config = _get_model_config_properties_from_hf(
                     self.env_vars.get("HF_MODEL_ID"), self.env_vars.get("HUGGING_FACE_HUB_TOKEN")
                 )
-
+ 
         self.pysdk_model = self._create_transformers_model()
-
+ 
         if self.mode in LOCAL_MODES:
             self._prepare_for_mode()
-
+ 
         return self.pysdk_model
-
+ 
     def _set_instance(self, **kwargs):
         """Set the instance : Given the detected notebook type or provided instance type"""
         if self.mode == Mode.SAGEMAKER_ENDPOINT:
@@ -312,7 +314,7 @@ class Transformers(ABC):
                 raise ValueError(
                     "Instance type must be provided when deploying to SageMaker Endpoint mode."
                 )
-
+ 
     def _get_supported_version(self, hf_config, hugging_face_version, base_fw):
         """Uses the hugging face json config to pick supported versions"""
         version_config = hf_config.get("versions").get(hugging_face_version)
@@ -324,7 +326,7 @@ class Transformers(ABC):
                     base_fw_version = ".".join(base_fw_version.split(".")[:-1])
                 versions_to_return.append(base_fw_version)
         return sorted(versions_to_return, reverse=True)[0]
-
+ 
     def _auto_detect_container(self):
         """Set image_uri by detecting container via model name or inference spec"""
         # Auto detect the container image uri
@@ -334,7 +336,7 @@ class Transformers(ABC):
                 self.image_uri,
             )
             return
-
+ 
         if self.model:
             logger.info(
                 "Auto detect container url for the provided model and on instance %s",
@@ -343,7 +345,7 @@ class Transformers(ABC):
             self.image_uri = auto_detect_container(
                 self.model, self.sagemaker_session.boto_region_name, self.instance_type
             )
-
+ 
         elif self.inference_spec:
             # TODO: this won't work for larger image.
             # Fail and let the customer include the image uri
@@ -360,26 +362,26 @@ class Transformers(ABC):
             raise ValueError(
                 "Cannot detect and set image_uri. Please pass model or inference spec."
             )
-
+ 
     def _build_for_transformers(self):
         """Method that triggers model build
-
+ 
         Returns:PySDK model
         """
         self.secret_key = None
         self.model_server = ModelServer.MMS
-
+ 
         if self.inference_spec:
-
+ 
             os.makedirs(self.model_path, exist_ok=True)
-
+ 
             code_path = Path(self.model_path).joinpath("code")
-
+ 
             save_pkl(code_path, (self.inference_spec, self.schema_builder))
             logger.info("PKL file saved to file: %s", code_path)
-
+ 
             self._auto_detect_container()
-
+ 
             self.secret_key = prepare_for_mms(
                 model_path=self.model_path,
                 shared_libs=self.shared_libs,
@@ -388,9 +390,11 @@ class Transformers(ABC):
                 image_uri=self.image_uri,
                 inference_spec=self.inference_spec,
             )
-
+ 
+        create_conda_env()
+ 
         self._build_transformers_env()
-
+ 
         if self.role_arn:
             self.pysdk_model.role = self.role_arn
         if self.sagemaker_session:
