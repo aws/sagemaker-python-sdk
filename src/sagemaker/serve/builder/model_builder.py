2ac83eb88944d68656779af1286d6fc5f2392acd
Bryannah Hernandez <brymh@amazon.com>
2024-07-24 09:47:49 -0700

    testing

--- src/sagemaker/serve/builder/model_builder.py
+++ src/sagemaker/serve/builder/model_builder.py
@@ -434,10 +434,9 @@ class ModelBuilder(Triton, DJL, JumpStart, TGI, Transformers, TensorflowServing,
                 env_vars=self.env_vars,
                 model_server=self.model_server,
             )
+            create_conda_env()
             code_path = Path(self.model_path).joinpath("code")
-            if self.inference_spec:
-                create_conda_env()
-                save_pkl(code_path, (self.inference_spec, self.schema_builder))
+            save_pkl(code_path, (self.inference_spec, self.schema_builder))
             self.modes[str(Mode.IN_PROCESS)].prepare()
             return None
 
