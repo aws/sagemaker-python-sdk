bca701fd5815eec60c242c70b2a4493606b37ecf
Malav Shastri <malavhs@amazon.com>
2024-08-14 16:06:20 -0400

    address linter issues

--- src/sagemaker/jumpstart/hub/parser_utils.py
+++ src/sagemaker/jumpstart/hub/parser_utils.py
@@ -10,6 +10,7 @@
 # distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF
 # ANY KIND, either express or implied. See the License for the specific
 # language governing permissions and limitations under the License.
+# pylint: skip-file
 """This module contains utilities related to SageMaker JumpStart Hub."""
 from __future__ import absolute_import
 
