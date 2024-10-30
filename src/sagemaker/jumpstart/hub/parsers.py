0092ff4fa9e5efac6e15af05cba9902068a5e500
chrstfu <chrstfu@amazon.com>
2024-10-30 21:31:12 +0000

    feat: Marketplace model support in HubService

--- src/sagemaker/jumpstart/hub/parsers.py
+++ src/sagemaker/jumpstart/hub/parsers.py
@@ -27,7 +27,7 @@ from sagemaker.jumpstart.hub.interfaces import (
     HubModelDocument,
 )
 from sagemaker.jumpstart.hub.parser_utils import (
-    camel_to_snake,
+    pascal_to_snake,
     snake_to_upper_camel,
     walk_and_apply_json,
 )
@@ -86,7 +86,7 @@ def get_model_spec_arg_keys(
         arg_keys = []
 
     if naming_convention == NamingConventionType.SNAKE_CASE:
-        arg_keys = [camel_to_snake(key) for key in arg_keys]
+        arg_keys = [pascal_to_snake(key) for key in arg_keys]
     elif naming_convention == NamingConventionType.UPPER_CAMEL_CASE:
         return arg_keys
     else:
@@ -137,6 +137,8 @@ def make_model_specs_from_describe_hub_content_response(
     hub_model_document: HubModelDocument = response.hub_content_document
     specs["url"] = hub_model_document.url
     specs["min_sdk_version"] = hub_model_document.min_sdk_version
+    specs["model_types"] = hub_model_document.model_types
+    specs["capabilities"] = hub_model_document.capabilities
     specs["training_supported"] = bool(hub_model_document.training_supported)
     specs["incremental_training_supported"] = bool(
         hub_model_document.incremental_training_supported
@@ -146,15 +148,19 @@ def make_model_specs_from_describe_hub_content_response(
     specs["inference_config_components"] = hub_model_document.inference_config_components
     specs["inference_config_rankings"] = hub_model_document.inference_config_rankings
 
-    hosting_artifact_bucket, hosting_artifact_key = parse_s3_url(  # pylint: disable=unused-variable
-        hub_model_document.hosting_artifact_uri
-    )
-    specs["hosting_artifact_key"] = hosting_artifact_key
-    specs["hosting_artifact_uri"] = hub_model_document.hosting_artifact_uri
-    hosting_script_bucket, hosting_script_key = parse_s3_url(  # pylint: disable=unused-variable
-        hub_model_document.hosting_script_uri
-    )
-    specs["hosting_script_key"] = hosting_script_key
+    if hub_model_document.hosting_artifact_uri:
+        _, hosting_artifact_key = parse_s3_url(  # pylint: disable=unused-variable
+            hub_model_document.hosting_artifact_uri
+        )
+        specs["hosting_artifact_key"] = hosting_artifact_key
+        specs["hosting_artifact_uri"] = hub_model_document.hosting_artifact_uri
+
+    if hub_model_document.hosting_script_uri:
+        _, hosting_script_key = parse_s3_url(  # pylint: disable=unused-variable
+            hub_model_document.hosting_script_uri
+        )
+        specs["hosting_script_key"] = hosting_script_key
+
     specs["inference_environment_variables"] = hub_model_document.inference_environment_variables
     specs["inference_vulnerable"] = False
     specs["inference_dependencies"] = hub_model_document.inference_dependencies
@@ -201,7 +207,7 @@ def make_model_specs_from_describe_hub_content_response(
     default_payloads: Dict[str, Any] = {}
     if hub_model_document.default_payloads is not None:
         for alias, payload in hub_model_document.default_payloads.items():
-            default_payloads[alias] = walk_and_apply_json(payload.to_json(), camel_to_snake)
+            default_payloads[alias] = walk_and_apply_json(payload.to_json(), pascal_to_snake)
         specs["default_payloads"] = default_payloads
     specs["gated_bucket"] = hub_model_document.gated_bucket
     specs["inference_volume_size"] = hub_model_document.inference_volume_size
@@ -219,6 +225,8 @@ def make_model_specs_from_describe_hub_content_response(
 
     if hub_model_document.hosting_model_package_arn:
         specs["hosting_model_package_arns"] = {region: hub_model_document.hosting_model_package_arn}
+
+    specs["model_subscription_link"] = hub_model_document.model_subscription_link
 
     specs["hosting_use_script_uri"] = hub_model_document.hosting_use_script_uri
 
