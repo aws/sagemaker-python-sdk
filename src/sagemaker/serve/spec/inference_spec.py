2e17bcbd7a1a3003033e2276d44e56c8917e7961
Pravali Uppugunduri <46845440+pravali96@users.noreply.github.com>
2024-11-01 16:15:15 -0700

    feat: add pre-processing and post-processing logic to inference_spec (#1560)
    * add pre-processing and post-processing logic to inference_spec
    
    * fix format
    
    * make  accept_type and content_type optional
    
    * remove accept_type and content_type from pre/post processing
    
    * correct typo

--- src/sagemaker/serve/spec/inference_spec.py
+++ src/sagemaker/serve/spec/inference_spec.py
@@ -28,6 +28,12 @@ class InferenceSpec(abc.ABC):
             model (object): The model object
         """
 
+    def preprocess(self, input_data: object):
+        """Custom pre-processing function"""
+
+    def postprocess(self, predictions: object):
+        """Custom post-processing function"""
+
     def prepare(self, *args, **kwargs):
         """Custom prepare function"""
 
