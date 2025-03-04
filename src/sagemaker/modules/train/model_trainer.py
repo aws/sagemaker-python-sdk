c86082fdeb61fc343ce94b8b096fa374446e5c63
Erick Benitez-Ramos <benieric@amazon.com>
2025-03-03 19:00:36 -0800

    fix tox.ini

--- src/sagemaker/modules/train/model_trainer.py
+++ src/sagemaker/modules/train/model_trainer.py
@@ -485,7 +485,7 @@ class ModelTrainer(BaseModel):
                         logger.info(f"contents: {contents}")
                         self.hyperparameters = yaml.safe_load(contents)
                         if not isinstance(self.hyperparameters, dict):
-                            raise ValueError(f"YAML contents must be a valid mapping")
+                            raise ValueError("YAML contents must be a valid mapping")
                         logger.info(f"hyperparameters: {self.hyperparameters}")
                         logger.debug("Hyperparameters loaded as YAML")
                     except (yaml.YAMLError, ValueError):
