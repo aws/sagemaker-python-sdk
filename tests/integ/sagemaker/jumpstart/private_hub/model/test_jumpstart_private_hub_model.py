e4974f384bc9eb9bf237ab9b97bdcf2e92c74636
Rohan Narayan <narrohag@amazon.com>
2025-03-07 19:12:30 +0000

    separate private hub setup code

--- tests/integ/sagemaker/jumpstart/private_hub/model/test_jumpstart_private_hub_model.py
+++ tests/integ/sagemaker/jumpstart/private_hub/model/test_jumpstart_private_hub_model.py
@@ -35,31 +35,10 @@ from tests.integ.sagemaker.jumpstart.utils import (
     with_exponential_backoff,
 )
 
+from tests.integ.sagemaker.jumpstart.private_hub.setup import add_model_references
+
+
 MAX_INIT_TIME_SECONDS = 5
-
-TEST_MODEL_IDS = {
-    "catboost-classification-model",
-    "huggingface-txt2img-conflictx-complex-lineart",
-    "meta-textgeneration-llama-2-7b",
-    "meta-textgeneration-llama-3-2-1b",
-    "catboost-regression-model",
-}
-
-
-@with_exponential_backoff()
-def create_model_reference(hub_instance, model_arn):
-    hub_instance.create_model_reference(model_arn=model_arn)
-
-
-@pytest.fixture(scope="session")
-def add_model_references():
-    # Create Model References to test in Hub
-    hub_instance = Hub(
-        hub_name=os.environ[ENV_VAR_JUMPSTART_SDK_TEST_HUB_NAME], sagemaker_session=get_sm_session()
-    )
-    for model in TEST_MODEL_IDS:
-        model_arn = get_public_hub_model_arn(hub_instance, model)
-        create_model_reference(hub_instance, model_arn)
 
 
 def test_jumpstart_hub_model(setup, add_model_references):
