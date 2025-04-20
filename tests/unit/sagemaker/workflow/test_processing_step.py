06801a4936bed9bccf8b41e6eb9651bdf0691aa2
rsareddy0329 <rsareddy0329@gmail.com>
2025-04-19 19:11:58 -0700

    Revert the PR changes 5122 (#5134)
    * change: Allow telemetry only in supported regions
    
    * change: Allow telemetry only in supported regions
    
    * change: Allow telemetry only in supported regions
    
    * change: Allow telemetry only in supported regions
    
    * change: Allow telemetry only in supported regions
    
    * documentation: Removed a line about python version requirements of training script which can misguide users.Training script can be of latest version based on the support provided by framework_version of the container
    
    * feature: Enabled update_endpoint through model_builder
    
    * fix: fix unit test, black-check, pylint errors
    
    * fix: fix black-check, pylint errors
    
    * fix:Added handler for pipeline variable while creating process job
    
    * fix: Added handler for pipeline variable while creating process job
    
    * Revert the PR changes: #5122, due to issue https://t.corp.amazon.com/P223568185/overview
    
    * Fix: fix the issue, https://t.corp.amazon.com/P223568185/communication
    
    * Revert PR 5122 changes, due to issues with other processor codeflows
    
    ---------
    
    Co-authored-by: Roja Reddy Sareddy <rsareddy@amazon.com>
    Co-authored-by: Zhaoqi <jzhaoqwa@amazon.com>

--- tests/unit/sagemaker/workflow/test_processing_step.py
+++ tests/unit/sagemaker/workflow/test_processing_step.py
@@ -825,13 +825,6 @@ def test_spark_processor(spark_processor, processing_input, pipeline_session):
     processor.sagemaker_session = pipeline_session
     processor.role = ROLE
 
-    arguments_output = [
-        "--input",
-        "input-data-uri",
-        "--output",
-        '{"Get": "Parameters.MyArgOutput"}',
-    ]
-
     run_inputs["inputs"] = processing_input
 
     step_args = processor.run(**run_inputs)
@@ -842,7 +835,7 @@ def test_spark_processor(spark_processor, processing_input, pipeline_session):
 
     step_args = get_step_args_helper(step_args, "Processing")
 
-    assert step_args["AppSpecification"]["ContainerArguments"] == arguments_output
+    assert step_args["AppSpecification"]["ContainerArguments"] == run_inputs["arguments"]
 
     entry_points = step_args["AppSpecification"]["ContainerEntrypoint"]
     entry_points_expr = []
@@ -1027,13 +1020,6 @@ def test_spark_processor_local_code(spark_processor, processing_input, pipeline_
     processor.sagemaker_session = pipeline_session
     processor.role = ROLE
 
-    arguments_output = [
-        "--input",
-        "input-data-uri",
-        "--output",
-        '{"Get": "Parameters.MyArgOutput"}',
-    ]
-
     run_inputs["inputs"] = processing_input
 
     step_args = processor.run(**run_inputs)
@@ -1044,7 +1030,7 @@ def test_spark_processor_local_code(spark_processor, processing_input, pipeline_
 
     step_args = get_step_args_helper(step_args, "Processing")
 
-    assert step_args["AppSpecification"]["ContainerArguments"] == arguments_output
+    assert step_args["AppSpecification"]["ContainerArguments"] == run_inputs["arguments"]
 
     entry_points = step_args["AppSpecification"]["ContainerEntrypoint"]
     entry_points_expr = []
