829030aaa8ff84ba2e5a2bbf594f6f890001c28a
Molly He <mollyhe@amazon.com>
2025-06-10 10:02:21 -0700

    Add ignore_patterns in ModelTrainer to ignore specific files/folders (#5194)
    * Add ignore_patterns in ModelTrainer to ignore specific files/folders
    
    * fix black format
    
    * add unit test
    
    * add default ignore_patterns, fix minor path issue when uploaded to s3
    
    * minor change to fix unit test failure
    
    * add new variables in default ignore_patterns
    
    * fix indentation error in docstring for readthedocs

--- src/sagemaker/modules/configs.py
+++ src/sagemaker/modules/configs.py
@@ -21,7 +21,7 @@ For more documentation on ``sagemaker_core.shapes``, see:
 
 from __future__ import absolute_import
 
-from typing import Optional, Union
+from typing import Optional, Union, List
 from pydantic import BaseModel, model_validator, ConfigDict
 
 import sagemaker_core.shapes as shapes
@@ -96,12 +96,23 @@ class SourceCode(BaseConfig):
         command (Optional[str]):
             The command(s) to execute in the training job container. Example: "python my_script.py".
             If not specified, entry_script must be provided.
+        ignore_patterns: (Optional[List[str]]) :
+            The ignore patterns to ignore specific files/folders when uploading to S3. If not specified,
+            default to: ['.env', '.git', '__pycache__', '.DS_Store', '.cache', '.ipynb_checkpoints'].
     """
 
     source_dir: Optional[str] = None
     requirements: Optional[str] = None
     entry_script: Optional[str] = None
     command: Optional[str] = None
+    ignore_patterns: Optional[List[str]] = [
+        ".env",
+        ".git",
+        "__pycache__",
+        ".DS_Store",
+        ".cache",
+        ".ipynb_checkpoints",
+    ]
 
 
 class Compute(shapes.ResourceConfig):
