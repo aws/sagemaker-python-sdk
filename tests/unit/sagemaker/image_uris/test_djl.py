c75f315bc3190df833b4dd3ecf18bc75e1f31ead
Malte Reimann <malterei@amazon.ch>
2025-09-12 16:54:28 +0200

    test: adds unit test for djl lmi regions

--- tests/unit/sagemaker/image_uris/test_djl.py
+++ tests/unit/sagemaker/image_uris/test_djl.py
@@ -41,3 +41,29 @@ def _test_djl_uris(account, region, version, tag, djl_framework):
         region,
     )
     assert expected == uri
+
+# Expected regions for DJL LMI based on documentation
+# https://github.com/aws/deep-learning-containers/blob/master/available_images.md
+EXPECTED_DJL_LMI_REGIONS = {
+    "us-east-1", "us-east-2", "us-west-1", "us-west-2",
+    "af-south-1", "ap-east-1", "ap-east-2", "ap-south-1", "ap-south-2",
+    "ap-southeast-1", "ap-southeast-2", "ap-southeast-3", "ap-southeast-4", 
+    "ap-southeast-5", "ap-southeast-7", "ap-northeast-1", "ap-northeast-2", 
+    "ap-northeast-3", "ca-central-1", "ca-west-1", "eu-central-1", 
+    "eu-central-2", "eu-west-1", "eu-west-2", "eu-west-3", "eu-north-1", 
+    "eu-south-1", "eu-south-2", "il-central-1", "mx-central-1", 
+    "me-south-1", "me-central-1", "sa-east-1", "cn-north-1", "cn-northwest-1"
+}
+
+
+def test_djl_lmi_config_for_framework_has_all_regions():
+    """Test that config_for_framework('djl-lmi') returns all expected regions for each version."""
+    config = image_uris.config_for_framework("djl-lmi")
+    
+    # Check that each version has all expected regions
+    for version, version_config in config["versions"].items():
+        actual_regions = set(version_config["registries"].keys())
+        missing_regions = EXPECTED_DJL_LMI_REGIONS - actual_regions
+        
+        assert not missing_regions, f"Version {version} missing regions: {missing_regions}"
+
