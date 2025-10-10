7daa34f25c0a1d27615e4916b82fb4d5748b7782
Roja Reddy Sareddy <rsareddy@amazon.com>
2025-10-09 22:35:01 -0700

    numpy fix for slow test

--- tests/integ/sagemaker/feature_store/feature_processor/test_feature_processor_integ.py
+++ tests/integ/sagemaker/feature_store/feature_processor/test_feature_processor_integ.py
@@ -373,7 +373,7 @@ def test_feature_processor_transform_with_customized_data_source(
         )
 
 
-# @pytest.mark.slow_test
+@pytest.mark.slow_test
 @pytest.mark.flaky(reruns=5, reruns_delay=2)
 def test_feature_processor_transform_offline_only_store_ingestion(
     sagemaker_session,
