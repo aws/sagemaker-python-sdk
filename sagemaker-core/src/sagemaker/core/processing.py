55a4ee5956e85594603fe56d4fac17ea77e38a9e
Gokul Anantha Narayanan <166456257+nargokul@users.noreply.github.com>
2026-03-06 13:15:23 -0800

    V3 Bug Fixes (#5601)
    * V3 Bug Fixes
    
    * fix(model_builder): Only set s3_upload_path for S3 URIs in passthrough
    
    In _build_for_passthrough(), model_path could be a local /tmp path.
    Setting s3_upload_path to a local path caused CreateModel API to reject
    the modelDataUrl with a validation error since it requires s3:// or
    https:// URIs. Now only S3 URIs are assigned to s3_upload_path; local
    paths are handled separately by _prepare_for_mode() in LOCAL_CONTAINER
    mode.
    
    * Test fixes
    
    * Bug fix 3 and 4

--- sagemaker-core/src/sagemaker/core/processing.py
+++ sagemaker-core/src/sagemaker/core/processing.py
@@ -487,6 +487,9 @@ class Processor(object):
                 # If the output's s3_uri is not an s3_uri, create one.
                 parse_result = urlparse(output.s3_output.s3_uri)
                 if parse_result.scheme != "s3":
+                    if getattr(self.sagemaker_session, "local_mode", False) and parse_result.scheme == "file":
+                        normalized_outputs.append(output)
+                        continue
                     if _pipeline_config:
                         s3_uri = Join(
                             on="/",
