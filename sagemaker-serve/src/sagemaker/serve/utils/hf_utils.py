192b22f8a4901c2967fbe0ed8307db463662e1e2
Enrique Hernández Calabrés <ehcalabres@gmail.com>
2026-04-07 10:39:42 +0200

    feat: add support for model_index.json fallback in HF config retrieval

--- sagemaker-serve/src/sagemaker/serve/utils/hf_utils.py
+++ sagemaker-serve/src/sagemaker/serve/utils/hf_utils.py
@@ -24,26 +24,35 @@ logger = logging.getLogger(__name__)
 def _get_model_config_properties_from_hf(model_id: str, hf_hub_token: str = None):
     """Placeholder docstring"""
 
-    config_url = f"https://huggingface.co/{model_id}/raw/main/config.json"
+    config_files = ["config.json", "model_index.json"]
+
     model_config = None
-    try:
-        if hf_hub_token:
-            config_url = urllib.request.Request(
-                config_url, headers={"Authorization": "Bearer " + hf_hub_token}
+    for config_file in config_files:
+        config_url = f"https://huggingface.co/{model_id}/raw/main/{config_file}"
+        request = config_url
+
+        try:
+            if hf_hub_token:
+                request = urllib.request.Request(
+                    config_url, headers={"Authorization": "Bearer " + hf_hub_token}
+                )
+
+            with urllib.request.urlopen(request) as response:
+                model_config = json.load(response)
+                break
+        except (HTTPError, URLError, TimeoutError, JSONDecodeError) as e:
+            if "HTTP Error 401: Unauthorized" in str(e):
+                raise ValueError(
+                    "Trying to access a gated/private HuggingFace model without valid credentials. "
+                    "Please provide a HUGGING_FACE_HUB_TOKEN in env_vars"
+                )
+
+            logger.warning(
+                "Exception encountered while trying to read config file %s. Details: %s",
+                config_url,
+                e,
             )
-        with urllib.request.urlopen(config_url) as response:
-            model_config = json.load(response)
-    except (HTTPError, URLError, TimeoutError, JSONDecodeError) as e:
-        if "HTTP Error 401: Unauthorized" in str(e):
-            raise ValueError(
-                "Trying to access a gated/private HuggingFace model without valid credentials. "
-                "Please provide a HUGGING_FACE_HUB_TOKEN in env_vars"
-            )
-        logger.warning(
-            "Exception encountered while trying to read config file %s. " "Details: %s",
-            config_url,
-            e,
-        )
+
     if not model_config:
         raise ValueError(
             f"Did not find a config.json or model_index.json file in huggingface hub for "
