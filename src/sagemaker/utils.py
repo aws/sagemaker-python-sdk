aws/sagemaker-python-sdk

commit 07471563aa88186ab7fd2e019ef8988d2896e2f5

fix: s3 bucket operations in V2 (#5803)
* fix: s3 bucket operations in V2

Add ExpectedBucketOwner to S3 data-plane calls

* Follow up test fixes

* fix unit tests

* fix unit test

src/sagemaker/utils.py
@@ -417,12 +417,21 @@ def download_folder(bucket_name, prefix, target, sagemaker_session):
 
     prefix = prefix.lstrip("/")
 
+    # Spot check: enforce ownership only when downloading from the session's default
+    # bucket. Cross-account buckets are left untouched.
+    expected_owner = sagemaker_session._get_account_id_if_default_bucket(bucket_name)
+    extra_args = None
+    if expected_owner:
+        extra_args = {"ExpectedBucketOwner": expected_owner}
+
     # Try to download the prefix as an object first, in case it is a file and not a 'directory'.
     # Do this first, in case the object has broader permissions than the bucket.
     if not prefix.endswith("/"):
         try:
             file_destination = os.path.join(target, os.path.basename(prefix))
-            s3.Object(bucket_name, prefix).download_file(file_destination)
+            s3.Object(bucket_name, prefix).download_file(
+                file_destination, ExtraArgs=extra_args
+            )
             return
         except botocore.exceptions.ClientError as e:
             err_info = e.response["Error"]
@@ -433,10 +442,10 @@ def download_folder(bucket_name, prefix, target, sagemaker_session):
             else:
                 raise
 
-    _download_files_under_prefix(bucket_name, prefix, target, s3)
+    _download_files_under_prefix(bucket_name, prefix, target, s3, extra_args=extra_args)
 
 
-def _download_files_under_prefix(bucket_name, prefix, target, s3):
+def _download_files_under_prefix(bucket_name, prefix, target, s3, extra_args=None):
     """Download all S3 files which match the given prefix
 
     Args:
@@ -444,6 +453,8 @@ def _download_files_under_prefix(bucket_name, prefix, target, s3):
         prefix (str): S3 prefix within the bucket that will be downloaded
         target (str): destination path where the downloaded items will be placed
         s3 (boto3.resources.base.ServiceResource): S3 resource
+        extra_args (dict): Optional extra arguments passed to each download_file call.
+            Used to carry ExpectedBucketOwner when the bucket is the session's default.
     """
     bucket = s3.Bucket(bucket_name)
     for obj_sum in bucket.objects.filter(Prefix=prefix):
@@ -461,7 +472,7 @@ def _download_files_under_prefix(bucket_name, prefix, target, s3):
             # anything else will be raised.
             if exc.errno != errno.EEXIST:
                 raise
-        obj.download_file(file_path)
+        obj.download_file(file_path, ExtraArgs=extra_args)
 
 
 def create_tar_file(source_files, target=None):
@@ -608,6 +619,16 @@ def _save_model(repacked_model_uri, tmp_model_path, sagemaker_session, kms_key):
             extra_args = {"ServerSideEncryption": "aws:kms"}
         else:
             extra_args = None
+
+        # Spot check: when the model is being uploaded to the session's default bucket,
+        # assert ownership to defend against bucket-squatting on the predictable default
+        # name. Other caller-supplied buckets are left untouched.
+        if sagemaker_session is not None:
+            expected_owner = sagemaker_session._get_account_id_if_default_bucket(bucket)
+            if expected_owner:
+                extra_args = dict(extra_args) if extra_args else {}
+                extra_args["ExpectedBucketOwner"] = expected_owner
+
         sagemaker_session.boto_session.resource(
             "s3", region_name=sagemaker_session.boto_region_name
         ).Object(bucket, new_key).upload_file(tmp_model_path, ExtraArgs=extra_args)
@@ -772,7 +793,17 @@ def download_file(bucket_name, path, target, sagemaker_session):
 
     s3 = boto_session.resource("s3", region_name=sagemaker_session.boto_region_name)
     bucket = s3.Bucket(bucket_name)
-    bucket.download_file(path, target)
+
+    # Spot check: assert ownership only when downloading from the session's default
+    # bucket. Non-default buckets (e.g. caller-supplied model URIs pointing at shared
+    # or cross-account data) are downloaded without ExpectedBucketOwner to preserve
+    # legitimate cross-account flows.
+    expected_owner = sagemaker_session._get_account_id_if_default_bucket(bucket_name)
+    extra_args = None
+    if expected_owner:
+        extra_args = {"ExpectedBucketOwner": expected_owner}
+
+    bucket.download_file(path, target, ExtraArgs=extra_args)
 
 
 def sts_regional_endpoint(region):

src/sagemaker/serve/model_format/mlflow/utils.py
@@ -244,8 +244,17 @@ def _download_s3_artifacts(s3_path: str, dst_path: str, session: Session) -> Non
 
     os.makedirs(dst_path, exist_ok=True)
 
+    # Spot check: enforce ownership only when downloading from the session's default
+    # bucket. Cross-account reads are left untouched.
+    expected_owner = session._get_account_id_if_default_bucket(s3_bucket)
+    extra_kwargs = {}
+    if expected_owner:
+        extra_kwargs["ExpectedBucketOwner"] = expected_owner
+
     paginator = s3.get_paginator("list_objects_v2")
-    for page in paginator.paginate(Bucket=s3_bucket, Prefix=s3_key):
+    paginate_kwargs = {"Bucket": s3_bucket, "Prefix": s3_key}
+    paginate_kwargs.update(extra_kwargs)
+    for page in paginator.paginate(**paginate_kwargs):
         for obj in page.get("Contents", []):
             key = obj["Key"]
             rel_path = os.path.relpath(key, s3_key)
@@ -257,7 +266,10 @@ def _download_s3_artifacts(s3_path: str, dst_path: str, session: Session) -> Non
 
                 # Download the file
                 logger.info(f"Downloading {key} to {local_file_path}")
-                s3.download_file(s3_bucket, key, local_file_path)
+                extra_args = None
+                if expected_owner:
+                    extra_args = {"ExpectedBucketOwner": expected_owner}
+                s3.download_file(s3_bucket, key, local_file_path, ExtraArgs=extra_args)
 
 
 def _copy_directory_contents(src_dir, dest_dir) -> None:

src/sagemaker/amazon/amazon_estimator.py
@@ -317,7 +317,8 @@ class AmazonAlgorithmEstimatorBase(EstimatorBase):
         key_prefix = key_prefix.lstrip("/")
         logger.debug("Uploading to bucket %s and key_prefix %s", bucket, key_prefix)
         manifest_s3_file = upload_numpy_to_s3_shards(
-            self.instance_count, s3, bucket, key_prefix, train, labels, encrypt
+            self.instance_count, s3, bucket, key_prefix, train, labels, encrypt,
+            sagemaker_session=self.sagemaker_session
         )
         logger.debug("Created manifest file %s", manifest_s3_file)
         return RecordSet(
@@ -455,7 +456,7 @@ def _build_shards(num_shards, array):
 
 
 def upload_numpy_to_s3_shards(
-    num_shards, s3, bucket, key_prefix, array, labels=None, encrypt=False
+    num_shards, s3, bucket, key_prefix, array, labels=None, encrypt=False, sagemaker_session=None
 ):
     """Upload the training ``array`` and ``labels`` arrays to ``num_shards``.
 
@@ -470,6 +471,8 @@ def upload_numpy_to_s3_shards(
         array:
         labels:
         encrypt:
+        sagemaker_session: Optional. SageMaker session used to resolve the
+            ExpectedBucketOwner spot check for the default bucket.
     """
     shards = _build_shards(num_shards, array)
     if labels is not None:
@@ -478,6 +481,12 @@ def upload_numpy_to_s3_shards(
     if key_prefix[-1] != "/":
         key_prefix = key_prefix + "/"
     extra_put_kwargs = {"ServerSideEncryption": "AES256"} if encrypt else {}
+    # Spot check: enforce ownership only when uploading to the session's default
+    # bucket. Cross-account destinations are left untouched.
+    if sagemaker_session is not None:
+        expected_owner = sagemaker_session._get_account_id_if_default_bucket(bucket)
+        if expected_owner:
+            extra_put_kwargs["ExpectedBucketOwner"] = expected_owner
     try:
         for shard_index, shard in enumerate(shards):
             with tempfile.TemporaryFile() as file:

src/sagemaker/async_inference/async_inference_response.py
@@ -97,7 +97,11 @@ class AsyncInferenceResponse(object):
         """Get inference result from the output Amazon S3 path"""
         bucket, key = parse_s3_url(output_path)
         try:
-            response = self.predictor_async.s3_client.get_object(Bucket=bucket, Key=key)
+            get_kwargs = {"Bucket": bucket, "Key": key}
+            expected_owner = self.predictor_async.sagemaker_session._get_account_id_if_default_bucket(bucket)
+            if expected_owner:
+                get_kwargs["ExpectedBucketOwner"] = expected_owner
+            response = self.predictor_async.s3_client.get_object(**get_kwargs)
             return self.predictor_async.predictor._handle_response(response)
         except ClientError as ex:
             if ex.response["Error"]["Code"] == "NoSuchKey":
@@ -113,14 +117,22 @@ class AsyncInferenceResponse(object):
         """Get inference result from the output & failure Amazon S3 path"""
         bucket, key = parse_s3_url(output_path)
         try:
-            response = self.predictor_async.s3_client.get_object(Bucket=bucket, Key=key)
+            get_kwargs = {"Bucket": bucket, "Key": key}
+            expected_owner = self.predictor_async.sagemaker_session._get_account_id_if_default_bucket(bucket)
+            if expected_owner:
+                get_kwargs["ExpectedBucketOwner"] = expected_owner
+            response = self.predictor_async.s3_client.get_object(**get_kwargs)
             return self.predictor_async.predictor._handle_response(response)
         except ClientError as e:
             if e.response["Error"]["Code"] == "NoSuchKey":
                 try:
                     failure_bucket, failure_key = parse_s3_url(failure_path)
+                    fail_kwargs = {"Bucket": failure_bucket, "Key": failure_key}
+                    fail_owner = self.predictor_async.sagemaker_session._get_account_id_if_default_bucket(failure_bucket)
+                    if fail_owner:
+                        fail_kwargs["ExpectedBucketOwner"] = fail_owner
                     failure_response = self.predictor_async.s3_client.get_object(
-                        Bucket=failure_bucket, Key=failure_key
+                        **fail_kwargs
                     )
                     failure_response = self.predictor_async.predictor._handle_response(
                         failure_response

src/sagemaker/estimator.py
@@ -1105,6 +1105,9 @@ class EstimatorBase(with_metaclass(ABCMeta, object)):  # pylint: disable=too-man
             kms_key=kms_key,
             s3_resource=self.sagemaker_session.s3_resource,
             settings=self.sagemaker_session.settings,
+            expected_bucket_owner=self.sagemaker_session._get_account_id_if_default_bucket(
+                code_bucket
+            ),
         )
 
     def _assign_s3_prefix(self, key_prefix=""):

src/sagemaker/experiments/_helper.py
@@ -95,6 +95,16 @@ class _ArtifactUploader(object):
         artifact_s3_key = "{}/{}/{}".format(
             self.artifact_prefix, self.trial_component_name, artifact_name
         )
+
+        # Spot check: enforce ownership only when uploading to the session's default
+        # bucket. Cross-account destinations are left untouched.
+        expected_owner = self.sagemaker_session._get_account_id_if_default_bucket(
+            self.artifact_bucket
+        )
+        if expected_owner:
+            extra_args = dict(extra_args) if extra_args else {}
+            extra_args["ExpectedBucketOwner"] = expected_owner
+
         self._s3_client.upload_file(
             file_path,
             self.artifact_bucket,
@@ -133,9 +143,21 @@ class _ArtifactUploader(object):
         artifact_s3_key = "{}/{}/{}".format(
             self.artifact_prefix, self.trial_component_name, artifact_name
         )
-        self._s3_client.put_object(
-            Body=json.dumps(artifact_object), Bucket=self.artifact_bucket, Key=artifact_s3_key
+
+        # Spot check: enforce ownership only when uploading to the session's default
+        # bucket. Cross-account destinations are left untouched.
+        put_kwargs = {
+            "Body": json.dumps(artifact_object),
+            "Bucket": self.artifact_bucket,
+            "Key": artifact_s3_key,
+        }
+        expected_owner = self.sagemaker_session._get_account_id_if_default_bucket(
+            self.artifact_bucket
         )
+        if expected_owner:
+            put_kwargs["ExpectedBucketOwner"] = expected_owner
+
+        self._s3_client.put_object(**put_kwargs)
         etag = self._try_get_etag(artifact_s3_key)
         return "s3://{}/{}".format(self.artifact_bucket, artifact_s3_key), etag
 
@@ -149,7 +171,14 @@ class _ArtifactUploader(object):
             str: The S3 object ETag if it allows, otherwise return None.
         """
         try:
-            response = self._s3_client.head_object(Bucket=self.artifact_bucket, Key=key)
+            head_kwargs = {"Bucket": self.artifact_bucket, "Key": key}
+            expected_owner = self.sagemaker_session._get_account_id_if_default_bucket(
+                self.artifact_bucket
+            )
+            if expected_owner:
+                head_kwargs["ExpectedBucketOwner"] = expected_owner
+
+            response = self._s3_client.head_object(**head_kwargs)
             return response["ETag"]
         except botocore.exceptions.ClientError as error:
             # requires read permissions

src/sagemaker/fw_utils.py
@@ -401,6 +401,7 @@ def tar_and_upload_dir(
     kms_key=None,
     s3_resource=None,
     settings: Optional[SessionSettings] = None,
+    expected_bucket_owner: Optional[str] = None,
 ) -> UploadedCode:
     """Package source files and upload a compress tar file to S3.
 
@@ -431,6 +432,12 @@ def tar_and_upload_dir(
         settings (sagemaker.session_settings.SessionSettings): Optional. The settings
             of the SageMaker ``Session``, can be used to override the default encryption
             behavior (default: None).
+        expected_bucket_owner (str): Optional. AWS account id passed as
+            ``ExpectedBucketOwner`` on the upload. Callers should supply this when
+            ``bucket`` is the session's default bucket (via
+            ``Session._get_account_id_if_default_bucket``) to defend against
+            bucket-squatting on the predictable default name. Leave as ``None`` for
+            cross-account destination buckets.
     Returns:
         sagemaker.fw_utils.UploadedCode: An object with the S3 bucket and key (S3 prefix) and
             script name.
@@ -471,6 +478,10 @@ def tar_and_upload_dir(
             extra_args = {"ServerSideEncryption": "aws:kms"}
         else:
             extra_args = None
+
+        if expected_bucket_owner:
+            extra_args = dict(extra_args) if extra_args else {}
+            extra_args["ExpectedBucketOwner"] = expected_bucket_owner
 
         if s3_resource is None:
             s3_resource = session.resource("s3", region_name=session.region_name)

src/sagemaker/lambda_helper.py
@@ -123,12 +123,17 @@ class Lambda:
             bucket, key_prefix = s3.determine_bucket_and_prefix(
                 bucket=self.s3_bucket, key_prefix=None, sagemaker_session=self.session
             )
+            # Spot check: if the resolved bucket is the session's default bucket,
+            # enforce ownership on the upload so an attacker cannot squat on the
+            # predictable default name.
+            expected_owner = self.session._get_account_id_if_default_bucket(bucket)
             key = _upload_to_s3(
                 s3_client=_get_s3_client(self.session),
                 function_name=self.function_name,
                 zipped_code_dir=self.zipped_code_dir,
                 s3_bucket=bucket,
                 s3_key_prefix=key_prefix,
+                expected_bucket_owner=expected_owner,
             )
             code = {"S3Bucket": bucket, "S3Key": key}
 
@@ -179,6 +184,13 @@ class Lambda:
                     else:
                         function_name_for_s3 = self.function_name
 
+                    # Spot check: enforce ownership only when the resolved bucket is
+                    # the session's default bucket (defends against squatting on the
+                    # predictable default name). Other buckets are left untouched.
+                    expected_owner = self.session._get_account_id_if_default_bucket(
+                        bucket
+                    )
+
                     response = lambda_client.update_function_code(
                         FunctionName=(self.function_name or self.function_arn),
                         S3Bucket=bucket,
@@ -188,6 +200,7 @@ class Lambda:
                             zipped_code_dir=self.zipped_code_dir,
                             s3_bucket=bucket,
                             s3_key_prefix=key_prefix,
+                            expected_bucket_owner=expected_owner,
                         ),
                     )
                 return response
@@ -276,12 +289,28 @@ def _get_lambda_client(session):
     return lambda_client
 
 
-def _upload_to_s3(s3_client, function_name, zipped_code_dir, s3_bucket, s3_key_prefix=None):
+def _upload_to_s3(
+    s3_client,
+    function_name,
+    zipped_code_dir,
+    s3_bucket,
+    s3_key_prefix=None,
+    expected_bucket_owner=None,
+):
     """Upload the zipped code to S3 bucket provided in the Lambda instance.
 
     Lambda instance must have a path to the zipped code folder and a S3 bucket to upload
     the code. The key will lambda/function_name/code and the S3 URI where the code is
     uploaded is in this format: s3://bucket_name/lambda/function_name/code.
+
+    Args:
+        s3_client: boto3 S3 client used for the upload.
+        function_name (str): Lambda function name used to build the S3 key.
+        zipped_code_dir (str): Local path to the zipped Lambda code.
+        s3_bucket (str): Destination S3 bucket.
+        s3_key_prefix (str): Optional S3 key prefix.
+        expected_bucket_owner (str): Optional account id passed as ``ExpectedBucketOwner``
+            on the upload when the destination bucket should belong to that account.
 
     Returns: the S3 key where the code is uploaded.
     """
@@ -292,7 +321,10 @@ def _upload_to_s3(s3_client, function_name, zipped_code_dir, s3_bucket, s3_key_p
         function_name,
         "code",
     )
-    s3_client.upload_file(zipped_code_dir, s3_bucket, key)
+    extra_args = None
+    if expected_bucket_owner:
+        extra_args = {"ExpectedBucketOwner": expected_bucket_owner}
+    s3_client.upload_file(zipped_code_dir, s3_bucket, key, ExtraArgs=extra_args)
     return key
 
 

commit 648624571c6911a20f287eabce087211298a37bf

fix: regression issue caused by #5803

src/sagemaker/utils.py
@@ -419,7 +419,11 @@ def download_folder(bucket_name, prefix, target, sagemaker_session):
 
     # Spot check: enforce ownership only when downloading from the session's default
     # bucket. Cross-account buckets are left untouched.
-    expected_owner = sagemaker_session._get_account_id_if_default_bucket(bucket_name)
+    expected_owner = (
+        sagemaker_session._get_account_id_if_default_bucket(bucket_name)
+        if hasattr(sagemaker_session, "_get_account_id_if_default_bucket")
+        else None
+    )
     extra_args = None
     if expected_owner:
         extra_args = {"ExpectedBucketOwner": expected_owner}