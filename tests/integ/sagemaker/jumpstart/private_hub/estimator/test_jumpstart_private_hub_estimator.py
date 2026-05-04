aws/sagemaker-python-sdk

commit 07471563aa88186ab7fd2e019ef8988d2896e2f5

fix: s3 bucket operations in V2 (#5803)
* fix: s3 bucket operations in V2

Add ExpectedBucketOwner to S3 data-plane calls

* Follow up test fixes

* fix unit tests

* fix unit test

src/sagemaker/utils.py
@@ -417,12 +417,21 @@ def download_folder(bucket_name, prefix, target, sagemaker_session):
 
     prefix = prefix.lstrip("/")
 
+    # Spot check: enforce ownership only when downloading from the session's default
+    # bucket. Cross-account buckets are left untouched.
+    expected_owner = sagemaker_session._get_account_id_if_default_bucket(bucket_name)
+    extra_args = None
+    if expected_owner:
+        extra_args = {"ExpectedBucketOwner": expected_owner}
+
     # Try to download the prefix as an object first, in case it is a file and not a 'directory'.
     # Do this first, in case the object has broader permissions than the bucket.
     if not prefix.endswith("/"):
         try:
             file_destination = os.path.join(target, os.path.basename(prefix))
-            s3.Object(bucket_name, prefix).download_file(file_destination)
+            s3.Object(bucket_name, prefix).download_file(
+                file_destination, ExtraArgs=extra_args
+            )
             return
         except botocore.exceptions.ClientError as e:
             err_info = e.response["Error"]
@@ -433,10 +442,10 @@ def download_folder(bucket_name, prefix, target, sagemaker_session):
             else:
                 raise
 
-    _download_files_under_prefix(bucket_name, prefix, target, s3)
+    _download_files_under_prefix(bucket_name, prefix, target, s3, extra_args=extra_args)
 
 
-def _download_files_under_prefix(bucket_name, prefix, target, s3):
+def _download_files_under_prefix(bucket_name, prefix, target, s3, extra_args=None):
     """Download all S3 files which match the given prefix
 
     Args:
@@ -444,6 +453,8 @@ def _download_files_under_prefix(bucket_name, prefix, target, s3):
         prefix (str): S3 prefix within the bucket that will be downloaded
         target (str): destination path where the downloaded items will be placed
         s3 (boto3.resources.base.ServiceResource): S3 resource
+        extra_args (dict): Optional extra arguments passed to each download_file call.
+            Used to carry ExpectedBucketOwner when the bucket is the session's default.
     """
     bucket = s3.Bucket(bucket_name)
     for obj_sum in bucket.objects.filter(Prefix=prefix):
@@ -461,7 +472,7 @@ def _download_files_under_prefix(bucket_name, prefix, target, s3):
             # anything else will be raised.
             if exc.errno != errno.EEXIST:
                 raise
-        obj.download_file(file_path)
+        obj.download_file(file_path, ExtraArgs=extra_args)
 
 
 def create_tar_file(source_files, target=None):
@@ -608,6 +619,16 @@ def _save_model(repacked_model_uri, tmp_model_path, sagemaker_session, kms_key):
             extra_args = {"ServerSideEncryption": "aws:kms"}
         else:
             extra_args = None
+
+        # Spot check: when the model is being uploaded to the session's default bucket,
+        # assert ownership to defend against bucket-squatting on the predictable default
+        # name. Other caller-supplied buckets are left untouched.
+        if sagemaker_session is not None:
+            expected_owner = sagemaker_session._get_account_id_if_default_bucket(bucket)
+            if expected_owner:
+                extra_args = dict(extra_args) if extra_args else {}
+                extra_args["ExpectedBucketOwner"] = expected_owner
+
         sagemaker_session.boto_session.resource(
             "s3", region_name=sagemaker_session.boto_region_name
         ).Object(bucket, new_key).upload_file(tmp_model_path, ExtraArgs=extra_args)
@@ -772,7 +793,17 @@ def download_file(bucket_name, path, target, sagemaker_session):
 
     s3 = boto_session.resource("s3", region_name=sagemaker_session.boto_region_name)
     bucket = s3.Bucket(bucket_name)
-    bucket.download_file(path, target)
+
+    # Spot check: assert ownership only when downloading from the session's default
+    # bucket. Non-default buckets (e.g. caller-supplied model URIs pointing at shared
+    # or cross-account data) are downloaded without ExpectedBucketOwner to preserve
+    # legitimate cross-account flows.
+    expected_owner = sagemaker_session._get_account_id_if_default_bucket(bucket_name)
+    extra_args = None
+    if expected_owner:
+        extra_args = {"ExpectedBucketOwner": expected_owner}
+
+    bucket.download_file(path, target, ExtraArgs=extra_args)
 
 
 def sts_regional_endpoint(region):

src/sagemaker/serve/model_format/mlflow/utils.py
@@ -244,8 +244,17 @@ def _download_s3_artifacts(s3_path: str, dst_path: str, session: Session) -> Non
 
     os.makedirs(dst_path, exist_ok=True)
 
+    # Spot check: enforce ownership only when downloading from the session's default
+    # bucket. Cross-account reads are left untouched.
+    expected_owner = session._get_account_id_if_default_bucket(s3_bucket)
+    extra_kwargs = {}
+    if expected_owner:
+        extra_kwargs["ExpectedBucketOwner"] = expected_owner
+
     paginator = s3.get_paginator("list_objects_v2")
-    for page in paginator.paginate(Bucket=s3_bucket, Prefix=s3_key):
+    paginate_kwargs = {"Bucket": s3_bucket, "Prefix": s3_key}
+    paginate_kwargs.update(extra_kwargs)
+    for page in paginator.paginate(**paginate_kwargs):
         for obj in page.get("Contents", []):
             key = obj["Key"]
             rel_path = os.path.relpath(key, s3_key)
@@ -257,7 +266,10 @@ def _download_s3_artifacts(s3_path: str, dst_path: str, session: Session) -> Non
 
                 # Download the file
                 logger.info(f"Downloading {key} to {local_file_path}")
-                s3.download_file(s3_bucket, key, local_file_path)
+                extra_args = None
+                if expected_owner:
+                    extra_args = {"ExpectedBucketOwner": expected_owner}
+                s3.download_file(s3_bucket, key, local_file_path, ExtraArgs=extra_args)
 
 
 def _copy_directory_contents(src_dir, dest_dir) -> None:

src/sagemaker/amazon/amazon_estimator.py
@@ -317,7 +317,8 @@ class AmazonAlgorithmEstimatorBase(EstimatorBase):
         key_prefix = key_prefix.lstrip("/")
         logger.debug("Uploading to bucket %s and key_prefix %s", bucket, key_prefix)
         manifest_s3_file = upload_numpy_to_s3_shards(
-            self.instance_count, s3, bucket, key_prefix, train, labels, encrypt
+            self.instance_count, s3, bucket, key_prefix, train, labels, encrypt,
+            sagemaker_session=self.sagemaker_session
         )
         logger.debug("Created manifest file %s", manifest_s3_file)
         return RecordSet(
@@ -455,7 +456,7 @@ def _build_shards(num_shards, array):
 
 
 def upload_numpy_to_s3_shards(
-    num_shards, s3, bucket, key_prefix, array, labels=None, encrypt=False
+    num_shards, s3, bucket, key_prefix, array, labels=None, encrypt=False, sagemaker_session=None
 ):
     """Upload the training ``array`` and ``labels`` arrays to ``num_shards``.
 
@@ -470,6 +471,8 @@ def upload_numpy_to_s3_shards(
         array:
         labels:
         encrypt:
+        sagemaker_session: Optional. SageMaker session used to resolve the
+            ExpectedBucketOwner spot check for the default bucket.
     """
     shards = _build_shards(num_shards, array)
     if labels is not None:
@@ -478,6 +481,12 @@ def upload_numpy_to_s3_shards(
     if key_prefix[-1] != "/":
         key_prefix = key_prefix + "/"
     extra_put_kwargs = {"ServerSideEncryption": "AES256"} if encrypt else {}
+    # Spot check: enforce ownership only when uploading to the session's default
+    # bucket. Cross-account destinations are left untouched.
+    if sagemaker_session is not None:
+        expected_owner = sagemaker_session._get_account_id_if_default_bucket(bucket)
+        if expected_owner:
+            extra_put_kwargs["ExpectedBucketOwner"] = expected_owner
     try:
         for shard_index, shard in enumerate(shards):
             with tempfile.TemporaryFile() as file:

src/sagemaker/async_inference/async_inference_response.py
@@ -97,7 +97,11 @@ class AsyncInferenceResponse(object):
         """Get inference result from the output Amazon S3 path"""
         bucket, key = parse_s3_url(output_path)
         try:
-            response = self.predictor_async.s3_client.get_object(Bucket=bucket, Key=key)
+            get_kwargs = {"Bucket": bucket, "Key": key}
+            expected_owner = self.predictor_async.sagemaker_session._get_account_id_if_default_bucket(bucket)
+            if expected_owner:
+                get_kwargs["ExpectedBucketOwner"] = expected_owner
+            response = self.predictor_async.s3_client.get_object(**get_kwargs)
             return self.predictor_async.predictor._handle_response(response)
         except ClientError as ex:
             if ex.response["Error"]["Code"] == "NoSuchKey":
@@ -113,14 +117,22 @@ class AsyncInferenceResponse(object):
         """Get inference result from the output & failure Amazon S3 path"""
         bucket, key = parse_s3_url(output_path)
         try:
-            response = self.predictor_async.s3_client.get_object(Bucket=bucket, Key=key)
+            get_kwargs = {"Bucket": bucket, "Key": key}
+            expected_owner = self.predictor_async.sagemaker_session._get_account_id_if_default_bucket(bucket)
+            if expected_owner:
+                get_kwargs["ExpectedBucketOwner"] = expected_owner
+            response = self.predictor_async.s3_client.get_object(**get_kwargs)
             return self.predictor_async.predictor._handle_response(response)
         except ClientError as e:
             if e.response["Error"]["Code"] == "NoSuchKey":
                 try:
                     failure_bucket, failure_key = parse_s3_url(failure_path)
+                    fail_kwargs = {"Bucket": failure_bucket, "Key": failure_key}
+                    fail_owner = self.predictor_async.sagemaker_session._get_account_id_if_default_bucket(failure_bucket)
+                    if fail_owner:
+                        fail_kwargs["ExpectedBucketOwner"] = fail_owner
                     failure_response = self.predictor_async.s3_client.get_object(
-                        Bucket=failure_bucket, Key=failure_key
+                        **fail_kwargs
                     )
                     failure_response = self.predictor_async.predictor._handle_response(
                         failure_response

src/sagemaker/estimator.py
@@ -1105,6 +1105,9 @@ class EstimatorBase(with_metaclass(ABCMeta, object)):  # pylint: disable=too-man
             kms_key=kms_key,
             s3_resource=self.sagemaker_session.s3_resource,
             settings=self.sagemaker_session.settings,
+            expected_bucket_owner=self.sagemaker_session._get_account_id_if_default_bucket(
+                code_bucket
+            ),
         )
 
     def _assign_s3_prefix(self, key_prefix=""):

src/sagemaker/experiments/_helper.py
@@ -95,6 +95,16 @@ class _ArtifactUploader(object):
         artifact_s3_key = "{}/{}/{}".format(
             self.artifact_prefix, self.trial_component_name, artifact_name
         )
+
+        # Spot check: enforce ownership only when uploading to the session's default
+        # bucket. Cross-account destinations are left untouched.
+        expected_owner = self.sagemaker_session._get_account_id_if_default_bucket(
+            self.artifact_bucket
+        )
+        if expected_owner:
+            extra_args = dict(extra_args) if extra_args else {}
+            extra_args["ExpectedBucketOwner"] = expected_owner
+
         self._s3_client.upload_file(
             file_path,
             self.artifact_bucket,
@@ -133,9 +143,21 @@ class _ArtifactUploader(object):
         artifact_s3_key = "{}/{}/{}".format(
             self.artifact_prefix, self.trial_component_name, artifact_name
         )
-        self._s3_client.put_object(
-            Body=json.dumps(artifact_object), Bucket=self.artifact_bucket, Key=artifact_s3_key
+
+        # Spot check: enforce ownership only when uploading to the session's default
+        # bucket. Cross-account destinations are left untouched.
+        put_kwargs = {
+            "Body": json.dumps(artifact_object),
+            "Bucket": self.artifact_bucket,
+            "Key": artifact_s3_key,
+        }
+        expected_owner = self.sagemaker_session._get_account_id_if_default_bucket(
+            self.artifact_bucket
         )
+        if expected_owner:
+            put_kwargs["ExpectedBucketOwner"] = expected_owner
+
+        self._s3_client.put_object(**put_kwargs)
         etag = self._try_get_etag(artifact_s3_key)
         return "s3://{}/{}".format(self.artifact_bucket, artifact_s3_key), etag
 
@@ -149,7 +171,14 @@ class _ArtifactUploader(object):
             str: The S3 object ETag if it allows, otherwise return None.
         """
         try:
-            response = self._s3_client.head_object(Bucket=self.artifact_bucket, Key=key)
+            head_kwargs = {"Bucket": self.artifact_bucket, "Key": key}
+            expected_owner = self.sagemaker_session._get_account_id_if_default_bucket(
+                self.artifact_bucket
+            )
+            if expected_owner:
+                head_kwargs["ExpectedBucketOwner"] = expected_owner
+
+            response = self._s3_client.head_object(**head_kwargs)
             return response["ETag"]
         except botocore.exceptions.ClientError as error:
             # requires read permissions

commit 338e095d2105f23f21c7ade15c610f34fbcb8596

fix: fix v2 integ tests (#5822)
* fix: fix v2 integ tests

* fix: regression issue caused by #5803

* fix: InsufficientInstanceCapacity for ml.g5.12xlarge, try ml.g5.48xlarge

* fix: try ml.g5.24xlarge

* fix: test_jumpstart_estimator ml.p3.2xlarge deprecated use ml.g4dn.xlarge instead

* fix: handle hub teardown error in jumpstart estimator integ test

* fix: traceback catch

.gitignore
@@ -18,6 +18,8 @@ scratch*.py
 examples/tensorflow/distributed_mnist/data
 *.iml
 doc/_build
+docs/_build
+docs/api/generated
 doc/_static
 doc/_templates
 **/.DS_Store

src/sagemaker/modules/local_core/local_container.py
@@ -204,12 +204,18 @@ class _LocalContainer(BaseModel):
         # Print our Job Complete line
         logger.info("Local training job completed, output artifacts saved to %s", artifacts)
 
-        shutil.rmtree(os.path.join(self.container_root, "input"))
-        shutil.rmtree(os.path.join(self.container_root, "shared"))
+        for dir_name in ["input", "shared"]:
+            dir_path = os.path.join(self.container_root, dir_name)
+            if os.path.exists(dir_path):
+                shutil.rmtree(dir_path, ignore_errors=True)
         for host in self.hosts:
-            shutil.rmtree(os.path.join(self.container_root, host))
+            host_path = os.path.join(self.container_root, host)
+            if os.path.exists(host_path):
+                shutil.rmtree(host_path, ignore_errors=True)
         for folder in self._temporary_folders:
-            shutil.rmtree(os.path.join(self.container_root, folder))
+            folder_path = os.path.join(self.container_root, folder)
+            if os.path.exists(folder_path):
+                shutil.rmtree(folder_path, ignore_errors=True)
         return artifacts
 
     def retrieve_artifacts(

src/sagemaker/utils.py
@@ -419,7 +419,11 @@ def download_folder(bucket_name, prefix, target, sagemaker_session):
 
     # Spot check: enforce ownership only when downloading from the session's default
     # bucket. Cross-account buckets are left untouched.
-    expected_owner = sagemaker_session._get_account_id_if_default_bucket(bucket_name)
+    expected_owner = (
+        sagemaker_session._get_account_id_if_default_bucket(bucket_name)
+        if hasattr(sagemaker_session, "_get_account_id_if_default_bucket")
+        else None
+    )
     extra_args = None
     if expected_owner:
         extra_args = {"ExpectedBucketOwner": expected_owner}

tests/conftest.py
@@ -641,42 +641,17 @@ def cpu_instance_type(sagemaker_session, request):
 
 @pytest.fixture(scope="session")
 def gpu_instance_type(sagemaker_session, request):
-    region = sagemaker_session.boto_session.region_name
-    if region in NO_P3_REGIONS:
-        return "ml.p2.xlarge"
-    else:
-        return "ml.p3.2xlarge"
+    return "ml.g4dn.xlarge"
 
 
 @pytest.fixture()
 def gpu_pytorch_instance_type(sagemaker_session, request):
-    fw_version = None
-    for pytorch_version_fixture in [
-        "pytorch_inference_version",
-        "huggingface_training_pytorch_latest_version",
-        "huggingface_inference_pytorch_latest_version",
-    ]:
-        if pytorch_version_fixture in request.fixturenames:
-            fw_version = request.getfixturevalue(pytorch_version_fixture)
-    if fw_version is None:
-        fw_version = request.param
-    region = sagemaker_session.boto_session.region_name
-    if region in NO_P3_REGIONS:
-        if Version(fw_version) >= Version("1.13"):
-            return PYTORCH_RENEWED_GPU
-        else:
-            return "ml.p2.xlarge"
-    else:
-        return "ml.p3.2xlarge"
+    return "ml.g4dn.xlarge"
 
 
 @pytest.fixture(scope="session")
 def gpu_instance_type_list(sagemaker_session, request):
-    region = sagemaker_session.boto_session.region_name
-    if region in NO_P3_REGIONS:
-        return ["ml.p2.xlarge"]
-    else:
-        return ["ml.p3.2xlarge", "ml.p2.xlarge"]
+    return ["ml.g4dn.xlarge"]
 
 
 @pytest.fixture(scope="session")
@@ -717,16 +692,7 @@ def pytest_generate_tests(metafunc):
         cpu_instance_type = "ml.m5.xlarge" if region in NO_M4_REGIONS else "ml.m4.xlarge"
 
         params = [cpu_instance_type]
-        if not (
-            region in tests.integ.HOSTING_NO_P3_REGIONS
-            or region in tests.integ.TRAINING_NO_P3_REGIONS
-        ):
-            params.append("ml.p3.2xlarge")
-        elif not (
-            region in tests.integ.HOSTING_NO_P2_REGIONS
-            or region in tests.integ.TRAINING_NO_P2_REGIONS
-        ):
-            params.append("ml.p2.xlarge")
+        params.append("ml.g4dn.xlarge")
 
         metafunc.parametrize("instance_type", params, scope="session")
 

tests/integ/sagemaker/jumpstart/conftest.py
@@ -186,13 +186,19 @@ def _cleanup_old_hubs(sagemaker_session):
 
 def _delete_hubs(sagemaker_session, hub_name):
     # list and delete all hub contents first
-    list_hub_content_response = sagemaker_session.list_hub_contents(
-        hub_name=hub_name, hub_content_type=HubContentType.MODEL_REFERENCE.value
-    )
-    for model in list_hub_content_response["HubContentSummaries"]:
-        _delete_hub_contents(sagemaker_session, hub_name, model)
+    try:
+        list_hub_content_response = sagemaker_session.list_hub_contents(
+            hub_name=hub_name, hub_content_type=HubContentType.MODEL_REFERENCE.value
+        )
+        for model in list_hub_content_response["HubContentSummaries"]:
+            _delete_hub_contents(sagemaker_session, hub_name, model)
 
-    sagemaker_session.delete_hub(hub_name)
+        sagemaker_session.delete_hub(hub_name)
+    except Exception as e:
+        if "ResourceNotFound" in str(e):
+            print(f"Hub {hub_name} does not exist, skipping deletion.")
+        else:
+            raise
 
 
 @with_exponential_backoff()

tests/integ/sagemaker/jumpstart/estimator/test_jumpstart_estimator.py
@@ -60,9 +60,10 @@ def test_jumpstart_estimator(setup):
         sagemaker_session=get_sm_session(),
         tags=[{"Key": JUMPSTART_TAG, "Value": os.environ[ENV_VAR_JUMPSTART_SDK_TEST_SUITE_ID]}],
         max_run=259200,  # avoid exceeding resource limits
+        instance_type="ml.g4dn.xlarge",
     )
 
-    # uses ml.p3.2xlarge instance
+    # uses ml.g4dn.xlarge instance
     estimator.fit(
         {
             "training": f"s3://{get_jumpstart_content_bucket(JUMPSTART_DEFAULT_REGION_NAME)}/"
@@ -78,11 +79,12 @@ def test_jumpstart_estimator(setup):
         sagemaker_session=get_sm_session(),
     )
 
-    # uses ml.p3.2xlarge instance
+    # uses ml.g4dn.xlarge instance
     predictor = estimator.deploy(
         tags=[{"Key": JUMPSTART_TAG, "Value": os.environ[ENV_VAR_JUMPSTART_SDK_TEST_SUITE_ID]}],
         role=get_sm_session().get_caller_identity_arn(),
         sagemaker_session=get_sm_session(),
+        instance_type="ml.g4dn.xlarge",
     )
 
     response = predictor.predict(["hello", "world"])

tests/integ/sagemaker/jumpstart/model/test_jumpstart_model.py
@@ -95,9 +95,10 @@ def test_prepacked_jumpstart_model(setup):
         sagemaker_session=get_sm_session(),
     )
 
-    # uses ml.p3.2xlarge instance
+    # uses ml.g4dn.xlarge instance
     predictor = model.deploy(
         tags=[{"Key": JUMPSTART_TAG, "Value": os.environ[ENV_VAR_JUMPSTART_SDK_TEST_SUITE_ID]}],
+        instance_type="ml.g4dn.xlarge",
     )
 
     response = predictor.predict("hello world!")
@@ -120,7 +121,7 @@ def test_model_package_arn_jumpstart_model(setup):
         sagemaker_session=get_sm_session(),
     )
 
-    # uses ml.g5.2xlarge instance
+    # uses ml.g4dn.2xlarge instance
     predictor = model.deploy(
         tags=[{"Key": JUMPSTART_TAG, "Value": os.environ[ENV_VAR_JUMPSTART_SDK_TEST_SUITE_ID]}],
     )
@@ -175,7 +176,7 @@ def test_jumpstart_gated_model(setup):
         sagemaker_session=get_sm_session(),
     )
 
-    # uses ml.g5.2xlarge instance
+    # uses ml.g4dn.2xlarge instance
     predictor = model.deploy(
         tags=[{"Key": JUMPSTART_TAG, "Value": os.environ[ENV_VAR_JUMPSTART_SDK_TEST_SUITE_ID]}],
         accept_eula=True,
@@ -206,6 +207,7 @@ def test_jumpstart_gated_model_inference_component_enabled(setup):
         tags=[{"Key": JUMPSTART_TAG, "Value": os.environ[ENV_VAR_JUMPSTART_SDK_TEST_SUITE_ID]}],
         accept_eula=True,
         endpoint_type=EndpointType.INFERENCE_COMPONENT_BASED,
+        instance_type="ml.g5.24xlarge",
     )
 
     predictor = retrieve_default(
@@ -262,7 +264,7 @@ def test_jumpstart_model_register(setup):
 
     # uses  instance
     predictor = model_package.deploy(
-        instance_type="ml.p3.2xlarge",
+        instance_type="ml.g4dn.xlarge",
         initial_instance_count=1,
     )
 
@@ -379,7 +381,7 @@ def test_jumpstart_model_with_deployment_configs(setup):
 
     model.set_deployment_config(
         configs[0]["ConfigName"],
-        "ml.g5.2xlarge",
+        "ml.g4dn.2xlarge",
     )
     assert model.config_name == configs[0]["ConfigName"]
 

tests/integ/sagemaker/jumpstart/private_hub/estimator/test_jumpstart_private_hub_estimator.py
@@ -69,6 +69,7 @@ def test_jumpstart_hub_estimator(setup, add_model_references):
         model_id=model_id,
         hub_name=os.environ[ENV_VAR_JUMPSTART_SDK_TEST_HUB_NAME],
         tags=[{"Key": JUMPSTART_TAG, "Value": os.environ[ENV_VAR_JUMPSTART_SDK_TEST_SUITE_ID]}],
+        instance_type="ml.g4dn.xlarge",
     )
 
     estimator.fit(
@@ -85,9 +86,10 @@ def test_jumpstart_hub_estimator(setup, add_model_references):
         model_version=model_version,
     )
 
-    # uses ml.p3.2xlarge instance
+    # uses ml.g4dn.xlarge instance
     predictor = estimator.deploy(
         tags=[{"Key": JUMPSTART_TAG, "Value": os.environ[ENV_VAR_JUMPSTART_SDK_TEST_SUITE_ID]}],
+        instance_type="ml.g4dn.xlarge",
     )
 
     response = predictor.predict(["hello", "world"])
@@ -107,6 +109,7 @@ def test_jumpstart_hub_estimator_with_session(setup, add_model_references):
         sagemaker_session=sagemaker_session,
         tags=[{"Key": JUMPSTART_TAG, "Value": os.environ[ENV_VAR_JUMPSTART_SDK_TEST_SUITE_ID]}],
         hub_name=os.environ[ENV_VAR_JUMPSTART_SDK_TEST_HUB_NAME],
+        instance_type="ml.g4dn.xlarge",
     )
 
     estimator.fit(
@@ -124,11 +127,12 @@ def test_jumpstart_hub_estimator_with_session(setup, add_model_references):
         sagemaker_session=get_sm_session(),
     )
 
-    # uses ml.p3.2xlarge instance
+    # uses ml.g4dn.xlarge instance
     predictor = estimator.deploy(
         tags=[{"Key": JUMPSTART_TAG, "Value": os.environ[ENV_VAR_JUMPSTART_SDK_TEST_SUITE_ID]}],
         role=get_sm_session().get_caller_identity_arn(),
         sagemaker_session=get_sm_session(),
+        instance_type="ml.g4dn.xlarge",
     )
 
     response = predictor.predict(["hello", "world"])
@@ -144,6 +148,7 @@ def test_jumpstart_hub_gated_estimator_with_eula(setup, add_model_references):
         model_id=model_id,
         hub_name=os.environ[ENV_VAR_JUMPSTART_SDK_TEST_HUB_NAME],
         tags=[{"Key": JUMPSTART_TAG, "Value": os.environ[ENV_VAR_JUMPSTART_SDK_TEST_SUITE_ID]}],
+        instance_type="ml.g5.2xlarge",
     )
 
     estimator.fit(
@@ -158,6 +163,7 @@ def test_jumpstart_hub_gated_estimator_with_eula(setup, add_model_references):
         tags=[{"Key": JUMPSTART_TAG, "Value": os.environ[ENV_VAR_JUMPSTART_SDK_TEST_SUITE_ID]}],
         role=get_sm_session().get_caller_identity_arn(),
         sagemaker_session=get_sm_session(),
+        instance_type="ml.g5.2xlarge",
     )
 
     payload = {
@@ -178,6 +184,7 @@ def test_jumpstart_hub_gated_estimator_without_eula(setup, add_model_references)
         model_id=model_id,
         hub_name=os.environ[ENV_VAR_JUMPSTART_SDK_TEST_HUB_NAME],
         tags=[{"Key": JUMPSTART_TAG, "Value": os.environ[ENV_VAR_JUMPSTART_SDK_TEST_SUITE_ID]}],
+        instance_type="ml.g5.2xlarge",
     )
     with pytest.raises(Exception):
         estimator.fit(

tests/integ/sagemaker/jumpstart/private_hub/model/test_jumpstart_private_hub_model.py
@@ -116,6 +116,7 @@ def test_jumpstart_hub_gated_model(setup, add_model_references):
     predictor = model.deploy(
         accept_eula=True,
         tags=[{"Key": JUMPSTART_TAG, "Value": os.environ[ENV_VAR_JUMPSTART_SDK_TEST_SUITE_ID]}],
+        instance_type="ml.g5.2xlarge",
     )
 
     payload = model.retrieve_example_payload()

tests/integ/sagemaker/jumpstart/retrieve_uri/test_transfer_learning.py
@@ -34,8 +34,8 @@ import pytest
 def test_jumpstart_transfer_learning_retrieve_functions(setup):
 
     model_id, model_version = "huggingface-spc-bert-base-cased", "2.0.3"
-    training_instance_type = "ml.p3.2xlarge"
-    inference_instance_type = "ml.p2.xlarge"
+    training_instance_type = "ml.g4dn.xlarge"
+    inference_instance_type = "ml.g4dn.xlarge"
 
     # training
     print("Starting training...")

tests/integ/sagemaker/jumpstart/script_mode_class/test_transfer_learning.py
@@ -53,12 +53,8 @@ def test_jumpstart_transfer_learning_estimator_class(setup):
 
     model_id, model_version = "huggingface-spc-bert-base-cased", "1.2.3"
 
-    inference_instance_type = instance_types.retrieve_default(
-        model_id=model_id, model_version=model_version, scope="inference"
-    )
-    training_instance_type = instance_types.retrieve_default(
-        model_id=model_id, model_version=model_version, scope="training"
-    )
+    inference_instance_type = "ml.g4dn.xlarge"
+    training_instance_type = "ml.g4dn.xlarge"
     instance_count = 1
 
     print("Starting training...")

tests/integ/sagemaker/modules/train/test_local_model_trainer.py
@@ -147,15 +147,14 @@ def test_single_container_local_mode_s3_data(modules_sagemaker_session):
         finally:
             subprocess.run(["docker", "compose", "down", "-v"])
 
-            assert not os.path.exists(os.path.join(CWD, "shared"))
-            assert not os.path.exists(os.path.join(CWD, "input"))
-            assert not os.path.exists(os.path.join(CWD, "algo-1"))
-
             directories = [
                 "compressed_artifacts",
                 "artifacts",
                 "model",
                 "output",
+                "shared",
+                "input",
+                "algo-1",
             ]
 
             for directory in directories:

tests/integ/sagemaker/serve/test_serve_model_builder_inference_component_happy.py
@@ -104,12 +104,9 @@ def test_model_builder_ic_sagemaker_endpoint(
         except Exception as e:
             caught_ex = e
         finally:
-            if caught_ex:
-                logger.exception(caught_ex)
-                cleanup_resources(sagemaker_session, [LLAMA_IC_NAME])
-                assert False, f"{caught_ex} thrown when running mb-IC deployment test."
-
             cleanup_resources(sagemaker_session, [LLAMA_IC_NAME])
+            if caught_ex:
+                raise caught_ex
 
 
 def cleanup_resources(sagemaker_session, ic_base_names):

tests/integ/test_training_compiler.py
@@ -30,7 +30,7 @@ from tests.integ.timeout import timeout
 
 @pytest.fixture(scope="module")
 def gpu_instance_type(request):
-    return "ml.p3.2xlarge"
+    return "ml.g4dn.xlarge"
 
 
 @pytest.fixture(scope="module")
@@ -86,8 +86,8 @@ def skip_if_incompatible(gpu_instance_type, request):
 @pytest.mark.parametrize(
     "gpu_instance_type,instance_count",
     [
-        pytest.param("ml.p3.2xlarge", 1, marks=pytest.mark.release),
-        pytest.param("ml.p3.16xlarge", 2),
+        pytest.param("ml.g4dn.xlarge", 1, marks=pytest.mark.release),
+        pytest.param("ml.g4dn.12xlarge", 2),
     ],
 )
 @pytest.mark.skipif(
@@ -138,8 +138,8 @@ def test_huggingface_pytorch(
 @pytest.mark.parametrize(
     "gpu_instance_type,instance_count",
     [
-        pytest.param("ml.p3.2xlarge", 1, marks=pytest.mark.release),
-        pytest.param("ml.p3.16xlarge", 2),
+        pytest.param("ml.g4dn.xlarge", 1, marks=pytest.mark.release),
+        pytest.param("ml.g4dn.12xlarge", 2),
     ],
 )
 @pytest.mark.skip("Temporarily skip to unblock")