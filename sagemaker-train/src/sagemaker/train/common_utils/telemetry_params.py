f2e69d847068e74f4a1c30945756e5e7c6ecf098
Roja Reddy Sareddy <rsareddy@amazon.com>
2026-06-21 19:44:58 -0700

    Add granular telemetry signals decorator params and error classification

---
+++ sagemaker-train/src/sagemaker/train/common_utils/telemetry_params.py
@@ -0,0 +1,21 @@
+"""Shared telemetry param lists for sagemaker-train classes."""
+from sagemaker.core.telemetry.telemetry_logging import TelemetryParamType
+
+# Common params for SFT, DPO, RLVR, RLAIF trainers
+BASE_TRAINER_TELEMETRY_PARAMS = [
+    ("_model_name", TelemetryParamType.ATTR_VALUE),
+    ("training_type", TelemetryParamType.ATTR_VALUE),
+    ("networking", TelemetryParamType.ATTR_EXISTS),
+    ("kms_key_id", TelemetryParamType.ATTR_EXISTS),
+    ("mlflow_resource_arn", TelemetryParamType.ATTR_EXISTS),
+    ("stopping_condition", TelemetryParamType.ATTR_EXISTS),
+    ("validation_dataset", TelemetryParamType.KWARG_EXISTS),
+    ("wait", TelemetryParamType.KWARG_EXISTS),
+]
+
+# Common params for all evaluators
+BASE_EVALUATOR_TELEMETRY_PARAMS = [
+    ("networking", TelemetryParamType.ATTR_EXISTS),
+    ("kms_key_id", TelemetryParamType.ATTR_EXISTS),
+    ("mlflow_resource_arn", TelemetryParamType.ATTR_EXISTS),
+]
